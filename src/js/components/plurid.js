590e0e5e8a7ca1e1a5334cfe7b6ebc69eb761d8c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2017-10-27 20:44:19 +0300

    Rotation Degree Transformation

--- src/js/components/plurid.js
+++ src/js/components/plurid.js
@@ -2,28 +2,29 @@ var plurid = document.getElementById('plurid');
 
 var lastPosition = {};
 
+
 plurid.addEventListener("mousedown", function() {
     plurid.addEventListener("mousemove", function(event) {
-        detectDirection(event, lastPosition, angle);
+        detectDirection(event, lastPosition);
     })
 })
 
-function detectDirection(e, pos, a) {
+function detectDirection(e, pos) {
     if (typeof(lastPosition.x) != 'undefined') {
         var deltaX = lastPosition.x - event.offsetX,
             deltaY = lastPosition.y - event.offsetY;
         if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX > 0) {
             //left
-            console.log(angle + "left");
+            console.log("left");
         } else if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX < 0) {
             //right
-            console.log(angle + "right");
+            console.log("right");
         } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY > 0) {
             //up
-            console.log(angle + "up");
+            console.log("up");
         } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY < 0) {
             //down
-            console.log(angle + "down");
+            console.log("down");
         }
     }
 
@@ -41,19 +42,45 @@ function getTransform(element) {
         rotateY = 0,
         rotateZ = 0;
 
-    if (values !== 'none') {
-        var pi = Math.PI,
-            sinB = parseFloat(values[8]),
-            b = Math.round(Math.asin(sinB) * 180 / pi),
-            cosB = Math.cos(b * pi / 180),
-            matrixVal10 = parseFloat(values[9]),
-            a = Math.round(Math.asin(-matrixVal10 / cosB) * 180 / pi),
-            matrixVal1 = parseFloat(values[0]),
-            c = Math.round(Math.acos(matrixVal1 / cosB) * 180 / pi);
+    var pi = Math.PI;
 
-        rotateX = a;
-        rotateY = b;
-        rotateZ = c;
+    if (values !== 'none') {
+        var cos1ForX = parseFloat(values[5]);
+        var sin1ForX = parseFloat(values[6]);
+        var sin2ForX = parseFloat(values[9]);
+        var cos2ForX = parseFloat(values[10]);
+        // if (sin1ForX > 0 && cos1ForX > 0) {
+        //     var rotX = Math.asin(sin1ForX) * 180 / pi;
+        // } else if (sin1ForX > 0 && cos1ForX < 0) {
+        //     var rotX = Math.acos(cos1ForX) * 180 / pi;
+        // } else {
+        //     var rotX = Math.acos(cos1ForX) * 180 / pi;
+        // }
+        if (sin1ForX > 0) {
+            var rotX = Math.acos(cos1ForX) * 180 / pi;
+        } else if (sin1ForX < 0 && cos1ForX < 0){
+            var rotX = Math.asin(sin2ForX) * 180 / pi + 180;
+        } else if (cos1ForX == 0) {
+            var rotX = 0;
+        } else {
+            console.log('test');
+            var rotX = 360 - Math.acos(cos1ForX) * 180 / pi;
+        }
+
+        // console.log(`sin1: ${sin1ForX}, sin2: ${sin2ForX}`);
+        // console.log(`cos1: ${cos1ForX}, cos2: ${cos2ForX}`);
+
+
+        var cosForY = parseFloat(values[0]);
+        var rotY = Math.acos(cosForY) * 180 / pi;
+
+        var sinForZ = parseFloat(values[1]);
+        var rotZ = Math.asin(sinForZ) * 180 / pi;
+
+
+        rotateX = rotX.toPrecision(4);
+        rotateY = rotY.toPrecision(4);
+        rotateZ = rotZ.toPrecision(4);
     }
 
     return {
@@ -73,4 +100,4 @@ function getMatrixValues(element) {
     return matrixValues;
 }
 
-// console.log(getMatrixValues(plurid));
+console.log(getMatrixValues(plurid));
