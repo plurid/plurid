fa854bcb61714e24ce569d8601c75035de36cadc
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2017-10-28 12:31:48 +0300

    Manual Rotation on X and Y

--- src/js/components/plurid.js
+++ src/js/components/plurid.js
@@ -45,48 +45,61 @@ function getTransform(element) {
     var pi = Math.PI;
 
     if (values !== 'none') {
+        // rotX
         var cos1ForX = parseFloat(values[5]);
         var sin1ForX = parseFloat(values[6]);
         var sin2ForX = parseFloat(values[9]);
-        var cos2ForX = parseFloat(values[10]);
-        // if (sin1ForX > 0 && cos1ForX > 0) {
-        //     var rotX = Math.asin(sin1ForX) * 180 / pi;
-        // } else if (sin1ForX > 0 && cos1ForX < 0) {
-        //     var rotX = Math.acos(cos1ForX) * 180 / pi;
-        // } else {
-        //     var rotX = Math.acos(cos1ForX) * 180 / pi;
-        // }
+
         if (sin1ForX > 0) {
+            console.log('X a');
             var rotX = Math.acos(cos1ForX) * 180 / pi;
-        } else if (sin1ForX < 0 && cos1ForX < 0){
-            var rotX = Math.asin(sin2ForX) * 180 / pi + 180;
+        } else if (sin1ForX < 0 && cos1ForX < 0) {
+            console.log('X b');
+            var rotX = Math.asin(sin1ForX) * 180 / pi * (-1) + 180;
         } else if (cos1ForX == 0) {
+            console.log('X c');
             var rotX = 0;
         } else {
-            console.log('test');
+            console.log('X d');
             var rotX = 360 - Math.acos(cos1ForX) * 180 / pi;
         }
 
-        // console.log(`sin1: ${sin1ForX}, sin2: ${sin2ForX}`);
-        // console.log(`cos1: ${cos1ForX}, cos2: ${cos2ForX}`);
+        // rotY
+        var cos1ForY = parseFloat(values[0]);
+        var sin1ForY = parseFloat(values[2]);
+        var sin2ForY = parseFloat(values[8]);
 
 
-        var cosForY = parseFloat(values[0]);
-        var rotY = Math.acos(cosForY) * 180 / pi;
+        if (sin1ForY > 0 && sin2ForY < 0) {
+            console.log('Y a0');
+            var rotY = 360 - Math.acos(cos1ForY) * 180 / pi;
+        } else if (sin1ForY > 0) {
+            console.log('Y a');
+            var rotY = Math.acos(cos1ForY) * 180 / pi;
+        } else if (sin1ForY < 0 && cos1ForY < 0) {
+            console.log('Y b');
+            var rotY = Math.asin(sin2ForY) * 180 / pi * (-1) + 180;
+        } else if (sin1ForY < 0) {
+            console.log('Y c');
+            var rotY = Math.asin(sin2ForY) * 180 / pi;
+        } else if (cos1ForY == 1) {
+            console.log('Y d');
+            var rotY = 0;
+        } else {
+            console.log('Y e');
+            var rotY = 360 - Math.acos(cos1ForY) * 180 / pi;
+        }
 
-        var sinForZ = parseFloat(values[1]);
-        var rotZ = Math.asin(sinForZ) * 180 / pi;
-
+        // console.log(`sin1: ${sin1ForY}, sin2: ${sin2ForY}`);
+        // console.log(`cos1: ${cos1ForY}`);
 
         rotateX = rotX.toPrecision(4);
         rotateY = rotY.toPrecision(4);
-        rotateZ = rotZ.toPrecision(4);
     }
 
     return {
         rotateX: rotateX,
         rotateY: rotateY,
-        rotateZ: rotateZ
     };
 }
 
