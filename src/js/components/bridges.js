a5e0d3dac4eb5b9d3dbd87d8fa08d886d04d44bb
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2017-12-30 00:09:54 +0200

    Plurid Branch somewhat in positions

--- src/js/components/bridges.js
+++ src/js/components/bridges.js
@@ -3,15 +3,17 @@
 var pluridBridges = document.getElementsByTagName('a');
 
 // console.log(pluridBridges[0]);
+var id = 0
 
 function pluridifyBridges(bridges) {
     for (i = 0; i < bridges.length; i++) {
         bridges[i].addEventListener('click', function(event) {
             event.preventDefault();
 
+            var bridgeId = "plurid-bridge-" + id;
             var newDiv = document.createElement("div")
 
-            newDiv.innerHTML = '<div class="plurid-bridge-container">' +
+            newDiv.innerHTML = `<div class="plurid-bridge-container ${bridgeId}">` +
                                     '<div class="plurid-bridge">' +
                                         '<iframe src="' +
                                             this.href +
@@ -19,8 +21,23 @@ function pluridifyBridges(bridges) {
                                     '</div>' +
                                 '</div>'
 
-            this.parentElement.parentElement.parentElement.appendChild(newDiv);
+            // this.parentElement.parentElement.parentElement.appendChild(newDiv);
+            this.parentElement.appendChild(newDiv);
 
+            var pluridBridge = document.getElementsByClassName(bridgeId)[0];
+
+            pluridBridge.style.marginLeft = this.offsetLeft - 570 + "px";
+            pluridBridge.style.marginTop = -50 + "px";
+            console.log(pluridBridge)
+            // console.log(pluridBridge)
+            console.log(this.offsetLeft, this.offsetTop)
+            // console.log(-this.offsetLeft, -this.offsetTop)
+            // console.log(this.getBoundingClientRect().left + window.scrollX, this.getBoundingClientRect().top + window.scrollY)
+            // console.log(this.getBoundingClientRect().left, this.getBoundingClientRect().top)
+            // console.log(this)
+
+
+            id += 1;
             // console.log(this.parentElement.parentElement)
         })
 
