27aaf944fab4b5c562dac3a92e29ea03b95cc771
Jan Cavel <caveljan@gmail.com>
2018-01-15 13:04:12 +0200

    Dev: detect plurid to transform

--- src/js/source/elements/commands.js
+++ src/js/source/elements/commands.js
@@ -1,16 +1,51 @@
 export function transform(element) {
     element.addEventListener('wheel', event => {
         // console.log(event)
+        // console.log(event.path);
+        // console.log(searchForRootId(event.path));
+
+        let pluridToTransform = getPluridToTransform(event.path)
+
         if(event.shiftKey) {
-            console.log(`Rotate ${event.path[0].localName}`);
+            console.log(`Rotate ${pluridToTransform}`);
         }
 
         if(event.altKey) {
-            console.log(`Translate ${event.path[0].localName}`);
+            console.log(`Translate ${pluridToTransform}`);
         }
 
         if(event.metaKey) {
-            console.log(`Scale ${event.path[0].localName}`);
+            console.log(`Scale ${pluridToTransform}`);
         }
     });
+}
+
+
+function getPluridToTransform(path) {
+    let pluridToTransform = path[0].localName;
+
+    if (pluridToTransform === "plurid-options") {
+        // do nothing
+    }
+
+    if (pluridToTransform === "plurid-roots" || pluridToTransform === "plurid-container") {
+        pluridToTransform = "plurid-roots";
+    } else {
+        // console.log(searchForRootId(path));
+        pluridToTransform = searchForRootId(path);
+    }
+
+    return pluridToTransform;
+}
+
+
+
+function searchForRootId(path) {
+    // console.log(path.length);
+    for (var i = 0; i < path.length; i++) {
+        // console.log(path[i]);
+        if (path[i].localName === "plurid-root") {
+            return path[i].id;
+        }
+    }
 }
