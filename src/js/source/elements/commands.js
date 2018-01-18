96ce7182b8e8e991b6c484c6a2314b01691b8e33
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-01-18 19:17:27 +0200

    Dev: inject plurid options

--- src/js/source/elements/commands.js
+++ src/js/source/elements/commands.js
@@ -35,21 +35,8 @@ export function transform(element) {
 
 
 export function renderOptions(element) {
-    // console.log(element);
-    // var link = document.createElement('link');
-    // link.rel = 'import';
-    // link.id = 'import-plurid-options';
-    // link.href = '../../src/html/source/templates/options.html';
-
-    // document.head.appendChild(link);
-
-
-    // let importDocument = document.querySelector('#import-plurid-options');
-    // console.log(importDocument);
-
-    // let templateOptions = importDocument.querySelector("#template-plurid-options");
-
-    // console.log(templateOptions);
+    var options = document.createElement("plurid-options");
+    element.appendChild(options);
 }
 
 
@@ -76,5 +63,63 @@ export function displayOptions(element) {
 }
 
 
-// let templateOptions = document.querySelector("#template-plurid-options");
-// console.log(templateOptions);
+export function contentOptions() {
+    let content = `<div class="plurid-container-options-content">
+                    <div class="plurid-container-options-group">
+                        <span class="plurid-container-button plurid-container-rotate-left">&#x25c0;</span>
+                        <span class="plurid-container-button plurid-container-rotate-up">&#x25B2;</span>
+                        <p>Rotation</p>
+                        <span class="plurid-container-button plurid-container-rotate-down">&#x25BC;</span>
+                        <span class="plurid-container-button plurid-container-rotate-right">&#x25b6;</span>
+                    </div>
+
+                    <div class="plurid-container-options-group">
+                        <span class="plurid-container-button plurid-container-translate-left">&#x25c0;</span>
+                        <span class="plurid-container-button plurid-container-translate-up">&#x25B2;</span>
+                        <p>Translation</p>
+                        <span class="plurid-container-button plurid-container-translate-down">&#x25BC;</span>
+                        <span class="plurid-container-button plurid-container-translate-right">&#x25b6;</span>
+                    </div>
+
+                    <div class="plurid-container-options-group">
+                        <span class="plurid-container-button plurid-container-scale-up">&#x25B2;</span>
+                        <p>Scale</p>
+                        <span class="plurid-container-button plurid-container-scale-down">&#x25BC;</span>
+                    </div>
+
+                    <div class="plurid-container-options-group">
+                        <p class="plurid-container-link-expand plurid-container-more-button">More...</p>
+                    </div>
+
+                    <div class="plurid-container-options-more">
+                        <div class="plurid-container-options-group">
+                            <p>Background</p>
+                            <span class="plurid-container-button plurid-container-color"></span>
+                        </div>
+                        <div class="plurid-container-options-group">
+                            <p>Use Scroll to Translate</p>
+                            <input type="checkbox" class="plurid-container-use-scroll">
+                        </div>
+                        <div class="plurid-container-options-group">
+                            <p class="plurid-container-link-expand plurid-container-shortcuts-button">Shortcuts</p>
+                        </div>
+                    </div>
+
+                    <div class="plurid-container-options-shortcuts">
+                        <div class="plurid-container-options-group">
+                            <p>Hold Button and Move Pointer:</p>
+                        </div>
+                        <div class="plurid-container-options-group">
+                            <p>Shift for Rotation</p>
+                        </div>
+                        <div class="plurid-container-options-group">
+                            <p>Alt/Option for Translation</p>
+                        </div>
+                        <div class="plurid-container-options-group">
+                            <p>Control/Command for Scale</p>
+                        </div>
+                    </div>
+                </div>`
+
+    return content;
+}
