5365808686132fdd1c82fa22091ba32b8eb6367a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-01-19 15:54:08 +0200

    Dev: shortcuts listeners

--- src/js/source/options/options.js
+++ src/js/source/options/options.js
@@ -31,144 +31,167 @@ var timer;
 // fix bug when clicking and moving mouse out of the hover area
 
 
-rotateLeft[0].addEventListener("click", event => {
-    let direction = "left";
-    plurid.rotatePlurid(event, pluridToTransform, direction);
-    // console.log(event);
-})
-
-rotateRight[0].addEventListener("click", event => {
-    let direction = "right";
-    plurid.rotatePlurid(event, pluridToTransform, direction);
-    // console.log(event);
-})
-
-rotateUp[0].addEventListener("click", event => {
-    let direction = "up";
-    plurid.rotatePlurid(event, pluridToTransform, direction);
-    // console.log(event);
-})
-
-rotateDown[0].addEventListener("click", event => {
-    let direction = "down";
-    plurid.rotatePlurid(event, pluridToTransform, direction);
-    // console.log(event);
-})
-
-
-
-
-
-// var rotatePlurid = function(event, direction) {
-
+// rotateLeft[0].addEventListener("click", event => {
+//     let direction = "left";
 //     plurid.rotatePlurid(event, pluridToTransform, direction);
-//     timer = setTimeout(rotatePlurid, 35, event, direction);
-// }
+//     // console.log(event);
+// })
 
-// var translatePlurid = function(event, direction) {
-//     plurid.translatePlurid(event, pluridToTransform, direction);
-//     timer = setTimeout(translatePlurid, 25, event, direction);
-// }
+// rotateRight[0].addEventListener("click", event => {
+//     let direction = "right";
+//     plurid.rotatePlurid(event, pluridToTransform, direction);
+//     // console.log(event);
+// })
 
-// var scalePlurid = function(event, direction) {
-//     plurid.scalePlurid(event, pluridToTransform, direction);
-//     timer = setTimeout(scalePlurid, 35, event, direction);
-// }
+// rotateUp[0].addEventListener("click", event => {
+//     let direction = "up";
+//     plurid.rotatePlurid(event, pluridToTransform, direction);
+//     // console.log(event);
+// })
 
-
-// // rotation
-// for (var i=0; i < rotateLeft.length; i++) {
-//     rotateLeft[i].addEventListener("mousedown", function(event) {
-//         console.log("a")
-//         var direction = "right";
-//         timer = setTimeout(rotatePlurid, 35, event, direction);
-//     });
-
-//     rotateLeft[i].addEventListener("mouseup", function(event) {
-//         clearTimeout(timer);
-//     });
-// }
-
-// for (var i=0; i < rotateRight.length; i++) {
-//     rotateRight[i].addEventListener("mousedown", function(event) {
-//         var direction = "left";
-//         timer = setTimeout(rotatePlurid, 35, event, direction);
-//     });
-
-//     rotateRight[i].addEventListener("mouseup", function(event) {
-//         clearTimeout(timer);
-//     });
-// }
+// rotateDown[0].addEventListener("click", event => {
+//     let direction = "down";
+//     plurid.rotatePlurid(event, pluridToTransform, direction);
+//     // console.log(event);
+// })
 
 
-// // translation
-// for (var i=0; i < translateLeft.length; i++) {
-//     translateLeft[i].addEventListener("mousedown", function(event) {
-//         var direction = "left";
-//         timer = setTimeout(translatePlurid, 25, event, direction);
-//     });
-
-//     translateLeft[i].addEventListener("mouseup", function(event) {
-//         clearTimeout(timer);
-//     });
-// }
-
-// for (var i=0; i < translateUp.length; i++) {
-//     translateUp[i].addEventListener("mousedown", function(event) {
-//         var direction = "up";
-//         timer = setTimeout(translatePlurid, 25, event, direction);
-//     });
-
-//     translateUp[i].addEventListener("mouseup", function(event) {
-//         clearTimeout(timer);
-//     });
-// }
-
-// for (var i=0; i < translateDown.length; i++) {
-//     translateDown[i].addEventListener("mousedown", function(event) {
-//         var direction = "down";
-//         timer = setTimeout(translatePlurid, 25, event, direction);
-//     });
-
-//     translateDown[i].addEventListener("mouseup", function(event) {
-//         clearTimeout(timer);
-//     });
-// }
-
-// for (var i=0; i < translateRight.length; i++) {
-//     translateRight[i].addEventListener("mousedown", function(event) {
-//         var direction = "right";
-//         timer = setTimeout(translatePlurid, 25, event, direction);
-//     });
-
-//     translateRight[i].addEventListener("mouseup", function(event) {
-//         clearTimeout(timer);
-//     });
-// }
 
 
-// // scale
-// for (var i=0; i < scaleUp.length; i++) {
-//     scaleUp[i].addEventListener("mousedown", function(event) {
-//         var direction = "up";
-//         timer = setTimeout(scalePlurid, 35, event, direction);
-//     });
 
-//     scaleUp[i].addEventListener("mouseup", function(event) {
-//         clearTimeout(timer);
-//     });
-// }
+var rotatePlurid = function(event, direction) {
+    plurid.rotatePlurid(event, pluridToTransform, direction);
+    timer = setTimeout(rotatePlurid, 35, event, direction);
+}
 
-// for (var i=0; i < scaleDown.length; i++) {
-//     scaleDown[i].addEventListener("mousedown", function(event) {
-//         var direction = "down";
-//         timer = setTimeout(scalePlurid, 35, event, direction);
-//     });
+var translatePlurid = function(event, direction) {
+    plurid.translatePlurid(event, pluridToTransform, direction);
+    timer = setTimeout(translatePlurid, 25, event, direction);
+}
 
-//     scaleDown[i].addEventListener("mouseup", function(event) {
-//         clearTimeout(timer);
-//     });
-// }
+var scalePlurid = function(event, direction) {
+    plurid.scalePlurid(event, pluridToTransform, direction);
+    timer = setTimeout(scalePlurid, 35, event, direction);
+}
+
+
+// rotation
+for (var i=0; i < rotateLeft.length; i++) {
+    rotateLeft[i].addEventListener("mousedown", function(event) {
+        var direction = "right";
+        timer = setTimeout(rotatePlurid, 35, event, direction);
+    });
+
+    rotateLeft[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
+
+for (var i=0; i < rotateRight.length; i++) {
+    rotateRight[i].addEventListener("mousedown", function(event) {
+        var direction = "left";
+        timer = setTimeout(rotatePlurid, 35, event, direction);
+    });
+
+    rotateRight[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
+
+for (var i=0; i < rotateRight.length; i++) {
+    rotateUp[i].addEventListener("mousedown", function(event) {
+        var direction = "down";
+        timer = setTimeout(rotatePlurid, 35, event, direction);
+    });
+
+    rotateUp[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
+
+
+for (var i=0; i < rotateRight.length; i++) {
+    rotateDown[i].addEventListener("mousedown", function(event) {
+        var direction = "up";
+        timer = setTimeout(rotatePlurid, 35, event, direction);
+    });
+
+    rotateDown[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
+
+
+
+
+// translation
+for (var i=0; i < translateLeft.length; i++) {
+    translateLeft[i].addEventListener("mousedown", function(event) {
+        var direction = "left";
+        timer = setTimeout(translatePlurid, 25, event, direction);
+    });
+
+    translateLeft[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
+
+for (var i=0; i < translateUp.length; i++) {
+    translateUp[i].addEventListener("mousedown", function(event) {
+        var direction = "up";
+        timer = setTimeout(translatePlurid, 25, event, direction);
+    });
+
+    translateUp[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
+
+for (var i=0; i < translateDown.length; i++) {
+    translateDown[i].addEventListener("mousedown", function(event) {
+        var direction = "down";
+        timer = setTimeout(translatePlurid, 25, event, direction);
+    });
+
+    translateDown[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
+
+for (var i=0; i < translateRight.length; i++) {
+    translateRight[i].addEventListener("mousedown", function(event) {
+        var direction = "right";
+        timer = setTimeout(translatePlurid, 25, event, direction);
+    });
+
+    translateRight[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
+
+
+// scale
+for (var i=0; i < scaleUp.length; i++) {
+    scaleUp[i].addEventListener("mousedown", function(event) {
+        var direction = "up";
+        timer = setTimeout(scalePlurid, 35, event, direction);
+    });
+
+    scaleUp[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
+
+for (var i=0; i < scaleDown.length; i++) {
+    scaleDown[i].addEventListener("mousedown", function(event) {
+        var direction = "down";
+        timer = setTimeout(scalePlurid, 35, event, direction);
+    });
+
+    scaleDown[i].addEventListener("mouseup", function(event) {
+        clearTimeout(timer);
+    });
+}
 
 
 
