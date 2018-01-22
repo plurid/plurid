d6958145b59434f09b99c1d9b684d6bc0294b6db
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-01-22 11:32:47 +0200

    Dev: insert plurid

--- src/js/source/elements/branch-core.js
+++ src/js/source/elements/branch-core.js
@@ -1,8 +1,42 @@
 export function testFun() {
     let contentTest = document.getElementById("test-content");
 
-    contentTest.innerHTML = `<input type="text"><h1>aAAA</h1><iframe src="./legacy.html"></iframe>`;
+    // let content = document.createElement("div");
+    // let contentInput = document.createElement("input");
 
-    console.log(contentTest);
+    // content.innerHTML = "TEST"
+
+    // contentTest.appendChild(content);
+    // contentTest.appendChild(contentInput);
+
+    // let contentExtraDiv = document.createElement("div");
+    // contentExtraDiv.innerHTML = `<input type="text"><h1>aAAA</h1>`;
+    // contentTest.appendChild(contentExtraDiv);
+
+    // console.log(contentTest);
+
+
+
+
+    let httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = innerHTMLContents;
+    httpRequest.open('GET', 'http://localhost:8000/test/examples/insert.html', true);
+    httpRequest.withCredentials = true;
+    httpRequest.send();
+
+    let httpReqDiv = document.createElement("div");
+
+    function innerHTMLContents() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                httpReqDiv.innerHTML = httpRequest.responseText;
+            } else {
+                console.log('There was a problem with the request.');
+            }
+        }
+    }
+
+    contentTest.appendChild(httpReqDiv);
+
 }
-
