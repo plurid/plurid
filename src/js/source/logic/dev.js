2d990fc284db84ecccadc3768a69355ca0bb2178
Jan Cavel <caveljan@gmail.com>
2018-05-02 13:53:10 +0300

    Dev: plurid-container creation

--- src/js/source/logic/dev.js
+++ src/js/source/logic/dev.js
@@ -13,23 +13,23 @@ function setLink() {
                 if (this.readyState == 4 && this.status == 200) {
                     let parser = new DOMParser();
                     let doc = parser.parseFromString(this.responseText, "text/html");
-                    console.log(doc.body);
+                    // console.log(doc.body);
 
                     let newBranch = document.createElement("plurid-branch");
 
-                    // newBranch.innerHTML = `
-                    //                         <plurid-insertion></plurid-insertion>
-                    //                         <plurid-bridge></plurid-bridge>
+                    newBranch.innerHTML = `
+                                            <plurid-insertion></plurid-insertion>
+                                            <plurid-bridge></plurid-bridge>
 
-                    //                         <plurid-scion>
-                    //                             <plurid-sheet>
-                    //                                 <plurid-content>
-                    //                                 ${doc.body.innerText}
-                    //                                 </plurid-content>
-                    //                             </plurid-sheet>
-                    //                         </plurid-scion>
-                    //                     `;
-                    newBranch.innerHTML = `${doc.body.innerHTML}`;
+                                            <plurid-scion>
+                                                <plurid-sheet>
+                                                    <plurid-content>
+                                                    ${doc.body.innerHTML}
+                                                    </plurid-content>
+                                                </plurid-sheet>
+                                            </plurid-scion>
+                                        `;
+                    // newBranch.innerHTML = `${doc.body.innerHTML}`;
 
                     insertAfter(newBranch, anchorTag);
                     // renderBranch();
@@ -52,3 +52,55 @@ function insertAfter(newNode, referenceNode) {
 
 
 setLink()
+
+
+
+
+function checkForContainers() {
+    return !!document.getElementsByTagName('plurid-container')[0];
+}
+
+
+if (!checkForContainers()) {
+    // console.log(checkForContainers());
+    let body = document.body;
+    let pluridPages = document.getElementsByTagName('plurid-page');
+    // console.log(pluridPages);
+
+    let container = document.createElement("plurid-container");
+
+    for (let pluridPage of pluridPages) {
+        let html = pluridPage.innerHTML;
+        let plurid = document.createElement('plurid-sheet');
+        plurid.innerHTML = html;
+        container.appendChild(plurid);
+    }
+
+    console.log(container);
+
+    // for (let i = 0; i < scripts.length; i++) {
+    //     body.removeChild(scripts[i]);
+    // }
+
+    // for (let script in scripts) {
+    //     body.removeChild(script);
+    // }
+
+    // let children = body.children;
+    // let container = document.createElement("plurid-container");
+
+    // console.log(children);
+    // console.log('aa', container);
+
+    // for (let child of children) {
+    //     // console.log(child);
+    //     // container.appendChild(child);
+    // }
+
+    // console.log('bb', container);
+
+
+    // console.log(container);
+    // console.log(body.children);
+    // console.log(children);
+}
