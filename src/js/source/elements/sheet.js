3ebe20bf53e84ceb9eaa75494cdb39fbfd232a83
Jan Cavel <caveljan@gmail.com>
2018-06-06 13:23:12 +0300

    Dev: local coordinates at click

--- src/js/source/elements/sheet.js
+++ src/js/source/elements/sheet.js
@@ -52,6 +52,44 @@ class PluridSheet extends HTMLElement {
         renderControls(this, sheetId);
 
         sheetId++;
+
+        this.addEventListener('click', event => {
+            // let cX = event.clientX;
+            // let sX = event.screenX;
+            // let cY = event.clientY;
+            // let sY = event.screenY;
+            // let pX = event.pageX;
+            // let pY = event.pageY;
+            // let oX = event.offsetX;
+            // let oY = event.offsetY;
+            // let thisTop = this.offsetTop;
+            // let thisLeft = this.offsetLeft;
+
+            // var x = event.pageX - this.offsetLeft;
+            // var y = event.pageY - this.offsetTop;
+
+            // console.log('-----------------');
+            // console.log('clientX', cX);
+            // console.log('clientY', cY);
+            // console.log('screenX', sX);
+            // console.log('screenY', sY);
+            // console.log('pageX', pX);
+            // console.log('pageY', pY);
+            // console.log('offsetX', oX);
+            // console.log('offsetY', oY);
+            // console.log('offsetThisTop', thisTop);
+            // console.log('offsetThisLeft', thisLeft);
+
+            // console.log('x', x);
+            // console.log('y', y);
+            // console.log(event);
+            // console.log(this);
+            let right = this.offsetLeft + this.offsetWidth;
+            let top = this.offsetTop;
+
+            // console.log('sheet right', right);
+            // console.log('sheet top', top);
+        });
     }
 
     get name() {
