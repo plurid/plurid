05a86e2b64c558214c07d5452ae274e704357753
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-06-13 10:00:46 +0300

    Refact: getBranchById() added to pluridScene global

--- src/js/source/state/plurid-scene.js
+++ src/js/source/state/plurid-scene.js
@@ -10,6 +10,27 @@
             rootSheets: [],
             pages: 0
         },
-        content: []
+        content: [],
+        getBranchById: (branchId) => {
+            for (let rootElement of pluridScene.content) {
+                for (let child of rootElement.children) {
+                    if (child.branchId == branchId) {
+                        return child;
+                    } else if (child.children != []) {
+                        return getChild(branchId, child.children);
+                    }
+                }
+            }
+
+            function getChild(branchId, children) {
+                for (let child of children) {
+                    if (child.branchId == branchId) {
+                        return child;
+                    } else if (child.children != []) {
+                        return getChild(branchId, child.children);
+                    }
+                }
+            }
+        }
     }
 }).call(this, global)
