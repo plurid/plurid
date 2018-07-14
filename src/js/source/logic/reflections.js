e2bbd6ae35a7c5f4b46df47a287c4eb4b4609f53
Jan Cavel <caveljan@gmail.com>
2018-07-14 14:49:59 +0300

    Dev: shadows and reflections files

---
+++ src/js/source/logic/reflections.js
@@ -0,0 +1,10 @@
+export function setReflections(pluridSheet, sheetHeight, ground, top = 0) {
+    const reflectGround = ground + 1 - sheetHeight - top;
+    // console.log('ground', ground);
+    // console.log('sheetHeight', sheetHeight);
+    // console.log('top', top);
+
+    // const reflectGround = ground - sheetHeight + 1;
+
+    pluridSheet.style.webkitBoxReflect = `below ${reflectGround}px linear-gradient(to top, rgba(0,0,0,0.5) 0%, rgba(30,30,30,0.25) 20%, rgba(60,60,60,0) 40%)`;
+}
