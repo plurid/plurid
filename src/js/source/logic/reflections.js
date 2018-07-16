8d22c213c09eb8caa87590d1c438487007619520
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-07-16 13:27:14 +0300

    Fix: reflection opacity

--- src/js/source/logic/reflections.js
+++ src/js/source/logic/reflections.js
@@ -6,5 +6,5 @@ export function setReflections(pluridSheet, sheetHeight, ground, top = 0) {
 
     // const reflectGround = ground - sheetHeight + 1;
 
-    pluridSheet.style.webkitBoxReflect = `below ${reflectGround}px linear-gradient(to top, rgba(0,0,0,0.5) 0%, rgba(30,30,30,0.25) 20%, rgba(60,60,60,0) 40%)`;
+    pluridSheet.style.webkitBoxReflect = `below ${reflectGround}px linear-gradient(to top, rgba(0,0,0,0.2) 0%, rgba(30,30,30,0.1) 20%, rgba(60,60,60,0) 40%)`;
 }
