a8598a0285db2f2a1b0decca59eac72e70ddcd87
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-07-24 23:03:03 +0300

    Dev: gyroscope rotation

--- src/js/source/logic/gyroscope.js
+++ src/js/source/logic/gyroscope.js
@@ -1,37 +1,54 @@
 import { computeQuaternionFromEulers,
          quaternionFromAxisAngle } from './quaternion';
+import { rotateMatrix,
+         translateMatrix,
+         scaleMatrix } from './matrix';
+import { setTransform,
+         getTransformTranslate,
+         getTransformScale,
+         getyPos } from './transforms-core';
+
+
+
+// Z - X - Y
+// processGyro(0, 0, 30);
+// var gyro = quaternionFromAxisAngle(0, 0, 0, 0);
 
 
 //get orientation info
 if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", () => {
-        processGyro(event.alpha, event.beta, event.gamma);
+    window.addEventListener("deviceorientation", event => {
+        let plurid = document.getElementById(pluridScene.meta.activePlurid);
+        console.log(plurid);
+	    document.getElementById("alpha").innerHTML= plurid.id;
+
+        processGyro(event.alpha, event.beta, event.gamma, plurid);
     }, true);
 }
 
 
-// Z - X - Y
-processGyro(0, 45, 0);
-
-
-function processGyro(alpha, beta, gamma) {
+function processGyro(alpha, beta, gamma, plurid) {
 	document.getElementById("alpha").innerHTML= alpha.toFixed(5);
 	document.getElementById("beta").innerHTML= beta.toFixed(5);
     document.getElementById("gamma").innerHTML = gamma.toFixed(5);
 
-    gyro = computeQuaternionFromEulers(alpha, beta, gamma);
+    let valRotationMatrix = rotateMatrix(beta, gamma, alpha);
 
-    document.getElementById("x").innerHTML=gyro.x.toFixed(5);
-    document.getElementById("y").innerHTML=gyro.y.toFixed(5);
-    document.getElementById("z").innerHTML=gyro.z.toFixed(5);
-    document.getElementById("w").innerHTML=gyro.w.toFixed(5);
+    var translateX = getTransformTranslate(plurid).translateX;
+    var translateY = getTransformTranslate(plurid).translateY;
+    var translateZ = 0;
+    var scale = getTransformScale(plurid).scale;
 
-    // let pointRotate = [0, -45, -45];
-    // let result = rotatePointViaQuaternion(pointRotate, gyro);
-    // console.log(result);
+    var valTranslationMatrix = translateMatrix(translateX, translateY, translateZ);
+    var valScaleMatrix = scaleMatrix(scale);
 
-    // let result2 = makeRotationMatrixFromQuaternion(result);
-    // console.log(result2);
+    var yPos = getyPos(event, plurid);
+
+    setTransform(plurid, valRotationMatrix, valTranslationMatrix, valScaleMatrix, yPos);
+
+    // gyro = computeQuaternionFromEulers(alpha, beta, gamma);
+    // document.getElementById("x").innerHTML=gyro.x.toFixed(5);
+    // document.getElementById("y").innerHTML=gyro.y.toFixed(5);
+    // document.getElementById("z").innerHTML=gyro.z.toFixed(5);
+    // document.getElementById("w").innerHTML=gyro.w.toFixed(5);
 }
-
-var gyro = quaternionFromAxisAngle(0, 0, 0, 0);
