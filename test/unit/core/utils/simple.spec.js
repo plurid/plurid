fe48c20ccac1cd1c7b3ac3038bf4e3f3a0353a84
Jan Cavel <caveljan@gmail.com>
2018-07-30 15:30:34 +0300

    Setup: jest

--- test/unit/core/utils/simple.spec.js
+++ test/unit/core/utils/simple.spec.js
@@ -1,14 +1,13 @@
 import { capitalize } from '../../../../src/js/source/core/utils/simple';
 
-const assert = require('chai').assert;
 
 
 describe('core > utils > simple', () => {
     describe('capitalize()', () => {
-        it('should return first letter of string capitalized', () => {
+        test('should return first letter of string capitalized', () => {
             let testString = 'testString';
 
-            assert.equal(capitalize(testString), "TestString");
+            expect(capitalize(testString)).toBe('TestString');
         });
     });
 });
