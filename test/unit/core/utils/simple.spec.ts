197bba5354d0f72328ea68edfa98bea7405309f2
Jan Cavel <caveljan@gmail.com>
2019-01-03 19:03:03 +0200

    test: examples/unit

--- test/unit/core/utils/simple.spec.ts
+++ test/unit/core/utils/simple.spec.ts
@@ -1,11 +1,11 @@
-import { capitalize } from '../../../../src/js/source/core/utils/simple';
+import { capitalize } from '../../../../src/ts/source/core/utils/simple';
 
 
 
 describe('core > utils > simple', () => {
     describe('capitalize()', () => {
         test('should return first letter of string capitalized', () => {
-            let testString = 'testString';
+            const testString = 'testString';
 
             expect(capitalize(testString)).toBe('TestString');
         });
