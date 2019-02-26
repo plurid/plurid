92e59f79836bedccf6ffba15d810adc9cef678d1
Jan Cavel <caveljan@gmail.com>
2019-02-26 16:03:20 +0200

    setup: styled-components

--- src/components/plurid/Container/index.tsx
+++ src/components/plurid/Container/index.tsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import PluridSpace from '../Space';
+
+const StyledPluridContainer = styled.div`
+    height: 100vh;
+`;
 
 class PluridContainer extends Component {
     public render() {
         const { children } = this.props;
 
         return (
-            <PluridSpace>
-                {children}
-            </PluridSpace>
+            <StyledPluridContainer>
+                <PluridSpace>
+                    {children}
+                </PluridSpace>
+            </StyledPluridContainer>
         );
     }
 }
