cd3a172f11911a8017ce4a15b5e2d9e3b1687d34
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-02-26 12:15:16 +0200

    setup: testing

--- src/test.spec.tsx
+++ src/test.spec.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import { PluridApp } from './index';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PluridApp />, div);
+    shallow(<PluridApp />);
 });
