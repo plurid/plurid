e1ada4009f7aeaddc97c7fe9fdb847939a8adae4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-02-28 10:22:20 +0200

    dev: matrix quaternion

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-import { getWheelDirection } from './modules/getDirection'
+import { getWheelDirection } from './modules/getDirection';
+import {
+    rotateMatrix,
+    translateMatrix,
+    scaleMatrix,
+    multiplyMatrices,
+    multiplyArrayOfMatrices,
+    matrixArrayToCssMatrix
+} from './modules/matrix';
+import {
+    computeQuaternionFromEulers,
+    quaternionFromAxisAngle,
+    inverseQuaternion,
+    conjugateQuaternion,
+    quaternionMultiply,
+    rotatePointViaQuaternion,
+    makeRotationMatrixFromQuaternion
+} from './modules/quaternion';
 
-export class PluridEngine {}
-
-export { getWheelDirection }
+export {
+    getWheelDirection,
+    rotateMatrix,
+    translateMatrix,
+    scaleMatrix,
+    multiplyMatrices,
+    multiplyArrayOfMatrices,
+    matrixArrayToCssMatrix,
+    computeQuaternionFromEulers,
+    quaternionFromAxisAngle,
+    inverseQuaternion,
+    conjugateQuaternion,
+    quaternionMultiply,
+    rotatePointViaQuaternion,
+    makeRotationMatrixFromQuaternion
+};
