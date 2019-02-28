4ba5d67071d9f205a1146849e7225df36fa24bff
Jan Cavel <caveljan@gmail.com>
2019-02-28 10:59:51 +0200

    test: getDirection

---
+++ test/modules/getDirection.spec.ts
@@ -0,0 +1,35 @@
+import { getWheelDirection } from '../../src/modules/getDirection';
+
+describe('getWheelDirection', () => {
+    it('returns "left" direction', () => {
+        const event = {
+            deltaX: 20,
+            deltaY: 0
+        };
+        expect(getWheelDirection(event)).toBe('left');
+    });
+
+    it('returns "right" direction', () => {
+        const event = {
+            deltaX: -20,
+            deltaY: 0
+        };
+        expect(getWheelDirection(event)).toBe('right');
+    });
+
+    it('returns "up" direction', () => {
+        const event = {
+            deltaX: 0,
+            deltaY: 10
+        };
+        expect(getWheelDirection(event)).toBe('up');
+    });
+
+    it('returns "down" direction', () => {
+        const event = {
+            deltaX: 0,
+            deltaY: -10
+        };
+        expect(getWheelDirection(event)).toBe('down');
+    });
+});
