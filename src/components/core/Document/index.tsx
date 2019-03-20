40f2e553ec1b6417668d2af3c5405fd502d03b2b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-03-20 17:06:30 +0200

    dev: routing

---
+++ src/components/core/Document/index.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+class PluridDocument extends Component {
+    public render() {
+        const { children } = this.props;
+
+        return (
+            <React.Fragment>
+                {children}
+            </React.Fragment>
+        );
+    }
+}
+
+export default PluridDocument;
