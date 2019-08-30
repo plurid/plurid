2992407457aea2e52f7e0b875ee4cf40c8f96508
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-30 21:56:20 +0300

    generate-plurid-app > setup: basic

---
+++ packages/generate-plurid-app/src/inquire/index.ts
@@ -0,0 +1,9 @@
+import { inquire as _inquire } from './inquire';
+import { questions as _questions } from './questions';
+import { processArgs as _processArgs } from './processArgs';
+
+
+
+export const inquire = _inquire;
+export const questions = _questions;
+export const processArgs = _processArgs;
