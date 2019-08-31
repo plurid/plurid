ec4bc1afd99102adc7e9e23a8dbd12bd961f13cf
Jan Cavel <caveljan@gmail.com>
2019-08-31 17:06:43 +0300

    plurid-engine > setup: cleanup

--- packages/plurid-engine/src/modules/matrix.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function rotateMatrix(xAngle: number, yAngle: number, zAngle?: number): number[];
-export declare function translateMatrix(x: number, y: number, z: number): number[];
-export declare function scaleMatrix(s: number): number[];
-export declare function multiplyMatrices(matrixA: number[], matrixB: number[]): number[];
-export declare function multiplyArrayOfMatrices(matrices: number[][]): number[];
-export declare function matrixArrayToCSSMatrix(array: number[]): string;
