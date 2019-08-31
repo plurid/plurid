204342b27503c1d800c4ccf002f5933d700e2d2b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-31 14:20:06 +0300

    plurid-react > dev: Plane

--- packages/plurid-react/src/modules/components/PluridPlane/styled.ts
+++ packages/plurid-react/src/modules/components/PluridPlane/styled.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 export const StyledPluridPlane = styled.div`
     position: absolute;
-    height: 200px;
-    width: 500px;
+    height: auto;
+    min-width: 100%;
     background-color: hsla(220, 10%, 40%, 0.4);
 `;
