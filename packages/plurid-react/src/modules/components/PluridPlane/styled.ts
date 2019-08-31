47a917e41f4e9741f85e27c3b1f9ddba8e6c07c5
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-31 13:43:24 +0300

    plurid-react > dev: Space, Plane

--- packages/plurid-react/src/modules/components/PluridPlane/styled.ts
+++ packages/plurid-react/src/modules/components/PluridPlane/styled.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 
 
 export const StyledPluridPlane = styled.div`
+    position: absolute;
+    height: 200px;
+    width: 500px;
+    background-color: hsla(220, 10%, 40%, 0.4);
 `;
--- packages/plurid-react/src/modules/components/PluridSpace/styled.ts
+++ packages/plurid-react/src/modules/components/PluridSpace/styled.ts
@@ -3,5 +3,7 @@ import styled from 'styled-components';
 
 
 export const StyledPluridSpace = styled.div`
+    position: relative;
     height: 100%;
+    perspective: 1000px;
 `;
