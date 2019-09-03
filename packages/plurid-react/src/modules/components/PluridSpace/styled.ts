da78b1285af5b9c87ede7ae2beba03b8e7167ff9
Jan Cavel <caveljan@gmail.com>
2019-09-03 11:53:43 +0300

    plurid-react > dev: interfaces, Space

--- packages/plurid-react/src/modules/components/PluridSpace/styled.ts
+++ packages/plurid-react/src/modules/components/PluridSpace/styled.ts
@@ -6,4 +6,5 @@ export const StyledPluridSpace = styled.div`
     position: relative;
     height: 100%;
     perspective: 1000px;
+    background: radial-gradient(ellipse at center, #252727 0%, #111212 100%);
 `;
--- packages/plurid-react/src/modules/services/state/modules/configuration/reducers.ts
+++ packages/plurid-react/src/modules/services/state/modules/configuration/reducers.ts
@@ -7,6 +7,7 @@ import {
 
 
 const initialState: ConfigurationState = {
+    perspective: 1000,
     theme: 'plurid',
     alterURL: false,
     pluridPlane: {
