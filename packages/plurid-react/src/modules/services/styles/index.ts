81bc5e15c8ae93029a0ce43aebf2958a56f8de67
Jan Cavel <caveljan@gmail.com>
2019-09-05 21:21:04 +0300

    plurid-react > dev: services > styles, PluridPlane > PlaneControls

---
+++ packages/plurid-react/src/modules/services/styles/index.ts
@@ -0,0 +1,18 @@
+import {
+    StyledButton,
+    StyledLinkButton,
+} from './buttons';
+
+import {
+    StyledInputText,
+    StyledSwitch,
+} from './inputs';
+
+
+
+export default {
+    Button: StyledButton,
+    LinkButton: StyledLinkButton,
+    InputText: StyledInputText,
+    Switch: StyledSwitch,
+}
