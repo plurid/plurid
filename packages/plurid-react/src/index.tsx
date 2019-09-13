5758dcfdfd2cce3d8abd5a763abd9cf5b15e7dcb
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-13 10:08:55 +0300

    plurid-react > dev: index

--- packages/plurid-react/src/index.tsx
+++ packages/plurid-react/src/index.tsx
@@ -1,11 +1,19 @@
 import PluridApp from './App';
 
 import PluridLink from './modules/components/PluridLink';
+import {
+    PluridPage,
+    PluridDocument,
+    PluridAppConfiguration,
+} from './modules/data/interfaces'
 
 
 
 export {
     PluridLink,
+    PluridPage,
+    PluridDocument,
+    PluridAppConfiguration,
 };
 
 export default PluridApp;
