298a8822fa62d7c755df7044d146d4b21cb4cdfe
Jan Cavel <caveljan@gmail.com>
2019-09-14 15:01:38 +0300

    setup: cleanup

--- packages/micro-plurid-react/src/components/core/Sheet/test.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import PluridSheet from '.';
-
-describe('PluridSheet', () => {
-    it('renders without crashing', () => {
-        shallow(<PluridSheet />);
-    });
-});
