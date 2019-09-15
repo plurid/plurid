04217628214ae3bd38c9b4738940b10e9f06cea2
Jan Cavel <caveljan@gmail.com>
2019-09-15 14:16:04 +0300

    plurid-react > setup: package

--- packages/plurid-react/src/index.tsx
+++ packages/plurid-react/src/index.tsx
@@ -1,20 +1,30 @@
-import PluridApp from './App';
-
-import PluridLink from './modules/components/PluridLink';
-
 import {
     PluridPage,
     PluridDocument,
     PluridConfiguration,
 } from '@plurid/plurid-data';
 
+import PluridPubSub, { TOPICS } from '@plurid/plurid-pubsub';
+
+import PluridLink from './modules/components/PluridLink';
+
+import PluridApp from './App';
+
 
 
 export {
-    PluridLink,
+    // interfaces
     PluridPage,
     PluridDocument,
     PluridConfiguration,
+
+    // pubsub
+    PluridPubSub,
+    TOPICS,
+
+    // components
+    PluridLink,
 };
+
 
 export default PluridApp;
