5f4dfd76cfa520d29bf76aa202af2bb1a26416be
Jan Cavel <caveljan@gmail.com>
2019-09-15 13:48:22 +0300

    plurid-pubsub > dev: basic pubsub

--- packages/plurid-pubsub/src/objects/PluridPubSub/__specs__/index.spec.ts
+++ packages/plurid-pubsub/src/objects/PluridPubSub/__specs__/index.spec.ts
@@ -3,16 +3,19 @@ import PluridPubSub from '../';
 
 
 describe('PluridPubSub', () => {
-    it('works', () => {
-        // const pluridPubSub = new PluridPubSub();
+    it('publishes and subscribes', () => {
+        const pluridPubSub = new PluridPubSub();
 
-        // pluridPubSub.publish('space.increase.rotateY', { value: 1 });
-        // pluridPubSub.subscribe('space.increase.rotateY', (data) => {
-        //     const {
-        //         value
-        //     } = data;
-        //     // increase the rotateY with value
-        // });
+        pluridPubSub.subscribe('space.increase.rotateY', (data: any) => {
+            const {
+                value,
+            } = data;
+            // increase the rotateY with value
+            console.log(value);
+        });
 
+        pluridPubSub.publish('space.increase.rotateY', { value: 1 });
+
+        expect(true).toBeTruthy();
     });
 });
