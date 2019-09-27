1fe18239d62632bbff83d3b6f2934d3e7e3b65e0
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-27 16:35:49 +0300

    plurid-react > setup: package

--- packages/plurid-react/src/modules/services/state/store/store.development.ts
+++ packages/plurid-react/src/modules/services/state/store/store.development.ts
@@ -3,7 +3,7 @@ import {
     applyMiddleware,
 } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import rootReducer from '../reducers';
 
 
