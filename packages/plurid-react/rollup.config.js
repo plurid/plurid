aea0354fda72ade6a5c3c49d7cda50927128a216
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-27 14:16:42 +0300

    plurid-react > setup: cleanup

--- packages/plurid-react/rollup.config.js
+++ packages/plurid-react/rollup.config.js
@@ -4,7 +4,6 @@ import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from 'rollup-plugin-node-resolve';
 import url from 'rollup-plugin-url';
-import svgr from '@svgr/rollup';
 import replace from 'rollup-plugin-replace';
 import babel from 'rollup-plugin-babel';
 
@@ -21,12 +20,12 @@ export default {
             exports: 'named',
             sourcemap: true
         },
-        // {
-        //     file: pkg.module,
-        //     format: 'es',
-        //     exports: 'named',
-        //     sourcemap: true
-        // }
+        {
+            file: pkg.module,
+            format: 'es',
+            exports: 'named',
+            sourcemap: true
+        }
     ],
     plugins: [
         replace({
@@ -37,7 +36,6 @@ export default {
             modules: true
         }),
         url(),
-        svgr(),
         babel({
             exclude: "node_modules/**"
         }),
