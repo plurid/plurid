733057fca6168032ff03c5761bf3604a166ad271
Jan Cavel <caveljan@gmail.com>
2019-10-11 10:03:59 +0300

    plurid-react > dev: PluridRoot

--- fixtures/plurid-react-default/src/App.jsx
+++ fixtures/plurid-react-default/src/App.jsx
@@ -112,27 +112,35 @@ const App = () => {
         {
             path: '/page-1',
             component: {
-                element: Page1,
+                element: () => (<Page1 />),
                 properties: {},
             },
             root: true,
         },
-        {
-            path: '/page-2',
-            component: {
-                element: Page2,
-                properties: {},
-            },
-            root: true,
-        },
-        {
-            path: '/page-3',
-            component: {
-                element: Page1,
-                properties: {},
-            },
-            root: true,
-        },
+        // {
+        //     path: '/page-1',
+        //     component: {
+        //         element: Page1,
+        //         properties: {},
+        //     },
+        //     root: true,
+        // },
+        // {
+        //     path: '/page-2',
+        //     component: {
+        //         element: Page2,
+        //         properties: {},
+        //     },
+        //     root: true,
+        // },
+        // {
+        //     path: '/page-3',
+        //     component: {
+        //         element: Page1,
+        //         properties: {},
+        //     },
+        //     root: true,
+        // },
         // {
         //     path: '/page-4',
         //     component: {
