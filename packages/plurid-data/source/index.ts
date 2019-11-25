a2a2d9977d193b102e8d5dd51d129055c3332893
Jan Cavel <caveljan@gmail.com>
2019-11-25 10:47:21 +0200

    plurid-data > setup: package

---
+++ packages/plurid-data/source/index.ts
@@ -0,0 +1,11 @@
+export * from './constants/space';
+export * from './constants/keys';
+export * from './constants/shortcuts';
+export * from './constants/configuration';
+
+
+export * from './enumerations';
+
+
+export * from './interfaces/external';
+export * from './interfaces/internal';
