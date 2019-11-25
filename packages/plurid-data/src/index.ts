ba0103e284d60c880348cb0d6380a87e157d5f71
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-11-25 10:43:08 +0200

    plurid-data > setup: package

--- packages/plurid-data/src/index.ts
+++ packages/plurid-data/src/index.ts
@@ -1,92 +1,105 @@
-import {
-    ROTATION_STEP,
-    TRANSLATION_STEP,
-    SCALE_STEP,
-    SCALE_LOWER_LIMIT,
-    SCALE_UPPER_LIMIT,
-    ROOTS_GAP,
-    PLANE_DEFAULT_ANGLE,
-} from './constants/space';
+// import {
+//     ROTATION_STEP,
+//     TRANSLATION_STEP,
+//     SCALE_STEP,
+//     SCALE_LOWER_LIMIT,
+//     SCALE_UPPER_LIMIT,
+//     ROOTS_GAP,
+//     PLANE_DEFAULT_ANGLE,
+// } from './constants/space';
 
-import {
-    KEY_MODIFIERS,
-} from './constants/keys';
+export * from './constants/space';
+export * from './constants/keys';
+export * from './constants/shortcuts';
+export * from './constants/configuration';
 
-import {
-    shortcutsNames,
-    defaultShortcuts,
-} from './constants/shortcuts';
-import defaultConfiguration from './constants/configuration';
+// import {
+//     KEY_MODIFIERS,
+// } from './constants/keys';
 
-import {
-    SHORTCUTS,
-} from './enumerations';
+// import {
+//     shortcutsNames,
+//     defaultShortcuts,
+// } from './constants/shortcuts';
 
-import {
-    PluridApp,
-    PluridPage,
-    PluridPageContext,
-    PluridComponentReact,
-    PluridDocument,
-    PluridLink,
-    PluridConfiguration,
-    PluridConfigurationTheme,
-    PluridConfigurationSpace,
-    LayoutColumns,
-    LayoutFaceToFace,
-    LayoutSheaves,
-} from './interfaces/external';
+// import defaultConfiguration from './constants/configuration';
 
-import {
-    PluridContext,
-    TreePage,
-    TreePageLocation,
-    SpaceLocation,
-    LocationCoordinates,
-} from './interfaces/internal';
+// import {
+//     SHORTCUTS,
+// } from './enumerations';
+
+export * from './enumerations';
+
+// import {
+//     PluridApp,
+//     PluridPage,
+//     PluridPageContext,
+//     PluridComponentReact,
+//     PluridDocument,
+//     PluridLink,
+//     PluridConfiguration,
+//     PluridConfigurationTheme,
+//     PluridConfigurationSpace,
+//     LayoutColumns,
+//     LayoutFaceToFace,
+//     LayoutSheaves,
+// } from './interfaces/external';
+
+// import {
+//     PluridContext,
+//     Tree,
+//     TreePage,
+//     TreePageLocation,
+//     SpaceLocation,
+//     LocationCoordinates,
+// } from './interfaces/internal';
+
+export * from './interfaces/external';
+export * from './interfaces/internal';
 
 
 
 export {
-    // constants
-    ROTATION_STEP,
-    TRANSLATION_STEP,
-    SCALE_STEP,
-    SCALE_LOWER_LIMIT,
-    SCALE_UPPER_LIMIT,
-    ROOTS_GAP,
-    PLANE_DEFAULT_ANGLE,
+    /** constants */
+    // ROTATION_STEP,
+    // TRANSLATION_STEP,
+    // SCALE_STEP,
+    // SCALE_LOWER_LIMIT,
+    // SCALE_UPPER_LIMIT,
+    // ROOTS_GAP,
+    // PLANE_DEFAULT_ANGLE,
 
-    KEY_MODIFIERS,
+    // KEY_MODIFIERS,
 
-    shortcutsNames,
-    defaultShortcuts,
-    defaultConfiguration,
-
-
-    // enumerations
-    SHORTCUTS,
+    // shortcutsNames,
+    // defaultShortcuts,
+    // defaultConfiguration,
 
 
-    // interfaces
-    // external
-    PluridApp,
-    PluridPage,
-    PluridPageContext,
-    PluridComponentReact,
-    PluridDocument,
-    PluridLink,
-    PluridConfiguration,
-    PluridConfigurationTheme,
-    PluridConfigurationSpace,
-    LayoutColumns,
-    LayoutFaceToFace,
-    LayoutSheaves,
+    // /** enumerations */
+    // SHORTCUTS,
 
-    // internal
-    PluridContext,
-    TreePageLocation,
-    TreePage,
-    SpaceLocation,
-    LocationCoordinates,
+
+    // /** interfaces */
+    // /** external */
+    // PluridApp,
+    // PluridPage,
+    // PluridPageContext,
+    // PluridComponentReact,
+    // PluridDocument,
+    // PluridLink,
+    // PluridConfiguration,
+    // PluridConfigurationTheme,
+    // PluridConfigurationSpace,
+    // LayoutColumns,
+    // LayoutFaceToFace,
+    // LayoutSheaves,
+
+    // /** internal */
+    // PluridContext,
+    // TreePageLocation,
+    // Tree,
+    // TreePage,
+    // SpaceLocation,
+    // LocationCoordinates,
 };
