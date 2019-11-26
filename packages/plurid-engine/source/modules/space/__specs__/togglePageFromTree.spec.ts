08030fd69192ede85adaba5b37dc37074d086ea7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-11-26 15:12:04 +0200

    plurid-engine > spec: togglePageFromTree

--- packages/plurid-engine/source/modules/space/__specs__/togglePageFromTree.spec.ts
+++ packages/plurid-engine/source/modules/space/__specs__/togglePageFromTree.spec.ts
@@ -18,7 +18,7 @@ const location = {
 
 
 describe('togglePageFromTree', () => {
-    it.only('toggle page', () => {
+    it('toggle page level one child', () => {
         const tree: TreePage[] = [
             {
                 pageID: '1',
@@ -78,6 +78,86 @@ describe('togglePageFromTree', () => {
         const togglePageID = 'bbb';
 
         const result = togglePageFromTree(tree, togglePageID);
+        expect(result).toMatchObject(updatedTree);
+    });
+
+    it.only('toggle page', () => {
+        const tree: TreePage[] = [
+            {
+                pageID: '1',
+                planeID: 'aaa',
+                path: '/aaa',
+                location,
+                show: true,
+                children: [
+                    {
+                        pageID: '2',
+                        planeID: 'bbb',
+                        path: '/aaa/bbb',
+                        location,
+                        show: true,
+                        children: [],
+                    }
+                ],
+            },
+            {
+                pageID: '3',
+                planeID: 'ccc',
+                path: '/ccc',
+                location,
+                show: true,
+                children: [],
+            },
+            {
+                pageID: '4',
+                planeID: 'ddd',
+                path: '/ddd',
+                location,
+                show: true,
+                children: [],
+            },
+        ];
+
+        const updatedTree: TreePage[] = [
+            {
+                pageID: '1',
+                planeID: 'aaa',
+                path: '/aaa',
+                location,
+                show: true,
+                children: [
+                    {
+                        pageID: '2',
+                        planeID: 'bbb',
+                        path: '/aaa/bbb',
+                        location,
+                        show: false,
+                        children: [],
+                    }
+                ],
+            },
+            {
+                pageID: '3',
+                planeID: 'ccc',
+                path: '/ccc',
+                location,
+                show: true,
+                children: [],
+            },
+            {
+                pageID: '4',
+                planeID: 'ddd',
+                path: '/ddd',
+                location,
+                show: true,
+                children: [],
+            },
+        ];
+
+        const togglePageID = 'bbb';
+
+        const result = togglePageFromTree(tree, togglePageID);
+
         expect(result).toMatchObject(updatedTree);
     });
 });
