ea8f817329650291c29629e8a669d13b9f89d151
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-11-26 20:07:10 +0200

    plurid-react > dev: Toolbar

--- packages/plurid-react/source/modules/components/Toolbar/General/styled.ts
+++ packages/plurid-react/source/modules/components/Toolbar/General/styled.ts
@@ -40,21 +40,39 @@ export const StyledToolbarRotate: any = styled.div`
     display: grid;
     align-items: center;
     justify-items: center;
-    grid-template-columns: 0.5fr 0.5fr 1fr 0.5fr 0.5fr;
+    grid-template-columns: ${(props: any) => {
+        if (props.showTransformButtons) {
+            return '1fr';
+        }
+
+        return '0.5fr 0.5fr 1fr 0.5fr 0.5fr';
+    }};
 `;
 
 export const StyledToolbarTranslate: any = styled.div`
     display: grid;
     align-items: center;
     justify-items: center;
-    grid-template-columns: 0.5fr 0.5fr 1fr 0.5fr 0.5fr;
+    grid-template-columns: ${(props: any) => {
+        if (props.showTransformButtons) {
+            return '1fr';
+        }
+
+        return '0.5fr 0.5fr 1fr 0.5fr 0.5fr';
+    }};
 `;
 
 export const StyledToolbarScale: any = styled.div`
     display: grid;
     align-items: center;
     justify-items: center;
-    grid-template-columns: 0.5fr 1fr 0.5fr;
+    grid-template-columns: ${(props: any) => {
+        if (props.showTransformButtons) {
+            return '1fr';
+        }
+
+        return '0.5fr 1fr 0.5fr';
+    }};
 `;
 
 
