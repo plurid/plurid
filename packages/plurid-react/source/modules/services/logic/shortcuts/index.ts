2fb99522014c8d647ea9ccca8708ed7ae67ee222
Jan Cavel <caveljan@gmail.com>
2019-11-29 15:05:15 +0200

    plurid-react > dev: logic > shortcuts

--- packages/plurid-react/source/modules/services/logic/shortcuts/index.ts
+++ packages/plurid-react/source/modules/services/logic/shortcuts/index.ts
@@ -23,6 +23,93 @@ export const handleGlobalShortcuts = (
         && !event.ctrlKey
         && !event.metaKey;
 
+
+
+    if (
+        event.code === 'KeyW'
+        && noModifiers
+    ) {
+        console.log('move forward');
+        dispatch(actions.space.viewCameraMoveForward());
+    }
+
+    if (
+        event.code === 'KeyS'
+        && noModifiers
+    ) {
+        console.log('move backward');
+        dispatch(actions.space.viewCameraMoveBackward());
+    }
+
+
+    if (
+        event.code === 'KeyA'
+        && noModifiers
+    ) {
+        console.log('move left');
+        dispatch(actions.space.viewCameraMoveLeft());
+    }
+
+    if (
+        event.code === 'KeyA'
+        && event.shiftKey
+    ) {
+        console.log('turn left');
+        dispatch(actions.space.viewCameraTurnLeft());
+    }
+
+
+    if (
+        event.code === 'KeyD'
+        && noModifiers
+    ) {
+        console.log('move right');
+        dispatch(actions.space.viewCameraMoveRight());
+    }
+
+    if (
+        event.code === 'KeyD'
+        && event.shiftKey
+    ) {
+        console.log('turn right');
+        dispatch(actions.space.viewCameraTurnRight());
+    }
+
+
+    if (
+        event.code === 'KeyQ'
+        && noModifiers
+    ) {
+        console.log('turn up');
+        dispatch(actions.space.viewCameraTurnUp());
+    }
+
+    if (
+        event.code === 'KeyZ'
+        && noModifiers
+    ) {
+        console.log('turn down');
+        dispatch(actions.space.viewCameraTurnDown());
+    }
+
+
+    if (
+        event.code === 'KeyE'
+        && noModifiers
+    ) {
+        console.log('move up');
+        dispatch(actions.space.viewCameraMoveUp());
+    }
+
+    if (
+        event.code === 'KeyC'
+        && noModifiers
+    ) {
+        console.log('move down');
+        dispatch(actions.space.viewCameraMoveDown());
+    }
+
+
     if (
         event.code === 'KeyR'
         && noModifiers
