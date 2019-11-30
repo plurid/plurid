ff99b1325271b4954c5d5edfafadb596b2d214cc
Jan Cavel <caveljan@gmail.com>
2019-11-30 10:21:30 +0200

    plurid-react > dev: PluridRoots

--- packages/plurid-react/source/modules/components/PluridRoots/styled.ts
+++ packages/plurid-react/source/modules/components/PluridRoots/styled.ts
@@ -5,3 +5,12 @@ import styled from 'styled-components';
 export const StyledPluridRoots = styled.div`
     transform-style: preserve-3d;
 `;
+
+
+export const StyledTransformOrigin = styled.div`
+    position: absolute;
+    height: 20px;
+    width: 20px;
+    background: red;
+    z-index: 999;
+`;
