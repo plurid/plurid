a35b52f0c40a782b10fd429b5b5d116fdec1eea5
Jan Cavel <caveljan@gmail.com>
2019-11-30 13:39:00 +0200

    plurid-react > dev: PluridRoots

--- packages/plurid-react/source/modules/components/PluridRoots/styled.ts
+++ packages/plurid-react/source/modules/components/PluridRoots/styled.ts
@@ -7,14 +7,36 @@ export const StyledPluridRoots = styled.div`
 `;
 
 
-export const StyledTransformOrigin = styled.div`
+export const StyledTransformOrigin: any = styled.div`
     background-color: ${(props: any) => {
         return props.theme.backgroundColorTertiaryAlpha;
     }};
+    height: ${(props: any) => {
+        switch (props.transformOriginSize) {
+            case 'small':
+                return '5px';
+            case 'normal':
+                return '10px';
+            case 'large':
+                return '15px';
+            default:
+                return '10px';
+        }
+    }};
+    width: ${(props: any) => {
+        switch (props.transformOriginSize) {
+            case 'small':
+                return '5px';
+            case 'normal':
+                return '10px';
+            case 'large':
+                return '15px';
+            default:
+                return '10px';
+        }
+    }};
 
     position: absolute;
-    height: 15px;
-    width: 15px;
     z-index: 999;
     border-radius: 100px;
     user-select: none;
