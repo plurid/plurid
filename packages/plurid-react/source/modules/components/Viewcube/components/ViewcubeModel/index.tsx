dcae29b5b981e69873f075a120825561839f879e
Jan Cavel <caveljan@gmail.com>
2019-12-02 17:06:17 +0200

    plurid-react > dev: Viewcube

---
+++ packages/plurid-react/source/modules/components/Viewcube/components/ViewcubeModel/index.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { AnyAction } from 'redux';
+import { connect } from 'react-redux';
+import { ThunkDispatch } from 'redux-thunk';
+
+import {
+    StyledViewcubeModel,
+    StyleViewcubeModelContainer,
+    StyledViewcubeModelCube,
+} from './styled';
+
+import ViewcubeFace from '../ViewcubeFace';
+
+import { AppState } from '../../../../services/state/store';
+import StateContext from '../../../../services/state/context';
+import selectors from '../../../../services/state/selectors';
+// import actions from '../../../../services/state/actions';
+
+
+
+
+interface ViewcubeModelOwnProperties {
+}
+
+interface ViewcubeModelStateProperties {
+    spaceRotationX: number;
+    spaceRotationY: number;
+}
+
+interface ViewcubeModelDispatchProperties {
+}
+
+type ViewcubeModelProperties = ViewcubeModelOwnProperties
+    & ViewcubeModelStateProperties
+    & ViewcubeModelDispatchProperties;
+
+const ViewcubeModel: React.FC<ViewcubeModelProperties> = (properties) => {
+    const {
+        /** own */
+
+        /** state */
+        spaceRotationX,
+        spaceRotationY,
+
+        /** dispatch */
+    } = properties;
+
+    return (
+        <StyledViewcubeModel>
+            <StyleViewcubeModelContainer>
+                <StyledViewcubeModelCube
+                    style={{
+                        transform: `
+                            translateX(32px)
+                            translateY(23px)
+                            rotateX(${spaceRotationX}deg)
+                            rotateY(${spaceRotationY}deg)
+                        `,
+                        transition: 'transform 450ms ease-in-out',
+                    }}
+                >
+                    <ViewcubeFace
+                        face="front"
+                    />
+                    <ViewcubeFace
+                        face="back"
+                    />
+                    <ViewcubeFace
+                        face="left"
+                    />
+                    <ViewcubeFace
+                        face="right"
+                    />
+                    <ViewcubeFace
+                        face="top"
+                    />
+                    <ViewcubeFace
+                        face="base"
+                    />
+                </StyledViewcubeModelCube>
+            </StyleViewcubeModelContainer>
+        </StyledViewcubeModel>
+    );
+}
+
+
+const mapStateToProperties = (
+    state: AppState,
+): ViewcubeModelStateProperties => ({
+    spaceRotationX: selectors.space.getRotationX(state),
+    spaceRotationY: selectors.space.getRotationY(state),
+});
+
+
+const mapDispatchToProperties = (
+    dispatch: ThunkDispatch<{}, {}, AnyAction>,
+): ViewcubeModelDispatchProperties => ({
+});
+
+
+export default connect(
+    mapStateToProperties,
+    mapDispatchToProperties,
+    null,
+    {
+        context: StateContext,
+    },
+)(ViewcubeModel);
--- packages/plurid-react/source/modules/components/Viewcube/index.tsx
+++ packages/plurid-react/source/modules/components/Viewcube/index.tsx
@@ -4,22 +4,13 @@ import { connect } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 
 import {
-    Theme,
-} from '@plurid/plurid-themes';
-
-import {
     StyledViewcube,
-    StyledViewcubeModel,
-    StyleViewcubeModelContainer,
-    StyledViewcubeModelCube,
 } from './styled';
 
-import ViewcubeFace from './components/ViewcubeFace';
+import ViewcubeModel from './components/ViewcubeModel';
 
 import { AppState } from '../../services/state/store';
 import StateContext from '../../services/state/context';
-import selectors from '../../services/state/selectors';
-// import actions from '../../services/state/actions';
 
 
 
@@ -27,9 +18,6 @@ interface ViewcubeOwnProperties {
 }
 
 interface ViewcubeStateProperties {
-    generalTheme: Theme;
-    spaceRotationX: number;
-    spaceRotationY: number;
 }
 
 interface ViewcubeDispatchProperties {
@@ -39,52 +27,10 @@ type ViewcubeProperties = ViewcubeOwnProperties
     & ViewcubeStateProperties
     & ViewcubeDispatchProperties;
 
-const Viewcube: React.FC<ViewcubeProperties> = (properties) => {
-    const {
-        /** state */
-        generalTheme,
-        spaceRotationX,
-        spaceRotationY,
-    } = properties;
-
+const Viewcube: React.FC<ViewcubeProperties> = () => {
     return (
-        <StyledViewcube
-            theme={generalTheme}
-        >
-            <StyledViewcubeModel>
-                <StyleViewcubeModelContainer>
-                    <StyledViewcubeModelCube
-                        style={{
-                            transform: `
-                                translateX(32px)
-                                translateY(23px)
-                                rotateX(${spaceRotationX}deg)
-                                rotateY(${spaceRotationY}deg)
-                            `,
-                            transition: 'transform 450ms ease-in-out',
-                        }}
-                    >
-                        <ViewcubeFace
-                            face="front"
-                        />
-                        <ViewcubeFace
-                            face="back"
-                        />
-                        <ViewcubeFace
-                            face="left"
-                        />
-                        <ViewcubeFace
-                            face="right"
-                        />
-                        <ViewcubeFace
-                            face="top"
-                        />
-                        <ViewcubeFace
-                            face="base"
-                        />
-                    </StyledViewcubeModelCube>
-                </StyleViewcubeModelContainer>
-            </StyledViewcubeModel>
+        <StyledViewcube>
+            <ViewcubeModel />
         </StyledViewcube>
     );
 }
@@ -93,9 +39,6 @@ const Viewcube: React.FC<ViewcubeProperties> = (properties) => {
 const mapStateToProperties = (
     state: AppState,
 ): ViewcubeStateProperties => ({
-    generalTheme: selectors.themes.getGeneralTheme(state),
-    spaceRotationX: selectors.space.getRotationX(state),
-    spaceRotationY: selectors.space.getRotationY(state),
 });
 
 
