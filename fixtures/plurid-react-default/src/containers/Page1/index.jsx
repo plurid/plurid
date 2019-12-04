e8adfc7ed95838f8ebcc0c992ed45713599e3536
Jan Cavel <caveljan@gmail.com>
2019-12-04 10:19:50 +0200

    plurid-react > dev: App, shortcuts, contentEditable

--- fixtures/plurid-react-default/src/containers/Page1/index.jsx
+++ fixtures/plurid-react-default/src/containers/Page1/index.jsx
@@ -7,6 +7,14 @@ import { PluridLink } from '@plurid/plurid-react';
 const Page1 = () => {
     return (
         <div style={{height: 300}}>
+            <div
+                tabIndex={0}
+                contentEditable={true}
+                suppressContentEditableWarning={true}
+            >
+                editable
+            </div>
+
             page 1
 
             <input />
