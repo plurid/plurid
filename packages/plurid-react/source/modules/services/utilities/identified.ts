b6398d74226c246a1bdfed8014a5a2981d260a8e
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-05 12:18:26 +0200

    plurid-react > dev: utilities > identified

--- packages/plurid-react/source/modules/services/utilities/identified.ts
+++ packages/plurid-react/source/modules/services/utilities/identified.ts
@@ -1,15 +1,40 @@
 import {
+    uuidv4 as uuid,
+} from '@plurid/plurid-functions';
+
+import {
     PluridPage,
     PluridDocument,
+
+    Identified,
 } from '@plurid/plurid-data';
 
 
 
-export const identifyPages = () => {
-
+export const identifyPages = (
+    pages: PluridPage[],
+): Identified<PluridPage>[] => {
+    const identifiedPages = pages.map(page => {
+        const updatedPage: Identified<PluridPage> = {
+            ...page,
+            id: page.id || uuid(),
+        };
+        return updatedPage;
+    });
+    return identifiedPages;
 }
 
 
-export const identifyDocuments = () => {
-
+export const identifyDocuments = (
+    documents: PluridDocument[],
+): Identified<PluridDocument>[] => {
+    const identifiedDocuments = documents.map(document => {
+        const updatedDocument: Identified<PluridDocument> = {
+            ...document,
+            pages: identifyPages(document.pages),
+            id: document.id || uuid(),
+        };
+        return updatedDocument;
+    });
+    return identifiedDocuments;
 }
