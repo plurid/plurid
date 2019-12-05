528831a9eb9db7fe002bcd11fc55db876482e5be
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-05 12:59:45 +0200

    plurid-react > dev: Toolbar, assets documents

--- packages/plurid-react/source/modules/assets/icons/more-icon.tsx
+++ packages/plurid-react/source/modules/assets/icons/more-icon.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const SVG = (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
-        <title>More</title>
+        <title>more</title>
 
         <g>
             <rect x="380" width="240" height="240" rx="50"/>
