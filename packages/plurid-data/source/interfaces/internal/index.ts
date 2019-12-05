7771ecaf6c5018dad911ebc05b45f487af7c1757
Jan Cavel <caveljan@gmail.com>
2019-12-05 11:53:51 +0200

    plurid-data > dev: internal interfaces, identified

--- packages/plurid-data/source/interfaces/internal/index.ts
+++ packages/plurid-data/source/interfaces/internal/index.ts
@@ -9,6 +9,12 @@ export interface Indexed<T> {
     [key: string]: T;
 }
 
+export interface WithID {
+    id: string;
+}
+
+export type Identified<T> = T & WithID;
+
 
 export interface PluridInternalPage {
     id: string;
