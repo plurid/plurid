cd20a694ed5fbe2e39071d54e27ce7389fc2cdd7
Jan Cavel <caveljan@gmail.com>
2019-12-05 11:01:26 +0200

    plurid-react > dev: utilities

--- packages/plurid-react/source/modules/services/utilities/documents.ts
+++ packages/plurid-react/source/modules/services/utilities/documents.ts
@@ -1,7 +1,20 @@
 import {
+    uuidv4 as uuid,
+} from '@plurid/plurid-functions';
+
+import {
     PluridPage,
     PluridDocument,
+
+    PluridInternalStateDocument,
+    PluridInternalContextDocument,
 } from '@plurid/plurid-data';
+
+import {
+    createInternalStatePage,
+    createInternalContextPage,
+} from './pages';
+
 
 
 /**
@@ -21,10 +34,42 @@ const createDefaultDocument = (
 
 
 /**
- * from PluridDocument create PluridInternalDocument
+ * from PluridDocument create PluridInternalStateDocument
  */
-const createInternalDocument = (
-    document: PluridDocument
-) => {
+const createInternalStateDocument = (
+    document: PluridDocument,
+): PluridInternalStateDocument => {
+    const statePages = document.pages.map(page => {
+        const internalStatePage = createInternalStatePage(page);
+        return internalStatePage;
+    });
 
+    const stateDocument: PluridInternalStateDocument = {
+        name: document.name,
+        pages: statePages,
+        id: document.id || uuid(),
+        ordinal: document.ordinal || 0,
+    }
+
+    return stateDocument;
+}
+
+/**
+ * from PluridDocument create PluridInternalStateDocument
+ */
+const createInternalContextDocument = (
+    document: PluridDocument,
+): PluridInternalContextDocument => {
+    const contextPages = document.pages.map(page => {
+        const internalStatePage = createInternalStatePage(page);
+        return internalStatePage;
+    });
+
+    const contextDocument: PluridInternalContextDocument = {
+        name: document.name,
+        pages: contextPages,
+        id: document.id || uuid(),
+    }
+
+    return contextDocument;
 }
