b0ca5664c6f1a03e27ad5d5a596839e201923841
Jan Cavel <caveljan@gmail.com>
2019-12-07 20:03:16 +0200

    plurid-react > dev: PluridVirtual, partial rendering

--- packages/plurid-react/source/modules/components/PluridVirtual/List/index.tsx
+++ packages/plurid-react/source/modules/components/PluridVirtual/List/index.tsx
@@ -1,34 +1,116 @@
-import React from 'react';
+import React, {
+    useRef,
+    useEffect,
+    useState,
+} from 'react';
 
+
+
+const sumTo = (
+    values: number[],
+    index: number,
+) => {
+    const _values = values.slice(0, index === 0 ? 1 : index);
+    return _values.reduce((total, val) => total + val);
+}
+
+
+interface VirtualListItemOwnProperties {
+    top: number;
+    index: number;
+    element: JSX.Element;
+    setHeight: any;
+}
+
+const VirtualListItem: React.FC<VirtualListItemOwnProperties> = (properties) => {
+    const {
+        top,
+        index,
+        element,
+        setHeight,
+    } = properties;
+
+    const el = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (el.current) {
+            setHeight(el.current.offsetHeight, index);
+        }
+    }, [
+        el.current,
+    ])
+
+    return (
+        <div
+            ref={el}
+            style={{
+                position: 'absolute',
+                top: top + 'px',
+            }}
+        >
+            {element}
+        </div>
+    );
+}
 
 
 interface PluridVirtualListOwnProperties {
     items: JSX.Element[];
+    generalHeight?: number;
 }
 
 const PluridVirtualList: React.FC<PluridVirtualListOwnProperties> = (properties) => {
     const {
         items,
+        generalHeight,
     } = properties;
 
+    const _generalHeight = generalHeight || 50;
+
+    const [start, setStart] = useState(0);
+    const [end, setEnd] = useState(Math.floor(1200 / _generalHeight));
+
+    const rows = useRef<any[]>([]);
+    const heights = useRef<number[]>(Array(end - start).fill(0));
+
     const renderRows = () => {
-        return (
-            <>
-                {items.map((element, index) => {
-                    return (
-                        <div
-                            key={index}
-                        >
-                            {element}
-                        </div>
-                    );
-                })}
-            </>
-        );
+        rows.current = [];
+        // heights.current= Array(end - start).fill(0);
+        console.log('foo', heights.current);
+
+        for (let i = start; i <= end; i++) {
+            let item = items[i];
+            rows.current.push(
+                <VirtualListItem
+                    key={i + Math.random()}
+                    index={i}
+                    top={sumTo(heights.current, i)}
+                    // top={20}
+                    element={item}
+                    setHeight={setHeight}
+                />
+            );
+        }
+        return rows.current;
     }
 
+    const setHeight = (
+        value: number,
+        index: number,
+    ) => {
+        heights.current[index] = value;
+    }
+
+    console.log(rows.current);
+    console.log(heights.current);
+
     return (
-        <div>
+        <div
+            style={{
+                height: sumTo(heights.current, heights.current.length),
+            }}
+            onClick={() => console.log(rows.current)}
+        >
             {renderRows()}
         </div>
     );
