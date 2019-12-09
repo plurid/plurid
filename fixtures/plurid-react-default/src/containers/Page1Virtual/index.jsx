9dc6df710cc45b0ed6207db5d890da8f9ccff802
Jan Cavel <caveljan@gmail.com>
2019-12-09 12:32:24 +0200

    plurid-react > fixture: PageVirtual

--- fixtures/plurid-react-default/src/containers/Page1Virtual/index.jsx
+++ fixtures/plurid-react-default/src/containers/Page1Virtual/index.jsx
@@ -11,7 +11,7 @@ const data = [];
 const renderData = () => {
     const text = 'Aliquam pulvinar commodo libero sit amet vulputate. Donec pulvinar sem sed eleifend elementum. Morbi ac rhoncus ipsum. Morbi accumsan dolor vitae tristique pulvinar. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec non condimentum quam. Maecenas sit amet dolor auctor, fringilla risus id, scelerisque tortor. Praesent maximus nibh sed risus finibus scelerisque. Suspendisse vestibulum dictum interdum. In odio mauris, congue sed felis in, molestie viverra mauris. Proin faucibus fermentum elit non accumsan. Curabitur sed porta nisi. Phasellus viverra ultrices libero. Duis commodo vel risus sit amet congue. Curabitur iaculis tortor quis sem aliquam varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quam enim, facilisis eu mauris ac, convallis consectetur turpis. Curabitur venenatis tincidunt tincidunt.';
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 1000; i++) {
         const el = (
             <div
                 key={i}
