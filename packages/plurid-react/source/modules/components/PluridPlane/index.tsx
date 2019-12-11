2ada0a6018fd2b1fcce0a5de968313d4f53cc00a
Jan Cavel <caveljan@gmail.com>
2019-12-11 12:22:41 +0200

    plurid-react > dev: PluridPlane

--- packages/plurid-react/source/modules/components/PluridPlane/index.tsx
+++ packages/plurid-react/source/modules/components/PluridPlane/index.tsx
@@ -1,7 +1,4 @@
-import React, {
-    // useState,
-    // useEffect,
-} from 'react';
+import React from 'react';
 import { AnyAction } from 'redux';
 import { connect } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
