b56a9e29f4a5f11ad5e7c64e797e7696298ff264
Jan Cavel <caveljan@gmail.com>
2019-12-12 11:52:33 +0200

    plurid-react > dev: Toolbar > Drawer, MenuMore

--- packages/plurid-react/source/modules/components/Toolbar/General/components/Drawer/styled.ts
+++ packages/plurid-react/source/modules/components/Toolbar/General/components/Drawer/styled.ts
@@ -8,7 +8,7 @@ export const StyledDrawer = styled.div`
         margin: 0;
     }
 
-    margin-bottom: 1rem;
+    margin-bottom: 1.5rem;
 `;
 
 
@@ -16,6 +16,7 @@ export const StyledDrawerHeading = styled.div`
     user-select: none;
     cursor: pointer;
     padding-bottom: 0.5rem;
+    margin-bottom: 0.5rem;
     border-bottom: 1px solid transparent;
 
     :hover {
