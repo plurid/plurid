14771845359979377e7342e13914c34f00146aef
Jan Cavel <caveljan@gmail.com>
2019-12-12 12:43:55 +0200

    plurid-react > dev: Toolbar > MenuMore

---
+++ packages/plurid-react/source/modules/components/Toolbar/General/components/MenuMore/data.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import {
+    TOOLBAR_DRAWERS,
+} from '@plurid/plurid-data';
+
+import MenuMoreThemes from './components/Themes';
+import MenuMoreTransform from './components/Transform';
+import MenuMoreSpace from './components/Space';
+import MenuMoreToolbar from './components/Toolbar';
+import MenuMoreViewcube from './components/Viewcube';
+import MenuMoreShortcuts from './components/Shortcuts';
+
+
+
+interface MoreMenu {
+    name: string;
+    drawer: keyof typeof TOOLBAR_DRAWERS,
+    component: JSX.Element,
+}
+
+export const moreMenus: MoreMenu[] = [
+    {
+        name: 'themes',
+        drawer: TOOLBAR_DRAWERS.THEMES,
+        component: (<MenuMoreThemes />),
+    },
+    {
+        name: 'transform',
+        drawer: TOOLBAR_DRAWERS.TRANSFORM,
+        component: (<MenuMoreTransform />),
+    },
+    {
+        name: 'space',
+        drawer: TOOLBAR_DRAWERS.SPACE,
+        component: (<MenuMoreSpace />),
+    },
+    {
+        name: 'toolbar',
+        drawer: TOOLBAR_DRAWERS.TOOLBAR,
+        component: (<MenuMoreToolbar />),
+    },
+    {
+        name: 'viewcube',
+        drawer: TOOLBAR_DRAWERS.VIEWCUBE,
+        component: (<MenuMoreViewcube />),
+    },
+    {
+        name: 'shortcuts',
+        drawer: TOOLBAR_DRAWERS.SHORTCUTS,
+        component: (<MenuMoreShortcuts />),
+    },
+];
