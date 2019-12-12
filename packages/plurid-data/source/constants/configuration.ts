d1adb4f83e01ca2d2933bcf0d5d185cdf3e377bc
Jan Cavel <caveljan@gmail.com>
2019-12-12 10:23:05 +0200

    plurid-data > dev: configuration

--- packages/plurid-data/source/constants/configuration.ts
+++ packages/plurid-data/source/constants/configuration.ts
@@ -24,6 +24,7 @@ export const defaultConfiguration: PluridConfiguration = {
             conceal: false,
             transformIcons: false,
             transformButtons: false,
+            toggledDrawers: [],
         },
         viewcube: {
             show: true,
