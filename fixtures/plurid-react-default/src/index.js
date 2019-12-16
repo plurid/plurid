26c833f216cd63e763289c6672e9d1232763b4da
Jan Cavel <caveljan@gmail.com>
2019-12-16 11:28:17 +0200

    plurid-engine > dev: space | links

--- fixtures/plurid-react-default/src/index.js
+++ fixtures/plurid-react-default/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App-single';
+// import App from './App-single';
 // import App from './App-documents';
 // import App from './App-virtual';
+import App from './App-links';
 import './index.css';
 
 
