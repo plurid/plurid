c929658c1836ca18fa40cf9e187ca714623d487e
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-16 19:34:15 +0200

    plurid-react > dev: PluridPlane > PlaneContent

--- fixtures/plurid-react-default/src/containers/Page2/index.jsx
+++ fixtures/plurid-react-default/src/containers/Page2/index.jsx
@@ -10,7 +10,9 @@ const Page2 = (properties) => {
     // console.log(properties.plurid);
 
     return (
-        <div style={{height: 400}}>
+        <div style={{
+            height: 400,
+        }}>
             page 2 <PluridLink page="/page-1">link to page 1</PluridLink>
 
             <div style={{ marginLeft: 200, marginTop: 100}}>
