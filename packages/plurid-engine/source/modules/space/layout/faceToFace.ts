f77a1b3e811656573a1dd8d5e6b1442ca0f43c93
Jan Cavel <caveljan@gmail.com>
2019-12-17 11:50:59 +0200

    plurid-engine > dev: space

--- packages/plurid-engine/source/modules/space/layout/faceToFace.ts
+++ packages/plurid-engine/source/modules/space/layout/faceToFace.ts
@@ -12,7 +12,7 @@ import {
 
 import {
     recomputeChildrenLocation,
-} from '../varia';
+} from '../location';
 
 
 
--- packages/plurid-engine/source/modules/space/layout/sheaves.ts
+++ packages/plurid-engine/source/modules/space/layout/sheaves.ts
@@ -4,7 +4,7 @@ import {
 
 import {
     recomputeChildrenLocation,
-} from '../varia';
+} from '../location';
 
 
 
