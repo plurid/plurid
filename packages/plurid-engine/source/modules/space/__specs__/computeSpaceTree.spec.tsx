de9367ac8443bc58a9f682c02a0701b507a62351
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-22 18:34:58 +0200

    plurid-engine > specs: space

--- packages/plurid-engine/source/modules/space/__specs__/computeSpaceTree.spec.tsx
+++ packages/plurid-engine/source/modules/space/__specs__/computeSpaceTree.spec.tsx
@@ -101,7 +101,7 @@ describe('computeSpaceTree', () => {
                 location: {
                     rotateX: 0,
                     rotateY: 0,
-                    translateX: 650,
+                    translateX: 1250,
                     translateY: 0,
                     translateZ: 0,
                 },
