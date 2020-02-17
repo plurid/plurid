64f94ae735d39ff247ad649f314d21e17a52993f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-17 13:52:20 +0200

    plurid-engine > dev: space > layout | rows

---
+++ packages/plurid-engine/source/modules/space/layout/row.ts
@@ -0,0 +1,65 @@
+import {
+    PluridConfiguration,
+    TreePage,
+
+    ROOTS_GAP,
+    defaultConfiguration,
+} from '@plurid/plurid-data';
+
+import {
+    mathematics,
+} from '@plurid/plurid-functions';
+
+import {
+    recomputeChildrenLocation,
+} from '../location';
+
+
+
+const computeRowLayout = (
+    roots: TreePage[],
+    columns: number = 2,
+    gap: number = ROOTS_GAP,
+    configuration: PluridConfiguration = defaultConfiguration,
+): TreePage[] => {
+    const tree: TreePage[] = [];
+    const width = mathematics.numbers.checkIntegerNonUnit(configuration.elements.plane.width)
+        ? configuration.elements.plane.width
+        : configuration.elements.plane.width * window.innerWidth;
+    const height = window.innerHeight;
+    const gapValue = mathematics.numbers.checkIntegerNonUnit(gap)
+        ? gap
+        : gap * width;
+
+    for (const [index, root] of roots.entries()) {
+        const rowIndex = Math.floor(index / columns);
+        const columnIndex = index % columns;
+        const translateX = columnIndex * (width + gapValue);
+        const translateY = rowIndex * (height + gapValue);
+
+        const treePage: TreePage = {
+            ...root,
+            location: {
+                translateX,
+                translateY,
+                translateZ: 0,
+                rotateX: 0,
+                rotateY: 0,
+            },
+        };
+
+        const children = recomputeChildrenLocation(treePage);
+
+        const treePageWithChildren = {
+            ...treePage,
+            children,
+        }
+
+        tree.push(treePageWithChildren);
+    }
+
+    return tree;
+}
+
+
+export default computeRowLayout;
