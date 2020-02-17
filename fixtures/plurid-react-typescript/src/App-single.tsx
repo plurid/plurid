12c45122a7742425fce987e7f8bcf1d8c98a4cb7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-17 15:27:13 +0200

    plurid-react > fixture: package

---
+++ fixtures/plurid-react-typescript/src/App-single.tsx
@@ -0,0 +1,144 @@
+import React, {
+    useState,
+    // useEffect,
+} from 'react';
+
+import PluridApp, {
+    // PluridConfiguration,
+    SPACE_LAYOUT,
+} from '@plurid/plurid-react';
+
+import Page1 from './containers/Page1';
+// import Page2 from './containers/Page2';
+
+
+
+const App = () => {
+    const appConfiguration = {
+        // micro: true,
+        theme: 'plurid',
+        // transparentUI: true,
+        space: {
+            layout: {
+                type: SPACE_LAYOUT.COLUMNS,
+                columns: 3,
+                gap: 0.1,
+            },
+            // layout: {
+            //     type: SPACE_LAYOUT.FACE_TO_FACE,
+            //     angle: 30,
+            //     gap: 50,
+            //     middle: 1,
+            // },
+            // layout: {
+            //     type: 'ZIG_ZAG',
+            //     angle: 50,
+            // },
+            // transformOrigin: {
+            //     show: false,
+            //     size: 'large',
+            // },
+            center: true,
+            // opaque: false,
+        },
+        elements: {
+            // toolbar: {
+            //     show: false,
+            // },
+            // viewcube: {
+            //     show: false,
+            // },
+            plane: {
+                width: 0.5,
+                // opacity: 0,
+                // controls: {
+                //     show: false,
+                // },
+            },
+        },
+    };
+    const anotherConfiguration = {
+        micro: true,
+    }
+
+    const appPages = [
+        // {
+        //     path: '/page-1',
+        //     component: {
+        //         element: Page1,
+        //         properties: {},
+        //     },
+        //     root: true,
+        // },
+        {
+            path: '/:page$',
+            component: {
+                element: Page1,
+                properties: {},
+            },
+            root: true,
+        },
+        // {
+        //     path: '/page-2',
+        //     component: {
+        //         element: Page2,
+        //         properties: {},
+        //     },
+        //     root: true,
+        // },
+        // {
+        //     path: '/page-3',
+        //     component: {
+        //         element: Page1,
+        //         properties: {},
+        //     },
+        //     // root: true,
+        // },
+        // {
+        //     path: '/page-4',
+        //     component: {
+        //         element: Page1,
+        //         properties: {},
+        //     },
+        //     root: true,
+        // },
+        // {
+        //     path: '/page-5',
+        //     component: {
+        //         element: Page1,
+        //         properties: {},
+        //     },
+        //     root: true,
+        // },
+        // {
+        //     path: '/page-6',
+        //     component: {
+        //         element: Page1,
+        //         properties: {},
+        //     },
+        //     root: true,
+        // },
+    ];
+
+    const [configuation, setConfiguration] = useState(appConfiguration);
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setConfiguration(anotherConfiguration);
+    //     }, 4000);
+    // }, [
+    //     anotherConfiguration
+    // ]);
+
+    return (
+        <div>
+            <PluridApp
+                configuration={configuation}
+                pages={appPages}
+            />
+        </div>
+    );
+}
+
+
+export default App;
---
+++ fixtures/plurid-react-typescript/src/containers/Page1/index.tsx
@@ -0,0 +1,71 @@
+import React, {
+    useRef,
+} from 'react';
+
+import {
+    PluridLink,
+} from '@plurid/plurid-react';
+
+
+
+const Page1 = (properties) => {
+    // console.log('aaa', properties);
+
+    // const a = useRef(Math.random() * 800 + 100);
+    // const b = useRef(Math.random() * 100 + 100);
+    const a = useRef(200);
+    const b = useRef(150);
+
+    return (
+        <div
+            style={{
+                padding: '1rem',
+                // height: 300,
+            }}
+        >
+            <div
+                tabIndex={0}
+                contentEditable={true}
+                suppressContentEditableWarning={true}
+            >
+                editable
+            </div>
+
+            page 1
+
+            <input />
+
+            <div>
+                <PluridLink page="/page-2/soo/loo">link to page 2</PluridLink>
+            </div>
+
+            <div style={{
+                marginLeft: a.current,
+                marginTop: b.current,
+            }}>
+                <PluridLink page="/page-3">link to page 3</PluridLink>
+            </div>
+
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id orci sit amet enim facilisis sagittis. Proin sagittis facilisis mauris, et bibendum ante dapibus at. Duis a sapien volutpat, egestas massa ac, vehicula orci. Donec vel est eros. Vivamus tellus arcu, molestie quis finibus in, euismod ut sapien. Etiam pretium eros et leo suscipit, at pharetra sapien bibendum. Integer elementum scelerisque malesuada. Curabitur rutrum iaculis justo. Vestibulum condimentum placerat neque ut <PluridLink page="/page-3">link to page 3</PluridLink> varius. <PluridLink page="/page-3">link to page 3</PluridLink> Ut at egestas augue. Ut quis elit nisl. Cras accumsan faucibus risus, quis tempor eros luctus sed. Suspendisse eu nulla sagittis, dignissim quam eget, rhoncus est. Fusce varius, sem sed vehicula rhoncus, quam turpis malesuada turpis, pretium posuere urna nunc eu ligula. Cras sapien nunc, sagittis a risus in, convallis sodales est.
+                </p>
+                <p>
+                    Aliquam eget volutpat urna. Proin fermentum euismod libero non dictum. Integer lacus ipsum, vulputate vel magna eu, fermentum aliquet metus. Cras nisi neque, aliquam eu imperdiet in, pharetra ut nisi. Aenean porta ante elit, vitae pellentesque ante commodo a. Nullam vitae lectus lacinia lacus volutpat aliquet et a nisi. Vestibulum eget tellus consectetur ante egestas bibendum ac ut magna. Sed eget leo at sem eleifend tristique nec quis eros. <PluridLink page="/page-3">link to page 3</PluridLink>  Quisque eget orci porta quam convallis consequat id id metus.
+                </p>
+                <p>
+                    In congue neque id ligula egestas suscipit. <PluridLink page="/page-3">link to page 3</PluridLink> Aliquam erat volutpat. Phasellus sit amet felis maximus velit rutrum dignissim mollis sed lorem. Suspendisse consectetur lobortis tortor a venenatis. Nullam ac ipsum nisi. In a diam urna. Nam pharetra sed ligula sed pulvinar. Fusce urna ante, blandit non interdum eu, auctor in velit. Aliquam quis faucibus nibh. Morbi venenatis tortor a dignissim pharetra. Donec malesuada maximus ligula non ullamcorper. Nam pellentesque ipsum sit amet nisi commodo, ac volutpat justo imperdiet.
+                </p>
+            </div>
+
+            {
+                // <div style={{ marginLeft: 630, marginTop: 130}}>
+                //     <PluridLink page="/page-2">link to page 2</PluridLink>
+                // </div>
+            }
+        </div>
+    );
+}
+
+
+export default Page1;
