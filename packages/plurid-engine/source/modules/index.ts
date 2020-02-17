c2710c02cc43405c8b4b753f3e39ae1f6a657baa
Jan Cavel <caveljan@gmail.com>
2020-02-17 19:12:17 +0200

    plurid-engine > dev: exports

---
+++ packages/plurid-engine/source/modules/index.ts
@@ -0,0 +1,15 @@
+import * as direction from './direction';
+import * as matrix from './matrix';
+import * as quaternion from './quaternion';
+import * as transform from './transform';
+import * as space from './space';
+
+
+
+export {
+    direction,
+    matrix,
+    quaternion,
+    transform,
+    space,
+}
