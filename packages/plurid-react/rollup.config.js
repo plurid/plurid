be96556d914395dd0c6d58ae624f353fbaf7247a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-18 10:59:27 +0200

    plurid-react > setup: package

--- packages/plurid-react/rollup.config.js
+++ packages/plurid-react/rollup.config.js
@@ -1,11 +1,11 @@
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import url from 'rollup-plugin-url';
+import url from '@rollup/plugin-url';
 import babel from 'rollup-plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 
 import pkg from './package.json';
 
