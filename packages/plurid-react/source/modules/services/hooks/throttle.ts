91ccfbc052879d41028c2c641ab4b55e1c2f4c08
Jan Cavel <caveljan@gmail.com>
2020-02-22 11:14:25 +0200

    plurid-react > dev: throttle

--- packages/plurid-react/source/modules/services/hooks/throttle.ts
+++ packages/plurid-react/source/modules/services/hooks/throttle.ts
@@ -42,54 +42,8 @@ const useThrottledCallback = <A extends any[]>(
             },
             delay - (Date.now() - lastRan.current)
         );
-
-        // // capture latest args
-
-        // // clear debounce timer
-        // cleanup();
-
-        // // start waiting again
-        // timeout.current = setTimeout(() => {
-        //     if(argsRef.current) {
-        //         callback(...argsRef.current);
-        //     }
-        // }, wait);
     };
 }
-
-
-// const useThrottledCallback = (
-//     callback: any,
-//     delay: number,
-//     dependencies: any[] = [],
-// ) => {
-//     const lastRan = useRef(Date.now());
-
-//     const timeout = useRef<ReturnType<typeof setTimeout>>();
-
-
-//     useEffect(
-//         () => {
-//             const handler = setTimeout(
-//                 () => {
-//                     if (Date.now() - lastRan.current >= delay) {
-//                         callback();
-//                         lastRan.current = Date.now();
-//                     }
-//                 },
-//                 delay - (Date.now() - lastRan.current)
-//             );
-
-//             return () => {
-//                 clearTimeout(handler);
-//             };
-//         },
-//         [
-//             delay,
-//             ...dependencies,
-//         ],
-//     );
-// };
 
 
 export default useThrottledCallback;
