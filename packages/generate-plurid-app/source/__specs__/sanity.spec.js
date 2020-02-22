9c51d9c1e80a0b2fafc8a91d33ada42dbe85091a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-22 18:31:23 +0200

    generate-plurid-app > setup: package

--- packages/generate-plurid-app/source/__specs__/sanity.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe('Generate Plurid App sanity test', () => {
-    it('works if true is truthy', () => {
-        expect(true).toBeTruthy();
-    });
-});
-//# sourceMappingURL=sanity.spec.js.map
--- packages/generate-plurid-app/source/inquire/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const inquire_1 = require("./inquire");
-const questions_1 = require("./questions");
-const processArgs_1 = require("./processArgs");
-exports.inquire = inquire_1.inquire;
-exports.questions = questions_1.questions;
-exports.processArgs = processArgs_1.processArgs;
-//# sourceMappingURL=index.js.map
