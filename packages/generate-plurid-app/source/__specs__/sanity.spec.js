6bdf5ee7620290f4e364854356ca1d1547886786
Jan Cavel <caveljan@gmail.com>
2020-02-22 18:30:02 +0200

    generate-plurid-app > setup: package

---
+++ packages/generate-plurid-app/source/__specs__/sanity.spec.js
@@ -0,0 +1,7 @@
+"use strict";
+describe('Generate Plurid App sanity test', () => {
+    it('works if true is truthy', () => {
+        expect(true).toBeTruthy();
+    });
+});
+//# sourceMappingURL=sanity.spec.js.map
