930ef0321e1e1f888f09110de5c0d8a2d70e405e
Jan Cavel <caveljan@gmail.com>
2020-02-23 13:28:43 +0200

    generate-plurid-app > setup: package

--- packages/generate-plurid-app/source/index.ts
+++ packages/generate-plurid-app/source/index.ts
@@ -30,8 +30,8 @@ async function main(program: CommanderStatic) {
         program
             .option('-d, --directory <path>', 'set the application directory')
             .option('-l, --language <language>', 'set language ("typescript" -> TypeScript || "javascript" -> JavaScript)')
-            .option('-u, --ui <ui-engine>', 'set UI engine ("html" -> HTML Custom Elements || "react" -> React || "vue" -> Vue)')
-            .option('-r, --renderer <renderer>', 'set application rendering side ("client" -> Client-Side Rendering || "server" -> Server-Side Rendering)')
+            .option('-u, --ui <ui-engine>', 'set UI engine ("html" -> HTML Custom Elements || "react" -> React || "vue" -> Vue || "angular" -> Angular)')
+            .option('-r, --renderer <renderer>', 'set the application rendering side ("client" -> Client-Side Rendering || "server" -> Server-Side Rendering)')
             .option('-m, --manager <package-manager>', 'set the package manager ("npm" || "yarn")')
             .action(async () => {
                 const {
