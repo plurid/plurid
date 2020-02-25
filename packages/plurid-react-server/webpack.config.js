d85ec662c1fb09274d5f491579080358fc9abf17
Jan Cavel <caveljan@gmail.com>
2020-02-25 22:41:10 +0200

    plurid-react-server > setup: package

--- packages/plurid-react-server/webpack.config.js
+++ packages/plurid-react-server/webpack.config.js
@@ -4,7 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 
 
 module.exports = {
-    entry: './src/server/index.tsx',
+    entry: './source/server/index.tsx',
 
     target: 'node',
 
