ad2d48e5f3a30e7af6a69e9942f217fffe5fee37
Jan Cavel <caveljan@gmail.com>
2020-02-25 16:55:41 +0200

    plurid-data > dev: interfaces > link | text fragment, page element

--- packages/plurid-data/source/interfaces/external/link.ts
+++ packages/plurid-data/source/interfaces/external/link.ts
@@ -14,6 +14,31 @@ export interface PluridLink {
      * The path of the page.
      *
      * If IDs are provided to the pages, the id of the page.
+     *
+     * The page path can:
+     *
+     * * be a simple string,
+     * e.g. `'/path/to/page'` or `'/d71b21673037485a'`,
+     * where `d71b21673037485a` is a generated page ID;
+     *
+     * * be a parametric route,
+     * e.g. `'/path/to/:page'`, where `:page` is the parameter name;
+     *
+     * * receive query key=value pairs,
+     * e.g. `'/path/to/page?id=1&show=true'`, where `id=1` and `show=true` are key=value pairs
+     *
+     * * specify a text fragment,
+     * e.g. `'/path/to/page#:~:text=A%20door,is%opened.[0]'`,
+     * where the fragment `#:~:text=A%20door,is%opened.[0]`
+     * is loosely based on the https://github.com/WICG/ScrollToTextFragment specification,
+     * and indicates the link to bring into view the first occurence `[0]`, if any,
+     * of the text fragment starting with `A door` and ending with `is opened`.
+     *
+     * * specify a page element,
+     * e.g. `'/path/to/page#:~:element=123[1]'`,
+     * where the fragment `#:~:element=123[1]`
+     * indicates the link to bring into view the second occurence `[1]`, if any,
+     * of the element with the attribute `data-plurid-element=123`.
      */
     page: string;
 
