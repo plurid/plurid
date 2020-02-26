6385c225362891f7848a215a2f8e5690ebb4e8a6
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-26 17:49:50 +0200

    plurid-react-server > fixture: client > App

--- fixtures/plurid-react-server-typescript/source/client/App.tsx
+++ fixtures/plurid-react-server-typescript/source/client/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
+import PluridApp from '@plurid/plurid-react';
+
 
 
 const Application = () => {
+    const pages = [
+        {
+            path: '/',
+            component: {
+                element: () => (<div>Plurid' Application</div>),
+            },
+        },
+    ];
+
+    const view = [
+        '/',
+    ];
+
     return (
-        <div>Plurid' Application</div>
+        <PluridApp
+            pages={pages}
+            view={view}
+        />
     );
 }
 
