f02319df50fdb7dc06308e4264e529b22c882767
Jan Cavel <caveljan@gmail.com>
2020-02-26 11:38:49 +0200

    plurid-react-server > dev: Server

---
+++ packages/plurid-react-server/source/server/data/interfaces/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import React from 'react';
+
+
+
+export interface PluridServerRoute {
+    path: string;
+    view: string | string[];
+    // type: 'get' | 'post' | 'put';
+    middleware?: PluridServerMiddleware;
+}
+
+
+export type PluridServerMiddleware = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction,
+) => void;
+
+
+export interface PluridServerOptions {
+    quiet: boolean;
+}
+
+export type PluridServerPartialOptions = Partial<PluridServerOptions>;
+
+
+export interface PluridServerConfiguration {
+    Application: React.FC<any>;
+    routes: PluridServerRoute[]
+    index: string;
+    middleware?: PluridServerMiddleware[];
+    options?: PluridServerPartialOptions;
+}
