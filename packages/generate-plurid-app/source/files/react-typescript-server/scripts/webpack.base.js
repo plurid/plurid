d8ab493d01f03cdd974179225815b72a1b444023
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-26 22:49:04 +0200

    generate-plurid-app > dev: files > react-typescript-server

--- packages/generate-plurid-app/source/files/react-typescript-server/scripts/webpack.base.js
+++ packages/generate-plurid-app/source/files/react-typescript-server/scripts/webpack.base.js
@@ -7,6 +7,14 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
                 test: /\.ts(x?)$/,
                 exclude: /node_modules/,
                 use: [
