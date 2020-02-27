1f34fb9ed29cccbb53d2a0c613ef3ae7892a464f
Jan Cavel <caveljan@gmail.com>
2020-02-27 11:29:21 +0200

    generate-plurid-app > dev: files > react-javascript-server

--- packages/generate-plurid-app/source/files/react-javascript-server/scripts/webpack.base.js
+++ packages/generate-plurid-app/source/files/react-javascript-server/scripts/webpack.base.js
@@ -17,7 +17,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.js$/,
+                test: /\.jsx?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 options: {
