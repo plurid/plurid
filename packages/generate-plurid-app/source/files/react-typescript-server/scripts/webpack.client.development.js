65da17438f4e9ddba344d295ef318c354b04dc02
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-27 10:52:20 +0200

    generate-plurid-app > dev: files > react-typescript-server

--- packages/generate-plurid-app/source/files/react-typescript-server/scripts/webpack.client.development.js
+++ packages/generate-plurid-app/source/files/react-typescript-server/scripts/webpack.client.development.js
@@ -1,20 +1,22 @@
 const path = require('path');
 const merge = require('webpack-merge');
+
 const baseConfig = require('./webpack.base');
 
 
 
 const config = {
     mode: 'development',
-    // Tell webpack to root file of our server app
+
     entry: './source/client/index.tsx',
 
-    // Tell webpack where to put output file
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../build')
+        path: path.resolve(__dirname, '../build'),
     },
-    devtool: 'inline-source-map'
+
+    devtool: 'inline-source-map',
 };
+
 
 module.exports = merge(baseConfig, config);
