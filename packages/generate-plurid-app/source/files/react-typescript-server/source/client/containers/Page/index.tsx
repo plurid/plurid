44054d8367e705b21c8d13953e9087b44a9e4b1d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-27 14:14:05 +0200

    generate-plurid-app > dev: files > react-typescript-server

--- packages/generate-plurid-app/source/files/react-typescript-server/source/client/containers/Page/index.tsx
+++ packages/generate-plurid-app/source/files/react-typescript-server/source/client/containers/Page/index.tsx
@@ -8,8 +8,8 @@ import {
     StyledPage,
 } from './styled';
 
-import pluridLogo from '../../plurid-logo.png';
-
+// import pluridLogo from '../../plurid-logo.png';
+import pluridLogoSVG from '../../plurid-logo.svg';
 
 
 
@@ -31,7 +31,8 @@ const Page: React.FC<any> = (
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <img src={pluridLogo} alt="plurid logo" height={150} />
+                    {/* <img src={pluridLogo} alt="plurid logo" height={250} /> */}
+                    <img src={pluridLogoSVG} alt="plurid logo" height={250} />
                 </a>
             </div>
 
