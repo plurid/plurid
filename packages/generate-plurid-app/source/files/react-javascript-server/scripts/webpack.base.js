f40e52c4b922fb3d743861e44b890f5ff636b79a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-27 11:06:23 +0200

    generate-plurid-app > dev: files > react-javascript-server

---
+++ packages/generate-plurid-app/source/files/react-javascript-server/scripts/webpack.base.js
@@ -0,0 +1,41 @@
+module.exports = {
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[hash].[ext]',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        '@babel/preset-react',
+                        ['@babel/env', { targets: { browsers: ['last 2 versions'] } }],
+                    ],
+                },
+            },
+        ],
+    },
+};
---
+++ packages/generate-plurid-app/source/files/react-javascript-server/scripts/webpack.client.production.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const baseConfig = require('./webpack.base');
+
+
+
+const config = {
+    mode: 'production',
+
+    stats: {
+        colors: false,
+        hash: true,
+        timings: true,
+        assets: true,
+        chunks: true,
+        chunkModules: true,
+        modules: true,
+        children: true,
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                test: /\.js(\?.*)?$/i,
+                sourceMap: true,
+                terserOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
+
+    entry: './source/client/index.tsx',
+
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../build'),
+    },
+
+    plugins: [
+        // new BundleAnalyzerPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+    ],
+};
+
+
+module.exports = merge(baseConfig, config);
