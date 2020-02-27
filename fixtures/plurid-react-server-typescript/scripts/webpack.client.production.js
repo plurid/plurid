79734212258d18339e0bfe9cea441257a3d4a021
Jan Cavel <caveljan@gmail.com>
2020-02-27 13:08:00 +0200

    plurid-react-server > fixture: package

--- fixtures/plurid-react-server-typescript/scripts/webpack.client.production.js
+++ fixtures/plurid-react-server-typescript/scripts/webpack.client.production.js
@@ -1,15 +1,16 @@
-const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
-const baseConfig = require('./webpack.base');
+const TerserPlugin = require('terser-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const baseConfig = require('./webpack.base');
 
 
 
 const config = {
     mode: 'production',
+
     stats: {
         colors: false,
         hash: true,
@@ -18,41 +19,41 @@ const config = {
         chunks: true,
         chunkModules: true,
         modules: true,
-        children: true
+        children: true,
     },
+
     optimization: {
+        minimize: true,
         minimizer: [
-        // we specify a custom UglifyJsPlugin here to get source maps in production
-        new UglifyJSPlugin({
-            cache: true,
-            parallel: true,
-            uglifyOptions: {
-            compress: false,
-            ecma: 6,
-            mangle: true
-            },
-            sourceMap: false
-        })
-        ]
+            new TerserPlugin({
+                test: /\.js(\?.*)?$/i,
+                sourceMap: true,
+                terserOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
     },
-    // Tell webpack to root file of our server app
+
     entry: './source/client/index.tsx',
 
-    // Tell webpack where to put output file
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../build')
+        path: path.resolve(__dirname, '../build'),
     },
-    devtool: 'inline-source-map',
+
     plugins: [
         // new BundleAnalyzerPlugin(),
-        new CompressionPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
-        })
-    ]
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+    ],
 };
+
 
 module.exports = merge(baseConfig, config);
