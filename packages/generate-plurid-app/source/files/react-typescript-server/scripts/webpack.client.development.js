e69b6670405e587abf41debba70ebfe7a114282e
Jan Cavel <caveljan@gmail.com>
2020-02-27 19:04:00 +0200

    generate-plurid-app > dev: files > react-typescript-server

--- packages/generate-plurid-app/source/files/react-typescript-server/scripts/webpack.client.development.js
+++ packages/generate-plurid-app/source/files/react-typescript-server/scripts/webpack.client.development.js
@@ -3,7 +3,7 @@ const path = require('path');
 const merge = require('webpack-merge');
 const CopyPlugin = require('copy-webpack-plugin');
 
-const baseConfig = require('./webpack.base');
+const baseConfig = require('./webpack.client.base');
 
 
 
@@ -15,6 +15,10 @@ const config = {
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, '../build'),
+    },
+
+    optimization: {
+        minimize: false,
     },
 
     devtool: 'inline-source-map',
