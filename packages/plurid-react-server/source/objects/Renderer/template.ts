3eaa4e868311a2e0dbca72026a4304ba4d6f30be
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-28 21:15:15 +0200

    plurid-react-server > dev: Renderer

--- packages/plurid-react-server/source/objects/Renderer/template.ts
+++ packages/plurid-react-server/source/objects/Renderer/template.ts
@@ -2,7 +2,7 @@ const cleanTemplate = (
     template: string,
 ) => {
     const cleanNewLines = template.replace(/(?:\r\n|\r|\n)/g, '');
-    const cleanWhitespace = cleanNewLines.replace(/ +/g, ' ');
+    const cleanWhitespace = cleanNewLines.replace(/  +/g, ' ');
 
     return cleanWhitespace;
 }
