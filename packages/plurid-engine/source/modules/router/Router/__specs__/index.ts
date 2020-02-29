c4e0d86307e09faead6b37e94c9190ee4b8152dd
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-29 17:00:37 +0200

    plurid-engine > specs: router > Router

---
+++ packages/plurid-engine/source/modules/router/Router/__specs__/index.ts
@@ -0,0 +1,87 @@
+import Router from '../';
+
+import {
+    Route,
+} from '../../interfaces';
+
+
+
+describe('Router', () => {
+    it('simple route', () => {
+        const routes: Route<any>[] = [
+            {
+                location: '/one',
+                view: 'one',
+            },
+            {
+                location: '/two',
+                view: 'two',
+            },
+        ];
+        const router = new Router(routes);
+        const response = router.match('/one');
+
+        expect(response?.pathname).toBe('/one');
+    });
+
+    it('simple route - parametric', () => {
+        const routes: Route<any>[] = [
+            {
+                location: '/one/:element',
+                view: 'one',
+            },
+            {
+                location: '/two',
+                view: 'two',
+            },
+        ];
+        const router = new Router(routes);
+        const response = router.match('/one/asd');
+
+        expect(response?.pathname).toBe('/one/asd');
+    });
+
+    it('simple route - query', () => {
+        const routes: Route<any>[] = [
+            {
+                location: '/one',
+                view: 'one',
+            },
+            {
+                location: '/two',
+                view: 'two',
+            },
+        ];
+        const router = new Router(routes);
+        const response = router.match('/one?q=true');
+
+        expect(response?.pathname).toBe('/one');
+
+        const query = {
+            q: 'true',
+        };
+        expect(response?.query).toStrictEqual(query);
+    });
+
+    it('simple route - fragment', () => {
+        const routes: Route<any>[] = [
+            {
+                location: '/one',
+                view: 'one',
+            },
+            {
+                location: '/two',
+                view: 'two',
+            },
+        ];
+        const router = new Router(routes);
+        const response = router.match('/two#:~:text=start');
+
+        expect(response?.pathname).toBe('/two');
+
+        const fragmentsTexts = [
+            { type: 'text', start: 'start', end: '', occurence: 0 },
+        ];
+        expect(response?.fragments.texts).toStrictEqual(fragmentsTexts);
+    });
+});
