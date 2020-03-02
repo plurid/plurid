e7fd3332559fe81585734d242f5eadc39739789f
Jan Cavel <caveljan@gmail.com>
2020-03-02 17:10:05 +0200

    plurid-react > dev: PluridRouterBrowser, PluridRouterStatic

---
+++ packages/plurid-react/source/modules/components/PluridRouterBrowser/index.tsx
@@ -0,0 +1,79 @@
+import React, {
+    useRef,
+    useState,
+    useEffect,
+} from 'react';
+
+import {
+    router,
+} from '@plurid/plurid-engine';
+
+import {
+    indexing,
+} from '@plurid/plurid-functions';
+
+
+
+interface RouteComponent<T> {
+    view: T;
+    component: React.FC<any>;
+}
+
+interface PluridRouterBrowserOwnProperties<T> {
+    path: string;
+    routes: router.Route<T>[];
+    components: RouteComponent<T>[];
+}
+
+function PluridRouterBrowser<T>(
+    properties: PluridRouterBrowserOwnProperties<T>,
+) {
+    /** properties */
+    const {
+        path,
+        routes,
+        components,
+    } = properties;
+
+
+    /** references */
+    const indexedComponents = useRef(indexing.create(components, 'view'));
+    const pluridRouter = useRef(new router.default(routes));
+
+
+    /** state */
+    const [matchedRoute, setMatchedRoute] = useState<router.MatcherResponse<T>>();
+    const [Component, setComponent] = useState<React.FC<any>>();
+
+
+    /** effects */
+    useEffect(() => {
+        if (pluridRouter.current && indexedComponents.current) {
+            const matchedRoute = pluridRouter.current.match(path);
+
+            if (matchedRoute) {
+                setMatchedRoute(matchedRoute);
+
+                const view = matchedRoute.route.view;
+                const routeComponent = indexedComponents.current[view as any];
+
+                if (routeComponent) {
+                    setComponent(routeComponent.component);
+                }
+            }
+        }
+    }, [
+        path,
+    ]);
+
+    return (
+        <>
+            {matchedRoute && Component && (
+                <Component />
+            )}
+        </>
+    );
+}
+
+
+export default PluridRouterBrowser;
---
+++ packages/plurid-react/source/modules/components/PluridRouterStatic/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import {
+    router,
+} from '@plurid/plurid-engine';
+
+import {
+    indexing,
+} from '@plurid/plurid-functions';
+
+
+
+interface RouteComponent<T> {
+    view: T;
+    component: React.FC<any>;
+}
+
+interface PluridRouterStaticOwnProperties<T> {
+    path: string;
+    routes: router.Route<T>[];
+    components: RouteComponent<T>[];
+}
+
+function PluridRouterStatic<T>(
+    properties: PluridRouterStaticOwnProperties<T>,
+) {
+    /** properties */
+    const {
+        path,
+        routes,
+        components,
+    } = properties;
+
+    const indexedComponents = indexing.create(components, 'view');
+    const pluridRouter = new router.default(routes);
+
+    const matchedRoute = pluridRouter.match(path);
+    if (!matchedRoute) {
+        return (
+            <></>
+        )
+    }
+
+    const view = matchedRoute.route.view;
+    const routeComponent = indexedComponents[view as any];
+
+    if (!routeComponent) {
+        return (
+            <></>
+        )
+    }
+
+    const Component = routeComponent.component;
+
+    return (
+        <>
+            {matchedRoute && Component && (
+                <Component />
+            )}
+        </>
+    );
+}
+
+
+export default PluridRouterStatic;
