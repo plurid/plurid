ff017a7231a78ca5dc92f78d19bfe02ac820a211
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-02 23:37:37 +0200

    plurid-data > dev: constants

--- packages/plurid-data/source/constants/index.ts
+++ packages/plurid-data/source/constants/index.ts
@@ -1,6 +1,14 @@
+import * as internationalization from './internationalization';
+
+
+
 export * from './configuration';
 export * from './defaults';
 export * from './events';
 export * from './keys';
 export * from './shortcuts';
 export * from './space';
+
+export {
+    internationalization,
+}
