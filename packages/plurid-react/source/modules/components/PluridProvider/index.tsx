6d003856ef24b70505e2bdc3c75b35555a49c2d4
Jan Cavel <caveljan@gmail.com>
2020-03-04 12:16:25 +0200

    plurid-react > dev: PluridProvider

---
+++ packages/plurid-react/source/modules/components/PluridProvider/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const PluridContext = React.createContext({});
+
+const PluridProvider = PluridContext.Provider;
+
+
+export {
+    PluridContext,
+    PluridProvider,
+}
