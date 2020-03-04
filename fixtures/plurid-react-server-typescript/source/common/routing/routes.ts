a523e5bdaa1843af4cfa039e3d1a9e7441dcae5d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-04 13:22:21 +0200

    plurid-react-server > fixture: common

--- fixtures/plurid-react-server-typescript/source/common/routing/routes.ts
+++ fixtures/plurid-react-server-typescript/source/common/routing/routes.ts
@@ -1,33 +1,16 @@
 import {
-    Indexed,
     PluridRouterRoute,
 } from '@plurid/plurid-data';
 
+import {
+    mapPathsToRoutes,
+} from '@plurid/plurid-react-server';
+
 import paths from './paths';
-import view from './view';
+import view, {
+    // ViewType,
+} from './view';
 
-
-
-const mapPathsToRoutes = <T>(
-    paths: Indexed<string>,
-    view: T,
-) => {
-    const routes: PluridRouterRoute<T>[] = [];
-
-    for (const [key, path] of Object.entries(paths)) {
-        const pathView = view[key];
-
-        if (pathView) {
-            const route: PluridRouterRoute<T> = {
-                path,
-                view: pathView,
-            };
-            routes.push(route);
-        }
-    }
-
-    return routes;
-}
 
 
 // const routes: PluridRouterRoute<any>[] = [
@@ -40,7 +23,6 @@ const mapPathsToRoutes = <T>(
 //         view: view.page,
 //     },
 // ];
-
 const routes: PluridRouterRoute<any>[] = mapPathsToRoutes(
     paths,
     view,
