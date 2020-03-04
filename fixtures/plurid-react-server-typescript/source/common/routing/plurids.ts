97a8827a92ebc9de4285203aae92850aba3a75c9
Jan Cavel <caveljan@gmail.com>
2020-03-04 18:26:00 +0200

    plurid-react-server > fixture: client, common

---
+++ fixtures/plurid-react-server-typescript/source/common/routing/plurids.ts
@@ -0,0 +1,12 @@
+const plurids = [
+    {
+        configuration: {},
+        pages: [],
+        view: '',
+    },
+    {
+        configuration: {},
+        pages: [],
+        view: '',
+    },
+];
