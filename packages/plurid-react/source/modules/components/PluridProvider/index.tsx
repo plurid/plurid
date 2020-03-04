3de1af9cb1dcc06abc1951d838e639874b898b92
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-04 17:05:31 +0200

    plurid-react > dev: PluridProvider

--- packages/plurid-react/source/modules/components/PluridProvider/index.tsx
+++ packages/plurid-react/source/modules/components/PluridProvider/index.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, {
+    Component,
+} from 'react';
+
+import {
+    runtime,
+} from '@plurid/plurid-functions';
 
 
-const PluridContext = React.createContext({});
 
-const PluridProvider = PluridContext.Provider;
+const defaultValue = {};
+export const Context = React.createContext(defaultValue);
 
 
-export {
-    PluridContext,
-    PluridProvider,
+interface PluridProviderProperties {
+    context: any;
+}
+
+export default class PluridProvider extends Component<PluridProviderProperties> {
+    static canUseDOM = runtime.isBrowser;
+    static displayName = 'PluridProvider';
+
+    private instances: any[] = [];
+    private value = {
+        setPlurid: (serverState: any) => {
+            this.props.context.plurid = serverState;
+        },
+        pluridInstances: {
+            get: () => this.instances,
+            add: (instance: any) => {
+                this.instances.push(instance);
+            },
+            remove: (instance: any) => {
+                const index = this.instances.indexOf(instance);
+                this.instances.splice(index, 1);
+            },
+        },
+    };
+
+    constructor(
+        props: any,
+    ) {
+        super(props);
+
+        if (!PluridProvider.canUseDOM) {
+            // props.context.helmet = mapStateOnServer({
+            // });
+        }
+    }
+
+    render() {
+        return (
+            <Context.Provider
+                value={this.value}
+            >
+                {this.props.children}
+            </Context.Provider>
+        );
+    }
 }
