6ff116f5032cc73a7730c7405a1919f5af14b581
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-04 15:04:26 +0200

    plurid-react-server > dev: utilities

--- packages/plurid-react-server/source/index.ts
+++ packages/plurid-react-server/source/index.ts
@@ -1,17 +1,8 @@
 import PluridServer from './objects/Server';
 
-import {
-    mapPathsToRoutes,
-} from './utilities/routing';
-
 
 
 export * from './data/interfaces';
 
 
 export default PluridServer;
-
-
-export {
-    mapPathsToRoutes,
-}
