d48f0b7a56b66b039aa2170679222b3bf06712f8
Jan Cavel <caveljan@gmail.com>
2020-03-04 08:48:53 +0200

    plurid-react-server > dev: utilities > routing

--- packages/plurid-react-server/source/index.ts
+++ packages/plurid-react-server/source/index.ts
@@ -1,8 +1,17 @@
 import PluridServer from './objects/Server';
 
+import {
+    mapPathsToRoutes,
+} from './utilities/routing';
+
 
 
 export * from './data/interfaces';
 
 
 export default PluridServer;
+
+
+export {
+    mapPathsToRoutes,
+}
