81d37c4f034d279647fdc4bb2b243156a7ecd9e1
Jan Cavel <caveljan@gmail.com>
2020-03-11 23:35:00 +0200

    plurid-cli > setup: package

---
+++ packages/plurid-cli/rollup.config.ts
@@ -0,0 +1,61 @@
+import resolve from '@rollup/plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import commonjs from '@rollup/plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+import json from '@rollup/plugin-json';
+import copy from 'rollup-plugin-copy';
+
+
+const pkg = require('./package.json');
+
+
+const globals = {
+    'commander': 'program',
+};
+
+
+export default {
+    input: `source/index.ts`,
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            globals,
+            sourcemap: true,
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            globals,
+            sourcemap: true,
+        },
+    ],
+    external: [
+        'child_process',
+        'path',
+        'fs',
+    ],
+    watch: {
+        include: 'source/**',
+    },
+    plugins: [
+        json(),
+        typescript({
+            useTsconfigDeclarationDir: true,
+        }),
+        external({
+            includeDependencies: true,
+        }),
+        resolve({
+            preferBuiltins: true,
+        }),
+        commonjs(),
+        sourceMaps(),
+        copy({
+            targets: [
+                { src: 'source/files/', dest: 'distribution/' },
+            ],
+        }),
+    ],
+}
