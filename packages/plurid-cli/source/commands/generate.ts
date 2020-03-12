25f8656e7c4a752b45fb711ccc7d1529af74356f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-12 07:20:05 +0200

    plurid-cli > dev: commands > generate

--- packages/plurid-cli/source/commands/generate.ts
+++ packages/plurid-cli/source/commands/generate.ts
@@ -1,11 +1,17 @@
 import {
-    executeCommand,
+    executeCommandSameTerminal,
 } from '../utilities';
 
 
 
 const generateCommand = async () => {
-    executeCommand('npx @plurid/generate-plurid-app');
+    // check if @plurid/generate-plurid-app is installed globally
+
+    // install @plurid/generate-plurid-app
+
+    console.log('\n\tLaunching generation program @plurid/generate-plurid-app...\n');
+
+    executeCommandSameTerminal('npx @plurid/generate-plurid-app');
 }
 
 
