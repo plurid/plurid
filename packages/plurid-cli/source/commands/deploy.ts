3acc24f49f3e0d49628494b68c1251951674a2d7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-12 12:32:01 +0200

    plurid-cli > dev: commands > deploy

--- packages/plurid-cli/source/commands/deploy.ts
+++ packages/plurid-cli/source/commands/deploy.ts
@@ -1,6 +1,16 @@
 const deployCommand = async (
     directory: string | undefined,
 ) => {
+    // check if user is loggedin
+
+    // resolve directory
+
+    // check if there is a plurid.app.yaml file and parse it
+
+    // upload files
+
+    // wait for deployment to finish
+
     console.log('deploy');
     console.log(directory);
 }
