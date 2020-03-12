dc62cfb1f3dbb13fb8b149e3396034692d5aa0f6
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-12 09:25:13 +0200

    plurid-cli > dev: commands > authenticate

--- packages/plurid-cli/source/commands/authenticate.ts
+++ packages/plurid-cli/source/commands/authenticate.ts
@@ -1,27 +1,53 @@
 import express from 'express';
 
+import open from 'open';
+
 import store from '../services/store';
 
 
-const app = express();
-const port = 33800;
 
-app.get('/access/:accessCode', (req, res) => res.send(req.params.accessCode || ''));
+const app = express();
+app.get('/access/:accessCode', (req, res) => {
+    const accessCode = req.params.accessCode || '';
+    store.set('accessCode', accessCode);
+    console.log(`\n\tSet access code: ${accessCode}`);
+
+    res.send(accessCode);
+});
 
 
 const authenticateCommand = async () => {
     // start server on port 33800
-    app.listen(port);
+    // get useable port
+    const port = 33800;
+    const server = app.listen(port);
+
 
     // open account.plurid.com?cli=33800
-
     // the user logins into account, creates an account
-
     // the user is redirected to http://localhost:33800/access/<access-code>
+    const accountLink = `https://account.plurid.com?cli=${port}`;
+    open(accountLink);
+    console.log(`\n\tOpened ${accountLink} in the default browser.`);
 
-    // the server listens for the access code and queries api.plurid.com for the tokens
 
-    console.log('authenticate');
+    // listen for accessCode to be set in store
+    const interval = setInterval(() => {
+        const accessCode = store.get('accessCode');
+
+        if (accessCode) {
+            // queries api.plurid.com for the tokens
+            const username = 'username';
+            console.log(`\n\tYou are now logged in as ${username}.`);
+
+            store.set('accessCode', '');
+            server.close();
+            clearInterval(interval);
+            return;
+        }
+
+        // check if more than 5 minutes have passed and close server
+    }, 1500);
 }
 
 
