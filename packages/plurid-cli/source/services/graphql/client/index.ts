6c491ba1f621c793f7563473e32abd12b93c5cd0
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-12 23:29:11 +0200

    plurid-cli > dev: graphql

--- packages/plurid-cli/source/services/graphql/client/index.ts
+++ packages/plurid-cli/source/services/graphql/client/index.ts
@@ -2,6 +2,8 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+import fetch from 'cross-fetch';
+
 
 
 const PLURID_API_URL = 'http://localhost:33600/graphql';
@@ -11,6 +13,7 @@ const client = new ApolloClient({
     link: createHttpLink({
         uri: PLURID_API_URL,
         credentials: 'include',
+        fetch,
     }),
     cache: new InMemoryCache(),
 });
