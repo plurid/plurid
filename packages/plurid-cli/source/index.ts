8285c8932fe23549d6c3a0651b46490e5984f318
Jan Cavel <caveljan@gmail.com>
2020-03-12 06:48:11 +0200

    plurid-cli > dev: index, test plurid.app.yaml

--- packages/plurid-cli/source/index.ts
+++ packages/plurid-cli/source/index.ts
@@ -4,34 +4,48 @@ import program, {
 
 
 
-async function main(program: CommanderStatic) {
+async function main(
+    program: CommanderStatic,
+) {
     program
+        .name('plurid')
+        .usage('<command>')
         .version('0.1.0', '-v, --version');
 
     program
+        .command('')
+        .action(async () => {
+            console.log('Run a command: authenticate, generate, deploy.');
+        });
+
+    program
         .command('authenticate')
-        .description('login or create account on plurid.com')
-        .action(() => {
+        .description('login or create an user account on plurid.com')
+        .action(async () => {
             console.log('authenticate');
         });
 
     program
         .command('generate')
-        .description('generate a plurid\' application')
-        .action(() => {
+        .description('generate a new plurid\' application')
+        .action(async () => {
             console.log('generate');
         });
 
     program
         .command('deploy [directory]')
         .description('deploy the plurid\' application from the current folder or from the target folder to plurid.app')
-        .action((directory) => {
+        .action(async (directory) => {
             console.log('deploy');
             console.log(directory);
         });
 
 
-    program.parse(process.argv);
+    if (!process.argv.slice(2).length) {
+        program.outputHelp();
+    }
+
+    program.parseAsync(process.argv);
 }
 
 
