163c4043459f48c8dbe3199da7902c128d264c6b
Jan Cavel <caveljan@gmail.com>
2020-03-14 13:38:00 +0200

    plurid-cli > dev: commands > deploy

--- packages/plurid-cli/source/data/constants/index.ts
+++ packages/plurid-cli/source/data/constants/index.ts
@@ -7,3 +7,12 @@ export const PLURID_API_URL_GRPAHQL = environment.production
     : environment.development
         ? 'https://api.plurid.dev/graphql/'
         : 'http://localhost:33600/graphql/';
+
+
+export const UPLOAD_HOSTNAME = environment.local
+    ? 'localhost'
+    : 'upload.plurid.com';
+
+export const UPLOAD_PORT = environment.local
+    ? 33605
+    : 443;
