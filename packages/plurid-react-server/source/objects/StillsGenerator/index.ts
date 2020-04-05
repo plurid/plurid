7ac9a8129ea5452d7cb85fbbe76ed599d39819a7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-05 21:51:10 +0300

    plurid-react-server > dev: StillsGenerator

---
+++ packages/plurid-react-server/source/objects/StillsGenerator/index.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+
+import {
+    StillsGeneratorOptions,
+} from '../../data/interfaces';
+
+
+
+class StillsGenerator {
+    private options: StillsGeneratorOptions;
+
+    constructor(
+        options: Partial<StillsGeneratorOptions>,
+    ) {
+        this.options = this.resolveOptions(options);
+    }
+
+    resolveOptions(
+        options: Partial<StillsGeneratorOptions>,
+    ) {
+        const stillsGeneratorOptions: StillsGeneratorOptions = {
+            serverSource: options.serverSource ?? './source/server/index.ts',
+            serverBuild: options.serverBuild ?? './build/server.js',
+            buildDirectory: options.buildDirectory ?? './build/',
+        };
+
+        return stillsGeneratorOptions;
+    }
+
+    initialize() {
+        const serverSourcePath = path.resolve(process.cwd(), this.options.serverSource);
+        const serverBuildPath = path.resolve(process.cwd(), this.options.serverBuild);
+        const buildDirectoryPath = path.resolve(process.cwd(), this.options.buildDirectory);
+        console.log('serverSourcePath', serverSourcePath);
+        console.log('serverBuildPath', serverBuildPath);
+        console.log('buildDirectoryPath', buildDirectoryPath);
+    }
+}
+
+
+export default StillsGenerator;
