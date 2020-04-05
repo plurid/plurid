1a454f5ebc07443e09c1b3c783839acc495b2ebb
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-05 21:52:28 +0300

    plurid-react-server > dev: exports

--- packages/plurid-react-server/source/index.ts
+++ packages/plurid-react-server/source/index.ts
@@ -1,8 +1,14 @@
 import PluridServer from './objects/Server';
 
+import PluridStillsGenerator from './objects/StillsGenerator';
+
 
 
 export * from './data/interfaces';
+
+export {
+    PluridStillsGenerator,
+}
 
 
 export default PluridServer;
