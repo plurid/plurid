06ba0a5f6c61800d83a85055ce72d60d2a172d60
Jan Cavel <caveljan@gmail.com>
2020-04-05 20:03:51 +0300

    plurid-react-server > setup: package

--- packages/plurid-react-server/rollup.config.js
+++ packages/plurid-react-server/rollup.config.js
@@ -6,7 +6,7 @@ import babel from 'rollup-plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import terser from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
