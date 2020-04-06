d81ff2a2210ff19286b626bbb0fb4ee6f312e4de
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-06 20:33:46 +0300

    plurid-react-server > fixture: App, manifest, service-worker

--- fixtures/plurid-react-server-typescript/source/client/index.tsx
+++ fixtures/plurid-react-server-typescript/source/client/index.tsx
@@ -5,6 +5,11 @@ import Client from './Client';
 
 
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js');
+}
+
+
 ReactDOM.hydrate(
     <Client />,
     document.getElementById('plurid-app'),
