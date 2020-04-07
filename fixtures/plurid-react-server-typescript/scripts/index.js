434c73c430ff5ddfece7f4803a4908a9b83b2c0b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-07 13:36:53 +0300

    plurid-react-server > fixture: package

--- fixtures/plurid-react-server-typescript/scripts/index.js
+++ fixtures/plurid-react-server-typescript/scripts/index.js
@@ -13,6 +13,7 @@ const {
 /** CONSTANTS */
 const command = process.argv[2];
 const buildFolder = path.join(process.cwd(), 'build');
+const verbose = process.argv[3] === 'verbose' ? 'inherit' : undefined;
 
 
 
