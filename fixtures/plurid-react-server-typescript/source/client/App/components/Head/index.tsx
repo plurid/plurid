cb15d8ce30fa1533b0b1163a49e580bec43bcfa6
Jan Cavel <caveljan@gmail.com>
2020-04-08 14:02:27 +0300

    plurid-react-server > fixture: client

--- fixtures/plurid-react-server-typescript/source/client/App/components/Head/index.tsx
+++ fixtures/plurid-react-server-typescript/source/client/App/components/Head/index.tsx
@@ -74,7 +74,7 @@ const Head: React.FC<HeadProperties> = (
 
             {/* SAFARI */}
             <meta name="apple-mobile-web-app-capable" content="yes" />
-            <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
             <meta name="apple-mobile-web-app-title" content={titleValue} />
             <link rel="apple-touch-icon" href="/icon-192x192.png" />
         </Helmet>
