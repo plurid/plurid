7c1a1d121fb65d45745f1e3945ab71b3c5c569d2
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-08 13:05:44 +0300

    plurid-react-server > fixture: public | service worker

--- fixtures/plurid-react-server-typescript/source/public/service-worker.js
+++ fixtures/plurid-react-server-typescript/source/public/service-worker.js
@@ -1,31 +1,41 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(async function() {
-        const cache = await caches.open('static-v1');
-        await cache.addAll([
-            /** Specify the URLs to be cached. */
-            /** Default static */
-            '/favicon.ico',
-            'icon-192x192.png',
-            'icon-512x512.png',
-            '/vendor.js',
+const CACHE_NAME_STATIC = 'static-v1';
+const CACHE_URLS_STATIC = [
+    /** Specify the URLs to be cached. */
+    /** Default static */
+    '/favicon.ico',
+    '/icon-192x192.png',
+    '/icon-512x512.png',
+    '/vendor.js',
+];
+const CACHE_NAME_ROUTES = 'routes-v1';
+const CACHE_URLS_ROUTES = [
+    /** Default routes */
+    // '/',
+];
 
-            /** Default routes */
-            // '/',
-        ]);
+
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(async () => {
+        const cache = await caches.open(CACHE_NAME_STATIC);
+        await cache.addAll(CACHE_URLS_STATIC);
+
+        const cache = await caches.open(CACHE_NAME_ROUTES);
+        await cache.addAll(CACHE_URLS_ROUTES);
     }());
 });
 
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(async function() {
+    event.waitUntil(async () => {
         const cacheNames = await caches.keys();
         await Promise.all(
             cacheNames.filter((cacheName) => {
                 /**
-                 * Return true if you want to remove this cache,
-                 * but remember that caches are shared across
-                 * the whole origin
+                 * Return true to pass the filter check.
+                 * Additional logic can be implemented.
                  */
+                return true;
             }).map(cacheName => caches.delete(cacheName))
         );
     }());
@@ -33,10 +43,32 @@ self.addEventListener('activate', (event) => {
 
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(async function() {
-      const cache = await caches.open('static-v1');
+    event.respondWith(async () => {
+        const cache = await caches.open(CACHE_NAME_STATIC);
+
         const cachedResponse = await cache.match(event.request);
-        if (cachedResponse) return cachedResponse;
+        if (cachedResponse) {
+            return cachedResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+
+        /** Uncomment to add any other unspecified requests to cache. */
+        // event.waitUntil(
+        //     cache.put(event.request, networkResponse.clone())
+        // );
+
+        return networkResponse;
+    }());
+
+    event.respondWith(async () => {
+        const cache = await caches.open(CACHE_NAME_ROUTES);
+
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+            return cachedResponse;
+        }
+
         const networkResponse = await fetch(event.request);
 
         /** Uncomment to add any other unspecified requests to cache. */
