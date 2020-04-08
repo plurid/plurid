af81f406832dd82c1120d62135d205b86e6fc640
Jan Cavel <caveljan@gmail.com>
2020-04-08 09:32:55 +0300

    plurid-react-server > fixture: scripts

--- fixtures/plurid-react-server-typescript/scripts/workings/stills.js
+++ fixtures/plurid-react-server-typescript/scripts/workings/stills.js
@@ -11,21 +11,25 @@ const {
 
 
 const main = () => {
-    const BUILD_DIRECTORY = process.env.PLURID_BUILD_DIRECTORY || 'build';
-    const buildDirectoryPath = path.join(process.cwd(), BUILD_DIRECTORY);
+    const buildDirectory = process.env.PLURID_BUILD_DIRECTORY || 'build';
+    const buildDirectoryPath = path.join(process.cwd(), buildDirectory);
 
     if (!existsSync(buildDirectoryPath)) {
         console.log('\n\tBuild directory does not exist. Run a project build process first, e.g. build.production.');
         return;
     }
 
-    const serverFile = path.join(process.cwd(), `${BUILD_DIRECTORY}/server.js`);
-    if (!existsSync(serverFile)) {
+    const serverFile = `${buildDirectory}/server.js`;
+    const serverFilePath = path.join(process.cwd(), serverFile);
+    if (!existsSync(serverFilePath)) {
         console.log('\n\tServer not built. Run a server build process first, e.g. build.server.production.');
         return;
     }
 
-    const stillsGenerator = new PluridStillsGenerator();
+    const stillsGenerator = new PluridStillsGenerator({
+        build: buildDirectory,
+        server: serverFile,
+    });
     stillsGenerator.initialize();
 }
 
