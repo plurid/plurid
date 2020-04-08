9fb28d5ebe0d420707bf49ddd20cfbf9eb8e5bbd
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-08 09:50:42 +0300

    generate-plurid-app > dev: files > react-typescript-server

--- packages/generate-plurid-app/source/files/react-typescript-server/scripts/workings/client.base.js
+++ packages/generate-plurid-app/source/files/react-typescript-server/scripts/workings/client.base.js
@@ -5,8 +5,10 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 
 /** CONSTANTS */
+const BUILD_DIRECTORY = process.env.PLURID_BUILD_DIRECTORY || 'build';
+
 const entryIndex = path.resolve(__dirname, '../../source/client/index.tsx');
-const outputPath = path.resolve(__dirname, '../../build');
+const outputPath = path.resolve(__dirname, `../../${BUILD_DIRECTORY}`);
 
 
 
--- packages/generate-plurid-app/source/files/react-typescript-server/scripts/workings/stills.js
+++ packages/generate-plurid-app/source/files/react-typescript-server/scripts/workings/stills.js
@@ -11,19 +11,25 @@ const {
 
 
 const main = () => {
-    const buildDirectory = path.join(process.cwd(), 'build');
-    if (!existsSync(buildDirectory)) {
+    const buildDirectory = process.env.PLURID_BUILD_DIRECTORY || 'build';
+    const buildDirectoryPath = path.join(process.cwd(), buildDirectory);
+
+    if (!existsSync(buildDirectoryPath)) {
         console.log('\n\tBuild directory does not exist. Run a project build process first, e.g. build.production.');
         return;
     }
 
-    const serverFile = path.join(process.cwd(), 'build/server.js');
-    if (!existsSync(serverFile)) {
+    const serverFile = `${buildDirectory}/server.js`;
+    const serverFilePath = path.join(process.cwd(), serverFile);
+    if (!existsSync(serverFilePath)) {
         console.log('\n\tServer not built. Run a server build process first, e.g. build.server.production.');
         return;
     }
 
-    const stillsGenerator = new PluridStillsGenerator();
+    const stillsGenerator = new PluridStillsGenerator({
+        build: buildDirectory,
+        server: serverFile,
+    });
     stillsGenerator.initialize();
 }
 
