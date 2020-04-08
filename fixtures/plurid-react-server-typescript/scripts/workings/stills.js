75dea77b6a7f372f9a625b9c54c765bbf29455e3
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-08 09:30:12 +0300

    plurid-react-server > fixture: scripts

--- fixtures/plurid-react-server-typescript/scripts/workings/stills.js
+++ fixtures/plurid-react-server-typescript/scripts/workings/stills.js
@@ -11,13 +11,15 @@ const {
 
 
 const main = () => {
-    const buildDirectory = path.join(process.cwd(), 'build');
-    if (!existsSync(buildDirectory)) {
+    const BUILD_DIRECTORY = process.env.PLURID_BUILD_DIRECTORY || 'build';
+    const buildDirectoryPath = path.join(process.cwd(), BUILD_DIRECTORY);
+
+    if (!existsSync(buildDirectoryPath)) {
         console.log('\n\tBuild directory does not exist. Run a project build process first, e.g. build.production.');
         return;
     }
 
-    const serverFile = path.join(process.cwd(), 'build/server.js');
+    const serverFile = path.join(process.cwd(), `${BUILD_DIRECTORY}/server.js`);
     if (!existsSync(serverFile)) {
         console.log('\n\tServer not built. Run a server build process first, e.g. build.server.production.');
         return;
