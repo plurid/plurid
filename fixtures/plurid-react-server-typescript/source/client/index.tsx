8052074a74c9eba5f3d54a7fd3b7b4603f392e60
Jan Cavel <caveljan@gmail.com>
2020-04-08 12:06:31 +0300

    plurid-react-server > fixture: client

--- fixtures/plurid-react-server-typescript/source/client/index.tsx
+++ fixtures/plurid-react-server-typescript/source/client/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import Client from './Client';
 
 
+
 /** Uncomment to use the service worker caching the static vendor.js and favicons */
 // if ('serviceWorker' in navigator) {
 //     navigator.serviceWorker.register('/service-worker.js');
