71b24ec5f09933424c3c739282437b269ff40f0a
Jan Cavel <caveljan@gmail.com>
2020-04-14 08:21:07 +0300

    plurid-react > dev: PluridPlaneConfigurator

--- packages/plurid-react/source/modules/components/PluridPlaneConfigurator/index.tsx
+++ packages/plurid-react/source/modules/components/PluridPlaneConfigurator/index.tsx
@@ -1,11 +1,21 @@
 import React, {
-    // useState,
+    useRef,
+    useState,
+    useEffect,
 } from 'react';
 import { AnyAction } from 'redux';
 import { connect } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 
 import themes from '@plurid/plurid-themes';
+
+import {
+    general as generalEngine,
+} from '@plurid/plurid-engine';
+
+import {
+    StyledPluridPlaneConfigurator,
+} from './styled';
 
 import { AppState } from '../../services/state/store';
 import StateContext from '../../services/state/context';
@@ -30,9 +40,17 @@ type PluridPlaneConfiguratorProperties = PluridPlaneConfiguratorOwnProperties
 
 type PluridPlaneConfiguratorPropertiesWithChildren = React.PropsWithChildren<PluridPlaneConfiguratorProperties>;
 
+
+/**
+ * Goes up the tree to find the first plurid plane
+ * and update it's properties.
+ *
+ * @param properties
+ */
 const PluridPlaneConfigurator: React.FC<PluridPlaneConfiguratorPropertiesWithChildren> = (
     properties,
 ) => {
+    /** properties */
     const {
         /** own */
         theme,
@@ -42,14 +60,36 @@ const PluridPlaneConfigurator: React.FC<PluridPlaneConfiguratorPropertiesWithChi
         /** dispatch */
     } = properties;
 
-    // go up the tree and find the first plurid plane
-    // listen for changes on the properties
-    // update the plane properties
 
+    /** references */
+    const configuratorElement: React.RefObject<HTMLDivElement> = useRef(null);
+
+
+    /** state */
+    const [planePlaneID, setParentPlaneID] = useState('');
+
+
+    /** effects */
+    /**
+     * Get Parent Plane ID
+     * Get Plurid Link Coordinates
+     */
+    useEffect(() => {
+        const parentPlaneID = generalEngine.plane.getPluridPlaneIDByData(
+            configuratorElement.current,
+        );
+        setParentPlaneID(parentPlaneID);
+    }, []);
+
+
+    console.log('planePlaneID', planePlaneID);
+
+
+    /** render */
     return (
-        <div>
-
-        </div>
+        <StyledPluridPlaneConfigurator
+            ref={configuratorElement}
+        />
     );
 }
 
