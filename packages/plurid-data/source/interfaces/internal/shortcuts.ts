e72dc0fbfadd6776a5ff442530e1fde38799c7b2
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-14 10:32:44 +0300

    plurid-data > dev: interfaces shortcuts

--- packages/plurid-data/source/interfaces/internal/shortcuts.ts
+++ packages/plurid-data/source/interfaces/internal/shortcuts.ts
@@ -1,4 +1,8 @@
 import {
+    SHORTCUTS,
+} from '../../enumerations';
+
+import {
     InternationalizationFieldType,
 } from './internationalization';
 
@@ -12,36 +16,7 @@ export interface ShortcutName {
 }
 
 
-export type ShortcutNamesTypes =
-    | 'TOGGLE_FIRST_PERSON'
-    | 'MOVE_FORWARD'
-    | 'MOVE_BACKWARD'
-    | 'MOVE_LEFT'
-    | 'MOVE_RIGHT'
-    | 'MOVE_UP'
-    | 'MOVE_DOWN'
-    | 'TURN_LEFT'
-    | 'TURN_RIGHT'
-    | 'TURN_UP'
-    | 'TURN_DOWN'
-
-    | 'ROTATE_UP'
-    | 'ROTATE_DOWN'
-    | 'ROTATE_LEFT'
-    | 'ROTATE_RIGHT'
-    | 'TOGGLE_ROTATE'
-
-    | 'TRANSLATE_UP'
-    | 'TRANSLATE_DOWN'
-    | 'TRANSLATE_LEFT'
-    | 'TRANSLATE_RIGHT'
-    | 'TRANSLATE_IN'
-    | 'TRANSLATE_OUT'
-    | 'TOGGLE_TRANSLATE'
-
-    | 'SCALE_UP'
-    | 'SCALE_DOWN'
-    | 'TOGGLE_SCALE';
+export type ShortcutNamesTypes = keyof typeof SHORTCUTS;
 
 
-export type ShortcutNames = Record<ShortcutNamesTypes, ShortcutName>
+export type ShortcutNames = Record<ShortcutNamesTypes, ShortcutName>;
