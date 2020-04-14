944c77c6ec158af549cbbfe36bf63730d3be257d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-14 08:49:46 +0300

    plurid-data > dev: entities constants

---
+++ packages/plurid-data/source/constants/entities.ts
@@ -0,0 +1,6 @@
+export const PLURID_ENTITY_PLANE = 'PluridPlane';
+export const PLURID_ENTITY_PLANE_CONFIGURATOR = 'PluridPlaneConfigurator';
+export const PLURID_ENTITY_LINK = 'PluridLink';
+export const PLURID_ENTITY_ROOT = 'PluridRoot';
+export const PLURID_ENTITY_ROOTS = 'PluridRoots';
+export const PLURID_ENTITY_SPACE = 'PluridSpace';
--- packages/plurid-data/source/constants/index.ts
+++ packages/plurid-data/source/constants/index.ts
@@ -4,6 +4,7 @@ import * as internationalization from './internationalization';
 
 export * from './configuration';
 export * from './defaults';
+export * from './entities';
 export * from './events';
 export * from './keys';
 export * from './shortcuts';
