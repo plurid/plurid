c061b19e8c203e37b632d984c119b5bf13b265d6
Jan Cavel <caveljan@gmail.com>
2020-04-14 20:01:27 +0300

    plurid-data > dev: constants entities

--- packages/plurid-data/source/constants/entities/index.ts
+++ packages/plurid-data/source/constants/entities/index.ts
@@ -4,3 +4,5 @@ export const PLURID_ENTITY_LINK = 'PluridLink';
 export const PLURID_ENTITY_ROOT = 'PluridRoot';
 export const PLURID_ENTITY_ROOTS = 'PluridRoots';
 export const PLURID_ENTITY_SPACE = 'PluridSpace';
+export const PLURID_ENTITY_TOOLBAR = 'PluridToolbar';
+export const PLURID_ENTITY_VIEWCUBE = 'PluridViewcube';
