54c0054aba4921edefd348393ef017d8b301f31e
Jan Cavel <caveljan@gmail.com>
2020-04-17 11:55:59 +0300

    plurid-engine > dev: router

--- packages/plurid-engine/source/modules/router/Router/index.ts
+++ packages/plurid-engine/source/modules/router/Router/index.ts
@@ -70,49 +70,52 @@ export default class Router {
     ) {
         if (location === this.options.gateway) {
             const query = extractQuery(window.location.search);
-            const pathDivisions = pluridLinkPathDivider(query.plurid);
 
-            for (const path of this.paths) {
-                if (
-                    path.value === pathDivisions.path.value
-                    || (path.value === '/' && pathDivisions.path.value === 'p')
-                ) {
-                    console.log('path', path);
-                    if (path.spaces) {
-                        for (const space of path.spaces) {
-                            if (
-                                space.value === pathDivisions.space.value
-                                || (space.value === 'default' && pathDivisions.space.value === 's')
-                            ) {
-                                console.log('space', space);
-                                if (space.universes) {
-                                    for (const universe of space.universes) {
-                                        if (
-                                            universe.value === pathDivisions.universe.value
-                                            || (universe.value === 'default' && pathDivisions.universe.value === 'u')
-                                        ) {
-                                            console.log('universe', universe);
-                                            if (universe.clusters) {
-                                                for (const cluster of universe.clusters) {
-                                                    if (
-                                                        cluster.value === pathDivisions.cluster.value
-                                                        || (cluster.value === 'default' && pathDivisions.cluster.value === 'c')
-                                                    ) {
-                                                        console.log('cluster', cluster);
-                                                        if (cluster.planes) {
-                                                            for (const plane of cluster.planes) {
-                                                                console.log('plane', plane);
-                                                                if (plane.value === pathDivisions.plane.value) {
-                                                                    return {
-                                                                        path,
-                                                                        pathname: path.value,
-                                                                        parameters: {},
-                                                                        query: {},
-                                                                        fragments: {
-                                                                            texts: [],
-                                                                            elements: [],
-                                                                        },
-                                                                    };
+            if (query.plurid) {
+                const pathDivisions = pluridLinkPathDivider(query.plurid);
+
+                for (const path of this.paths) {
+                    if (
+                        path.value === pathDivisions.path.value
+                        || (path.value === '/' && pathDivisions.path.value === 'p')
+                    ) {
+                        console.log('path', path);
+                        if (path.spaces) {
+                            for (const space of path.spaces) {
+                                if (
+                                    space.value === pathDivisions.space.value
+                                    || (space.value === 'default' && pathDivisions.space.value === 's')
+                                ) {
+                                    console.log('space', space);
+                                    if (space.universes) {
+                                        for (const universe of space.universes) {
+                                            if (
+                                                universe.value === pathDivisions.universe.value
+                                                || (universe.value === 'default' && pathDivisions.universe.value === 'u')
+                                            ) {
+                                                console.log('universe', universe);
+                                                if (universe.clusters) {
+                                                    for (const cluster of universe.clusters) {
+                                                        if (
+                                                            cluster.value === pathDivisions.cluster.value
+                                                            || (cluster.value === 'default' && pathDivisions.cluster.value === 'c')
+                                                        ) {
+                                                            console.log('cluster', cluster);
+                                                            if (cluster.planes) {
+                                                                for (const plane of cluster.planes) {
+                                                                    console.log('plane', plane);
+                                                                    if (plane.value === pathDivisions.plane.value) {
+                                                                        return {
+                                                                            path,
+                                                                            pathname: path.value,
+                                                                            parameters: {},
+                                                                            query: {},
+                                                                            fragments: {
+                                                                                texts: [],
+                                                                                elements: [],
+                                                                            },
+                                                                        };
+                                                                    }
                                                                 }
                                                             }
                                                         }
@@ -126,11 +129,27 @@ export default class Router {
                         }
                     }
                 }
+
+                console.log('GATEWAY');
+                console.log('query', query);
+                console.log('pathDivisions', pathDivisions);
+                console.log('paths', this.paths);
             }
-            console.log('GATEWAY');
-            console.log('query', query);
-            console.log('pathDivisions', pathDivisions);
-            console.log('paths', this.paths);
+
+            if (query.plurids) {
+                const split = query.plurids.split(',');
+                const divisions = [];
+
+                for (const plurid of split) {
+                    const pathDivisions = pluridLinkPathDivider(plurid);
+                    divisions.push(pathDivisions);
+                }
+
+                console.log('GATEWAY');
+                console.log('query', query);
+                console.log('divisions', divisions);
+                console.log('paths', this.paths);
+            }
         }
 
         const cached = this.cachedMatched[location];
