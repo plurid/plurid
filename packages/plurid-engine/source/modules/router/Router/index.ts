a0cb6fcfefc51c1de8f6478f97efca266ec101c4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-17 11:04:10 +0300

    plurid-engine > dev: router

--- packages/plurid-engine/source/modules/router/Router/index.ts
+++ packages/plurid-engine/source/modules/router/Router/index.ts
@@ -68,18 +68,60 @@ export default class Router {
     public match(
         location: string,
     ) {
-        // const parsed = new Parser(location, path);
-        // const query = extractQuery(location);
-
-        // console.log('query', query);
-        // console.log('LOCATION', location);
-        // console.log('this.options.gateway', this.options.gateway);
         if (location === this.options.gateway) {
             const query = extractQuery(window.location.search);
             const pathDivisions = pluridLinkPathDivider(query.plurid);
+
+            for (const path of this.paths) {
+                if (
+                    path.value === pathDivisions.path.value
+                    || (path.value === '/' && pathDivisions.path.value === 'p')
+                ) {
+                    console.log('path', path);
+                    if (path.spaces) {
+                        for (const space of path.spaces) {
+                            if (
+                                space.value === pathDivisions.space.value
+                                || (space.value === 'default' && pathDivisions.space.value === 's')
+                            ) {
+                                console.log('space', space);
+                                if (space.universes) {
+                                    for (const universe of space.universes) {
+                                        if (
+                                            universe.value === pathDivisions.universe.value
+                                            || (universe.value === 'default' && pathDivisions.universe.value === 'u')
+                                        ) {
+                                            console.log('universe', universe);
+                                            if (universe.clusters) {
+                                                for (const cluster of universe.clusters) {
+                                                    if (
+                                                        cluster.value === pathDivisions.cluster.value
+                                                        || (cluster.value === 'default' && pathDivisions.cluster.value === 'c')
+                                                    ) {
+                                                        console.log('cluster', cluster);
+                                                        if (cluster.planes) {
+                                                            for (const plane of cluster.planes) {
+                                                                console.log('plane', plane);
+                                                                if (plane.value === pathDivisions.plane.value) {
+                                                                    console.log('planeaa', plane);
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             console.log('GATEWAY');
             console.log('query', query);
             console.log('pathDivisions', pathDivisions);
+            console.log('paths', this.paths);
         }
 
         const cached = this.cachedMatched[location];
