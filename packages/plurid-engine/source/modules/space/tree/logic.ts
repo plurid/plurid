80ab04148b6f7be2d378bda6a59c84e592d88958
Jan Cavel <caveljan@gmail.com>
2020-04-21 14:35:39 +0300

    plurid-engine > dev: cleanup

--- packages/plurid-engine/source/modules/space/tree/logic.ts
+++ packages/plurid-engine/source/modules/space/tree/logic.ts
@@ -268,7 +268,7 @@ export const updateTreeWithNewPlane = (
     tree: TreePlane[],
 ): UpdatedTreeWithNewPlane => {
     const parentPlane = getTreePlaneByPlaneID(tree, parentPlaneID);
-    console.log('parentPlane', parentPlane);
+    // console.log('parentPlane', parentPlane);
 
     if (!parentPlane) {
         return {
@@ -337,7 +337,7 @@ export const updateTreeWithNewPlane = (
         planeAngle: 90,
         linkCoordinates,
     };
-    console.log('newPlane', newPlane);
+    // console.log('newPlane', newPlane);
 
     const updatedParentPlane: TreePlane = {
         ...parentPlane,
