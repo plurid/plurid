f7119710aa09fea8e511745b16c83ae45f618e92
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-22 18:52:58 +0300

    plurid-react > setup: package

---
+++ packages/plurid-react/rollup.development.js
@@ -0,0 +1,51 @@
+// import replace from '@rollup/plugin-replace';
+// import external from 'rollup-plugin-peer-deps-external';
+// import postcss from 'rollup-plugin-postcss';
+// import url from '@rollup/plugin-url';
+// import babel from 'rollup-plugin-babel';
+// import typescript from '@rollup/plugin-typescript';
+// import commonjs from '@rollup/plugin-commonjs';
+// import resolve from '@rollup/plugin-node-resolve';
+
+import {
+    input,
+    plugins,
+} from './rollup.data';
+
+
+
+
+export default [
+    {
+        input,
+        // input: 'source/index.tsx',
+        output: {
+            dir: 'distribution',
+            format: 'cjs',
+            exports: 'named',
+            sourcemap: true
+        },
+        plugins,
+        // plugins: [
+        //     replace({
+        //         'process.env.ENV_MODE': JSON.stringify(process.env.ENV_MODE),
+        //     }),
+        //     external(),
+        //     postcss({
+        //         modules: true,
+        //     }),
+        //     url(),
+        //     babel({
+        //         exclude: 'node_modules/**',
+        //     }),
+        //     typescript({
+        //         declaration: true,
+        //         declarationDir: 'distribution',
+        //     }),
+        //     commonjs(),
+        //     resolve({
+        //         modulesOnly: true,
+        //     }),
+        // ],
+    },
+];
