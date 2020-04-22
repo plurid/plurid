506e55ef477fe5c845a6ae150571fa6b5c9d3eeb
Jan Cavel <caveljan@gmail.com>
2020-04-22 21:38:08 +0300

    plurid-react > dev: PluridPlane | Search

--- packages/plurid-react/source/modules/components/PluridPlane/components/PlaneControls/components/Search/styled.ts
+++ packages/plurid-react/source/modules/components/PluridPlane/components/PlaneControls/components/Search/styled.ts
@@ -6,8 +6,9 @@ export const StyledSearch = styled.div`
     position: absolute;
     left: 0;
     right: 0;
-    top: 36px;
-    border-radius: 15px;
+    top: 47px;
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
     z-index: 9999;
 
     background-color: ${(props: any) => {
@@ -19,16 +20,23 @@ export const StyledSearch = styled.div`
 
     ul {
         padding: 0;
+        margin: 0;
         list-style: none;
     }
 
     li {
         padding: 0.7rem 1.4rem;
+        cursor: pointer;
+    }
+
+    li:last-child {
+        border-bottom-left-radius: 15px;
+        border-bottom-right-radius: 15px;
     }
 
     li:hover {
         background-color: ${(props: any) => {
-            return props.theme.backgroundColorSecondary;
+            return props.theme.backgroundColorPrimary;
         }};
     }
 `;
