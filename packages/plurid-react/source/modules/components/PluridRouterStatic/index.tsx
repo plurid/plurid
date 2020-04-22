19447221be6ee67e4562fea4711f2552ac407c13
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-22 12:08:00 +0300

    plurid-react > dev: PluridRouterStatic, PluridRouterBrowser

--- packages/plurid-react/source/modules/components/PluridRouterStatic/index.tsx
+++ packages/plurid-react/source/modules/components/PluridRouterStatic/index.tsx
@@ -5,18 +5,20 @@ import React, {
 import {
     PluridRouterPath,
     IndexedPluridPlane,
-    PluridPlane,
+    // PluridPlane,
 } from '@plurid/plurid-data';
 
 import {
     router,
-    utilities,
+    // utilities,
 } from '@plurid/plurid-engine';
 
-import PluridApplication from '../../../Application';
+// import PluridApplication from '../../../Application';
 
-import environment from '../../services/utilities/environment';
-
+import {
+    getComponentFromRoute,
+    computeIndexedPlanes,
+} from '../../services/logic/router';
 
 
 const PluridRouter = router.default;
@@ -41,8 +43,8 @@ const PluridRouterStatic = (
         host: hostProperty,
     } = properties;
 
-    const protocol = 'http';
-    const host = 'localhost:63000';
+    const protocol = protocolProperty || 'http';
+    const host = hostProperty || 'localhost:63000';
     // const protocol = protocolProperty
     //     ? protocolProperty
     //     : window.location.protocol.replace(':', '');
@@ -55,112 +57,16 @@ const PluridRouterStatic = (
 
 
     /** references */
-    const indexedPlanes = useRef<Map<string, IndexedPluridPlane>>(new Map());
+    const indexedPlanes = useRef<Map<string, IndexedPluridPlane>>(
+        computeIndexedPlanes(
+            paths,
+            protocol,
+            host,
+        ),
+    );
     const pluridRouter = useRef(new PluridRouter(
         paths,
     ));
-
-    const handleMatchedRoute = (
-        matchedRoute: router.MatcherResponse,
-    ) => {
-        const {
-            path,
-        } = matchedRoute;
-
-        const {
-            exterior,
-            spaces,
-            slotted,
-        } = path;
-
-        let Exterior: React.FC<any> = () => (<></>);
-        if (exterior) {
-            switch (exterior.kind) {
-                case 'elementql':
-                    break;
-                case 'react':
-                    Exterior = exterior.element
-            }
-        }
-
-        let Spaces: React.FC<any> = () => (<></>);
-        const spacesArray: any[] = [];
-        if (spaces) {
-            for (const space of spaces) {
-                // console.log('SPACE', space);
-                const planes: PluridPlane[] = [];
-                const view = [];
-
-                for (const universe of space.universes) {
-                    for (const cluster of universe.clusters) {
-                        for (const plane of cluster.planes) {
-                            const {
-                                component,
-                                value,
-                            } = plane;
-
-                            const pathDivisions = [
-                                protocol,
-                                host,
-                                path.value === '/' ? 'p' : utilities.cleanPathElement(path.value),
-                                space.value === 'default' ? 's' : utilities.cleanPathElement(space.value),
-                                universe.value === 'default' ? 'u' : utilities.cleanPathElement(universe.value),
-                                cluster.value === 'default' ? 'c' : utilities.cleanPathElement(cluster.value),
-                                utilities.cleanPathElement(value),
-                            ];
-                            const fullPath = pathDivisions.join('://');
-                            // console.log('pathDivisions', pathDivisions);
-
-                            if (component.kind === 'react') {
-                                const pluridPlane: PluridPlane = {
-                                    component: {
-                                        element: component.element,
-                                    },
-                                    path: fullPath,
-                                };
-
-                                planes.push(pluridPlane);
-                                view.push(fullPath);
-                            }
-                        }
-                    }
-
-                    // console.log('PLANES', planes);
-
-                    const App = (
-                        <PluridApplication
-                            key={Math.random() + ''}
-                            planes={planes}
-                            indexedPlanes={indexedPlanes.current}
-                            view={view}
-                        />
-                    );
-                    spacesArray.push(App);
-                }
-            }
-
-            Spaces = () => (
-                <>
-                    {spacesArray}
-                </>
-            );
-        }
-
-        const Component = (
-            <>
-                {exterior && (
-                    <Exterior
-                        spaces={slotted ? spacesArray : undefined}
-                    />
-                )}
-
-                {spaces && !slotted && (
-                    <Spaces />
-                )}
-            </>
-        );
-        return Component;
-    }
 
 
     /** render */
@@ -172,7 +78,13 @@ const PluridRouterStatic = (
         );
     }
 
-    const Component = handleMatchedRoute(matchedRoute);
+    // const Component = handleMatchedRoute(matchedRoute);
+    const Component = getComponentFromRoute(
+        matchedRoute,
+        protocol,
+        host,
+        indexedPlanes.current,
+    );
 
     return (
         <>
