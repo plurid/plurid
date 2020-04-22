c533e3ecc3487683fa22afb1eda83e74adb27115
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-22 11:50:04 +0300

    plurid-react > dev: PluridRouterBrowser

--- packages/plurid-react/source/modules/components/PluridRouterBrowser/index.tsx
+++ packages/plurid-react/source/modules/components/PluridRouterBrowser/index.tsx
@@ -100,101 +100,11 @@ const PluridRouterBrowser = (
             '__PLURID_ROUTER__',
         );
 
-        const {
-            path,
-        } = matchedRoute;
-
-        const {
-            exterior,
-            spaces,
-            slotted,
-        } = path;
-
-        let Exterior: React.FC<any> = () => (<></>);
-        if (exterior) {
-            switch (exterior.kind) {
-                case 'elementql':
-                    break;
-                case 'react':
-                    Exterior = exterior.element
-            }
-        }
-
-        let Spaces: React.FC<any> = () => (<></>);
-        const spacesArray: any[] = [];
-        if (spaces) {
-            for (const space of spaces) {
-                // console.log('SPACE', space);
-                const planes: PluridPlane[] = [];
-                const view = [];
-
-                for (const universe of space.universes) {
-                    for (const cluster of universe.clusters) {
-                        for (const plane of cluster.planes) {
-                            const {
-                                component,
-                                value,
-                            } = plane;
-
-                            const pathDivisions = [
-                                protocol,
-                                host,
-                                path.value === '/' ? 'p' : utilities.cleanPathElement(path.value),
-                                space.value === 'default' ? 's' : utilities.cleanPathElement(space.value),
-                                universe.value === 'default' ? 'u' : utilities.cleanPathElement(universe.value),
-                                cluster.value === 'default' ? 'c' : utilities.cleanPathElement(cluster.value),
-                                utilities.cleanPathElement(value),
-                            ];
-                            const fullPath = pathDivisions.join('://');
-                            // console.log('pathDivisions', pathDivisions);
-
-                            if (component.kind === 'react') {
-                                const pluridPlane: PluridPlane = {
-                                    component: {
-                                        element: component.element,
-                                    },
-                                    path: fullPath,
-                                };
-
-                                planes.push(pluridPlane);
-                                view.push(fullPath);
-                            }
-                        }
-                    }
-
-                    // console.log('PLANES', planes);
-
-                    const App = (
-                        <PluridApplication
-                            key={Math.random() + ''}
-                            planes={planes}
-                            indexedPlanes={indexedPlanes.current}
-                            view={view}
-                        />
-                    );
-                    spacesArray.push(App);
-                }
-            }
-
-            Spaces = () => (
-                <>
-                    {spacesArray}
-                </>
-            );
-        }
-
-        const Component = (
-            <>
-                {exterior && (
-                    <Exterior
-                        spaces={slotted ? spacesArray : undefined}
-                    />
-                )}
-
-                {spaces && !slotted && (
-                    <Spaces />
-                )}
-            </>
+        const Component = getComponentFromRoute(
+            matchedRoute,
+            protocol,
+            host,
+            indexedPlanes.current,
         );
 
         setComponent(Component);
