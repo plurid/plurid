64a3f8d129f877dff7656b0629b85288e6e4d884
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-24 11:36:32 +0300

    generate-plurid-app > dev: process > generateReactApp

--- packages/generate-plurid-app/source/process/generateReactApp.ts
+++ packages/generate-plurid-app/source/process/generateReactApp.ts
@@ -419,8 +419,12 @@ const generatePluridReactApplication = async (
 
     const requiredPluridReactPackages = [
         '@plurid/generate-plurid-app',
+        '@plurid/plurid-data',
+        '@plurid/plurid-engine',
         '@plurid/plurid-functions',
+        '@plurid/plurid-functions-react',
         '@plurid/plurid-icons-react',
+        '@plurid/plurid-pubsub',
         '@plurid/plurid-react',
         '@plurid/plurid-themes',
         '@plurid/plurid-ui-react',
