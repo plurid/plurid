e8de759eb5d4ed61a41941fd2a7cc2779695ae38
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-24 18:03:17 +0300

    plurid-data > dev: constants > entities

--- packages/plurid-data/source/constants/entities/index.ts
+++ packages/plurid-data/source/constants/entities/index.ts
@@ -9,5 +9,6 @@ export const PLURID_ENTITY_LINK = 'PluridLink';
 export const PLURID_ENTITY_ROOT = 'PluridRoot';
 export const PLURID_ENTITY_ROOTS = 'PluridRoots';
 export const PLURID_ENTITY_SPACE = 'PluridSpace';
+export const PLURID_ENTITY_MULTISPACE = 'PluridMultispace';
 export const PLURID_ENTITY_TOOLBAR = 'PluridToolbar';
 export const PLURID_ENTITY_VIEWCUBE = 'PluridViewcube';
