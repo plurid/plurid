22b428a29fb1b28244ae07c3fb38fb96ee72f34a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-24 12:17:49 +0300

    fixtures/plurid-react-client-typescript > setup: package

---
+++ fixtures/plurid-react-client-typescript/src/App.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import PluridApp, {
+    PluridPlane,
+    PluridView,
+    SPACE_LAYOUT,
+} from '@plurid/plurid-react';
+
+import Plane from './planes/Plane';
+
+
+
+const App = () => {
+    /** properties */
+    const pluridConfiguration = {
+        theme: 'plurid',
+        space: {
+            layout: {
+                type: SPACE_LAYOUT.COLUMNS,
+                columns: 2,
+                gap: 0.1,
+            },
+            center: true,
+        },
+        elements: {
+            plane: {
+                width: 0.5,
+            },
+        },
+    };
+
+    const pluridPlanes: PluridPlane[] = [
+        {
+            path: '/plane',
+            component: {
+                kind: 'react',
+                element: Plane,
+                properties: {},
+            },
+        },
+    ];
+
+    const pluridView: PluridView[] = [
+        {
+            path: '/plane',
+        }
+    ];
+
+
+    /** render */
+    return (
+        <div>
+            <PluridApp
+                configuration={pluridConfiguration}
+                planes={pluridPlanes}
+                view={pluridView}
+            />
+        </div>
+    );
+}
+
+
+export default App;
---
+++ fixtures/plurid-react-client-typescript/src/planes/Plane/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import {
+    PluridLink,
+} from '@plurid/plurid-react';
+
+import {
+    StyledPlane,
+} from './styled';
+
+import pluridLogo from '../../plurid-logo.png';
+
+
+
+
+const Plane: React.FC<any> = (
+    properties,
+) => {
+    /** properties */
+    // const {
+    //     plurid,
+    // } = properties;
+
+
+    /** render */
+    return (
+        <StyledPlane>
+            <div>
+                <a
+                    href="https://plurid.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src={pluridLogo} alt="plurid logo" height={150} />
+                </a>
+            </div>
+
+            <h1>
+                enjoy the plurid' exploration
+            </h1>
+
+            <div>
+                <PluridLink page="/page">self link</PluridLink>
+            </div>
+        </StyledPlane>
+    );
+}
+
+
+export default Plane;
