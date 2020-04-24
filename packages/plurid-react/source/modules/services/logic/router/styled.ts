c764b5b3ff95ba0edc96d77fc8f7a6913c06d357
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-24 18:31:29 +0300

    plurid-react > dev: logic > router

--- packages/plurid-react/source/modules/services/logic/router/styled.ts
+++ packages/plurid-react/source/modules/services/logic/router/styled.ts
@@ -2,8 +2,40 @@ import styled from 'styled-components';
 
 
 
-export const StyledSpaces = styled.div`
-    scroll-snap-type: y mandatory;
-    height: 100vh;
+interface IStyledSpaces {
+    alignment: any;
+    snapType: any;
+}
+
+export const StyledSpaces = styled.div<IStyledSpaces>`
+    scroll-snap-type: ${(properties: IStyledSpaces) => {
+        const {
+            alignment,
+            snapType,
+        } = properties;
+
+        if (snapType === 'none') {
+            return 'none';
+        }
+
+        return alignment + ' ' + snapType;
+    }};
+
+    flex-direction: ${(properties: IStyledSpaces) => {
+        const {
+            alignment,
+        } = properties;
+
+        if (alignment === 'x') {
+            return 'row';
+        }
+
+        return 'column';
+    }};
+
+    outline: none;
     overflow: auto;
+    display: flex;
+    height: 100vh;
+    width: 100vw;
 `;
