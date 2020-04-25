d61a8b481a44086ea5231634d44b61e43d27cfa3
Jan Cavel <caveljan@gmail.com>
2020-04-25 15:22:27 +0300

    plurid-engine > tests: router

--- packages/plurid-engine/source/modules/router/utilities/__tests__/resolveAbsolutePluridLinkPath.test.ts
+++ packages/plurid-engine/source/modules/router/utilities/__tests__/resolveAbsolutePluridLinkPath.test.ts
@@ -8,16 +8,14 @@ describe('resolveAbsolutePluridLinkPath', () => {
     it('handles plane', () => {
         const link = '/plane-one';
         const result = resolveAbsolutePluridLinkPath(link);
-        console.log(result);
 
-        expect(result).toEqual('http://localhost://p://s://u://c://plane-one');
+        expect(result?.resolvedPath).toEqual('http://localhost://p://s://u://c://plane-one');
     });
 
     it('handles static path', () => {
         const link = 'http://localhost:3000://static';
         const result = resolveAbsolutePluridLinkPath(link);
-        console.log(result);
 
-        expect(result).toEqual('http://localhost:3000://static');
+        expect(result?.resolvedPath).toEqual('http://localhost:3000://static');
     });
 });
