89b39163dec2a47c206b6370dc855c4a7c3ceb3e
Jan Cavel <caveljan@gmail.com>
2020-04-25 17:05:24 +0300

    fixtures/plurid-react-javascript-server > setup: package

--- fixtures/plurid-react-javascript-server/configurations/.eslintrc.js
+++ fixtures/plurid-react-javascript-server/configurations/.eslintrc.js
@@ -6,20 +6,15 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended"
     ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
-    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
     "rules": {
         "no-unused-vars": 0
     }
