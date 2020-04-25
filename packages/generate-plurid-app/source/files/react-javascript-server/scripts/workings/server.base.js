ff3f9c1408efcaffa9e378c9ce391b469949e847
Jan Cavel <caveljan@gmail.com>
2020-04-25 22:01:31 +0300

    generate-plurid-app > dev: files

--- packages/generate-plurid-app/source/files/react-javascript-server/scripts/workings/server.base.js
+++ packages/generate-plurid-app/source/files/react-javascript-server/scripts/workings/server.base.js
@@ -1,4 +1,7 @@
 const svg = require('rollup-plugin-svg');
+const postcss = require('rollup-plugin-postcss');
+const image = require('@rollup/plugin-image');
+const url = require('@rollup/plugin-url');
 const typescript = require('rollup-plugin-typescript2');
 const babel = require('rollup-plugin-babel');
 const external = require('rollup-plugin-peer-deps-external');
@@ -21,6 +24,13 @@ const output = [
 
 const plugins = {
     svg: () => svg(),
+    postcss: () => postcss(),
+    image: () => image(),
+    url: () => url({
+        include: [
+            '**/*.woff', '**/*.ttf', '**/*.wav', '**/*.mp3', '**/*.pdf', '**/*.mov', '**/*.mp4',
+        ],
+    }),
     typescript: () => typescript({
         tsconfig: './tsconfig.json',
     }),
