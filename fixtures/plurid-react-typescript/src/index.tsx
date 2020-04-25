470334355b80a3f7ce1596e08739c9861d207645
Jan Cavel <caveljan@gmail.com>
2020-04-25 09:46:27 +0300

    fixtures/plurid-react-typescript > dev: App

--- fixtures/plurid-react-typescript/src/index.tsx
+++ fixtures/plurid-react-typescript/src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-// import App from './App-simple-router';
+import App from './App-simple-router';
 // import App from './App-simple-router-parametric';
-import App from './App-simple';
+// import App from './App-simple';
 
 import * as serviceWorker from './serviceWorker';
 
