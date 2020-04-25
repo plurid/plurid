33ff4d27a0741867ebb2004e20191a8e824643f8
Jan Cavel <caveljan@gmail.com>
2020-04-25 08:16:14 +0300

    plurid-engine > dev: general, router

--- packages/plurid-engine/source/modules/general/helpers/identified.ts
+++ packages/plurid-engine/source/modules/general/helpers/identified.ts
@@ -12,34 +12,34 @@ import {
 
 
 
-export const identifyPlanes = (
-    pages: PluridPlane[],
-): Identified<PluridPlane>[] => {
-    return indexing.identify(pages) as Identified<PluridPlane>[];
+// export const identifyPlanes = (
+//     pages: PluridPlane[],
+// ): Identified<PluridPlane>[] => {
+//     return indexing.identify(pages) as Identified<PluridPlane>[];
 
-    // const identifiedPlanes = pages.map(page => {
-    //     const updatedPlane: Identified<PluridPlane> = {
-    //         ...page,
-    //         id: page.id || uuid(),
-    //     };
-    //     return updatedPlane;
-    // });
-    // return identifiedPlanes;
-}
+//     // const identifiedPlanes = pages.map(page => {
+//     //     const updatedPlane: Identified<PluridPlane> = {
+//     //         ...page,
+//     //         id: page.id || uuid(),
+//     //     };
+//     //     return updatedPlane;
+//     // });
+//     // return identifiedPlanes;
+// }
 
 
-export const identifyUniverses = (
-    documents: PluridUniverse[],
-): IdentifiedPluridUniverse[] => {
-    return indexing.identify(documents) as IdentifiedPluridUniverse[];
+// export const identifyUniverses = (
+//     documents: PluridUniverse[],
+// ): IdentifiedPluridUniverse[] => {
+//     return indexing.identify(documents) as IdentifiedPluridUniverse[];
 
-    // const identifiedUniverses = documents.map(document => {
-    //     const updatedUniverse: IdentifiedPluridUniverse = {
-    //         ...document,
-    //         pages: identifyPlanes(document.pages),
-    //         id: document.id || uuid(),
-    //     };
-    //     return updatedUniverse;
-    // });
-    // return identifiedUniverses;
-}
+//     // const identifiedUniverses = documents.map(document => {
+//     //     const updatedUniverse: IdentifiedPluridUniverse = {
+//     //         ...document,
+//     //         pages: identifyPlanes(document.pages),
+//     //         id: document.id || uuid(),
+//     //     };
+//     //     return updatedUniverse;
+//     // });
+//     // return identifiedUniverses;
+// }
