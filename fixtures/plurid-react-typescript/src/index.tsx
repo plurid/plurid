da68c5c35db1ac3d638a342b9c5a837549fc5e3f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-26 13:46:16 +0300

    fixtures/plurid-react-typescript > dev: App

--- fixtures/plurid-react-typescript/src/index.tsx
+++ fixtures/plurid-react-typescript/src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-import App from './App-simple-router';
+// import App from './App-simple-router';
+import App from './App-simple-router-link';
 // import App from './App-simple-router-parametric';
 // import App from './App-simple';
+// import App from './App-simple-link';
 
 import * as serviceWorker from './serviceWorker';
 
