0b63a5d131388e140ca46d4fb43a37340e0200b1
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-05-06 13:17:02 +0300

    plurid-react > dev: PluridLink

--- packages/plurid-react/source/modules/components/PluridLink/index.tsx
+++ packages/plurid-react/source/modules/components/PluridLink/index.tsx
@@ -118,7 +118,7 @@ const PluridLink: React.FC<React.PropsWithChildren<PluridLinkProperties>> = (
     const previewAppearTime = PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_FADE_IN;
     const previewDisappearTime = PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_FADE_OUT;
 
-    const planePathResolved = planePath.startsWith('/') && window
+    const planePathResolved = planePath.startsWith('/') && typeof window !== 'undefined'
         ? ':/' + window.location.pathname + '://s://u://c://' + planePath.slice(1)
         : planePath;
 
