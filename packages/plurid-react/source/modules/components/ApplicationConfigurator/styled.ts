1339738019b0f99ef85b983eb8e15fe7a76e2e34
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-05-19 14:22:44 +0300

    plurid-react > dev: ApplicationConfigurator, PlaneConfigurator

--- packages/plurid-react/source/modules/components/ApplicationConfigurator/styled.ts
+++ packages/plurid-react/source/modules/components/ApplicationConfigurator/styled.ts
@@ -4,4 +4,8 @@ import styled from 'styled-components';
 
 export const StyledPluridApplicationConfigurator = styled.div`
     position: absolute;
+    height: 0;
+    width: 0;
+    user-select: none;
+    pointer-events: none;
 `;
