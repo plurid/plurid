8406d5b63192842114a1204bdf3ba7dd0ed7370e
Jan Cavel <caveljan@gmail.com>
2020-05-19 16:00:41 +0300

    fixtures/plurid-react-typescript-server > dev: server

--- fixtures/plurid-react-typescript-server/source/server/handlers/index.ts
+++ fixtures/plurid-react-typescript-server/source/server/handlers/index.ts
@@ -5,8 +5,10 @@ import PluridServer from '@plurid/plurid-react-server';
 export const setRouteHandlers = (
     server: PluridServer,
 ) => {
+    const handler = server.handle();
+
     // mock-up API request handler
-    server.handle().post('/api/v1/status', (request, response, next) => {
+    handler.post('/api/v1/status', (request, response, next) => {
         response.setHeader('Content-Type', 'application/json');
         response.end(
             JSON.stringify(
