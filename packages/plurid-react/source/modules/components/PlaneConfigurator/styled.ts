ef1d9aafb6efe6b412e0ddddba993353835ead4a
Jan Cavel <caveljan@gmail.com>
2020-05-19 14:22:44 +0300

    plurid-react > dev: ApplicationConfigurator, PlaneConfigurator

--- packages/plurid-react/source/modules/components/PlaneConfigurator/styled.ts
+++ packages/plurid-react/source/modules/components/PlaneConfigurator/styled.ts
@@ -4,4 +4,8 @@ import styled from 'styled-components';
 
 export const StyledPluridPlaneConfigurator = styled.div`
     position: absolute;
+    height: 0;
+    width: 0;
+    user-select: none;
+    pointer-events: none;
 `;
