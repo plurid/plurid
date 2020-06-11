a6087a445822a0edac819a24217efcdcf4c28738
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-11 12:22:02 +0300

    fixtures/plurid-react-typescript > dev: App-parametric

---
+++ fixtures/plurid-react-typescript/src/App-parametric-link.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import {
+    PluridApplication,
+    PluridPlane,
+    PluridLink,
+} from '@plurid/plurid-react';
+
+
+
+const App = () => {
+    const pluridPlanes: PluridPlane[] = [
+        {
+            path: '/:id',
+            component: {
+                kind: 'react',
+                element: ({ plurid} ) => {
+                    console.log('plurid', plurid);
+
+                    return (
+                        <div>
+                            <h1>Plane 1</h1>
+                            <br />
+                            <PluridLink path="/plane-2">link to plane 2</PluridLink>
+                        </div>
+                    );
+                },
+            },
+        },
+    ];
+
+    const pluridView: string[] = [
+        '/one',
+    ];
+
+    return (
+        <PluridApplication
+            planes={pluridPlanes}
+            view={pluridView}
+        />
+    );
+}
+
+
+export default App;
