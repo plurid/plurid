e5d7cc5e33e66b14cf03fff5d72f3c08c2d7ede1
Jan Cavel <caveljan@gmail.com>
2020-06-13 15:23:24 +0300

    plurid-data > dev: interfaces

--- packages/plurid-data/source/interfaces/external/view/index.ts
+++ packages/plurid-data/source/interfaces/external/view/index.ts
@@ -1,15 +1,17 @@
 export interface PluridView {
     /**
-     * Path to a `PluridPage`.
+     * Route to a `PluridPlane`.
      *
-     * The path can be URL-like, e.g. `'/page/one?query=true'`, or the page id.
+     * The route can be URL-like, e.g. `'/plane/one?query=true'`,
+     * for a plane in the same space, or fully defined,
+     * e.g. `'protocol://host://path://space://universe://cluster://plane`.
      */
-    path: string;
+    plane: string;
 
     /**
-     * By default, the order the pages are shown in is based on their index in the `view[]`.
+     * By default, the order the planes are shown in is based on their index in the `view[]`.
      * The ordinal can be used to overrule the default order.
-     * If not unique, the pages with equal `ordinal` will be ordered by index.
+     * If not unique, the planes with equal `ordinal` will be ordered by index.
      *
      * 0-based.
      */
