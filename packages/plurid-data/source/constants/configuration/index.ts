e9d190788ce3ff2961939ef1ae16659f39980b9b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-14 09:59:30 +0300

    plurid-data > dev: constants, interfaces

--- packages/plurid-data/source/constants/configuration/index.ts
+++ packages/plurid-data/source/constants/configuration/index.ts
@@ -1,5 +1,10 @@
 import {
     PluridConfiguration,
+    PluridConfigurationGlobal,
+    PluridConfigurationElements,
+    PluridConfigurationSpace,
+    PluridConfigurationNetwork,
+    PluridConfigurationDevelopment,
 } from '../../interfaces';
 
 import {
@@ -11,17 +16,20 @@ import {
 
 import {
     PLURID_DEFAULT_CONFIGURATION_SPACE_CULLING_DISTANCE,
+
     PLURID_DEFAULT_CONFIGURATION_LINK_SUFFIX,
     PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_SHOW,
     PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_FADE_IN,
     PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_FADE_OUT,
     PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_OFFSET_X,
     PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_OFFSET_Y,
+
+    PLURID_DEFAULT_CONFIGURATION_NETWORK_HOST,
 } from '../defaults';
 
 
 
-export const defaultConfiguration: PluridConfiguration = {
+export const defaultConfigurationGlobal: PluridConfigurationGlobal = {
     micro: false,
     theme: {
         general: 'plurid',
@@ -29,75 +37,106 @@ export const defaultConfiguration: PluridConfiguration = {
     },
     language: 'english',
     transparentUI: false,
+    render: 'plurid',
+};
+
+
+export const defaultConfigurationElements: PluridConfigurationElements = {
+    toolbar: {
+        show: true,
+        opaque: true,
+        conceal: false,
+        transformIcons: false,
+        transformButtons: false,
+        toggledDrawers: [],
+    },
+    viewcube: {
+        show: true,
+        opaque: true,
+        conceal: false,
+        buttons: true,
+    },
+    plane: {
+        width: 1,
+        opacity: 1,
+        controls: {
+            show: true,
+            pathbar: {
+                domainURL: true,
+            },
+        },
+    },
+    link: {
+        suffix: PLURID_DEFAULT_CONFIGURATION_LINK_SUFFIX,
+        preview: {
+            show: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_SHOW,
+            fadeIn: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_FADE_IN,
+            fadeOut: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_FADE_OUT,
+            offsetX: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_OFFSET_X,
+            offsetY: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_OFFSET_Y,
+        },
+    },
+    switch: {
+        show: false,
+    },
+};
+
+
+export const defaultConfigurationSpace: PluridConfigurationSpace = {
+    layout: {
+        type: LAYOUT_TYPES.COLUMNS,
+        columns: 2,
+    },
+    perspective: 2000,
+    opaque: true,
+    center: false,
+    transformOrigin: {
+        show: true,
+        size: SIZES.NORMAL,
+    },
+    transformLocks: {
+        rotationX: true,
+        rotationY: true,
+        translationY: true,
+        translationX: true,
+        translationZ: true,
+        scale: true,
+    },
+    transformMode: TRANSFORM_MODES.ALL,
+    transformMultimode: false,
+    transformTouch: TRANSFORM_TOUCHES.PAN,
+    firstPerson: false,
+    cullingDistance: PLURID_DEFAULT_CONFIGURATION_SPACE_CULLING_DISTANCE,
+};
+
+
+export const defaultConfigurationNetwork: PluridConfigurationNetwork = {
+    protocol: 'https',
+    host: PLURID_DEFAULT_CONFIGURATION_NETWORK_HOST,
+};
+
+
+export const defaultConfigurationDevelopment: PluridConfigurationDevelopment = {
+    planeDebugger: false,
+    spaceDebugger: false,
+};
+
+
+export const defaultConfiguration: PluridConfiguration = {
+    global: {
+        ...defaultConfigurationGlobal,
+    },
     elements: {
-        toolbar: {
-            show: true,
-            opaque: true,
-            conceal: false,
-            transformIcons: false,
-            transformButtons: false,
-            toggledDrawers: [],
-        },
-        viewcube: {
-            show: true,
-            opaque: true,
-            conceal: false,
-            buttons: true,
-        },
-        plane: {
-            width: 1,
-            opacity: 1,
-            controls: {
-                show: true,
-                pathbar: {
-                    domainURL: true,
-                },
-            },
-        },
-        link: {
-            suffix: PLURID_DEFAULT_CONFIGURATION_LINK_SUFFIX,
-            preview: {
-                show: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_SHOW,
-                fadeIn: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_FADE_IN,
-                fadeOut: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_FADE_OUT,
-                offsetX: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_OFFSET_X,
-                offsetY: PLURID_DEFAULT_CONFIGURATION_LINK_PREVIEW_OFFSET_Y,
-            },
-        },
-        switch: {
-            show: false,
-        },
+        ...defaultConfigurationElements,
     },
     space: {
-        layout: {
-            type: LAYOUT_TYPES.COLUMNS,
-            columns: 2,
-        },
-        perspective: 2000,
-        opaque: true,
-        center: false,
-        transformOrigin: {
-            show: true,
-            size: SIZES.NORMAL,
-        },
-        transformLocks: {
-            rotationX: true,
-            rotationY: true,
-            translationY: true,
-            translationX: true,
-            translationZ: true,
-            scale: true,
-        },
-        transformMode: TRANSFORM_MODES.ALL,
-        transformMultimode: false,
-        transformTouch: TRANSFORM_TOUCHES.PAN,
-        firstPerson: false,
-        cullingDistance: PLURID_DEFAULT_CONFIGURATION_SPACE_CULLING_DISTANCE,
+        ...defaultConfigurationSpace,
     },
-    render: 'plurid',
+    network: {
+        ...defaultConfigurationNetwork,
+    },
     development: {
-        planeDebugger: false,
-        spaceDebugger: false,
+        ...defaultConfigurationDevelopment,
     },
 };
 
