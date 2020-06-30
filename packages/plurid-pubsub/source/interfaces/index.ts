331d0cc860cbc14d631b46b0de4e4f915a8ce987
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-30 11:00:47 +0300

    plurid-pubsub > dev: PluridPubSub

--- packages/plurid-pubsub/source/interfaces/index.ts
+++ packages/plurid-pubsub/source/interfaces/index.ts
@@ -1,11 +1,21 @@
 export interface IPluridPubSub {
-    publish: (topic: string, data: any) => void;
-    subscribe: (topic: string, callback: (data: any) => void) => void;
-    unsubscribe: (topic: string) => boolean;
+    publish: (
+        topic: string,
+        data: any,
+    ) => void;
+    subscribe: (
+        topic: string,
+        callback: (data: any) => void,
+    ) => void;
+    unsubscribe: (
+        topic: string,
+    ) => boolean;
 }
 
 
 export interface Subscription {
     topic: string;
-    callback: (data: any) => void;
+    callback: (
+        data: any,
+    ) => void;
 }
