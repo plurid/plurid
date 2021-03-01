2e870d66525ced381aa3a22753f13f54d6cdf294
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-03-01 22:58:50 +0200

    plurid-web/plurid-core/plurid-routes-server > tests: index

--- packages/plurid-web/plurid-core/plurid-routes-server/tests/index.js
+++ packages/plurid-web/plurid-core/plurid-routes-server/tests/index.js
@@ -1,5 +1,30 @@
 const PluridRoutesServer = require('../distribution').default;
 
 
-const server = new PluridRoutesServer();
+
+const queryRoute = async (
+    route,
+) => {
+    console.log('queryRoute', route);
+
+    return {
+        elementql: '/path/to/element',
+    };
+}
+
+const registerRoute = async (
+    route,
+    data,
+) => {
+    console.log('registerRoute', route, data);
+
+    return true;
+}
+
+
+const server = new PluridRoutesServer({
+    queryRoute,
+    registerRoute,
+});
+
 server.start();
