8f69e6a047c61405011220a4edaff86d71bc96e1
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-04-27 14:30:04 +0300

    fixtures/plurid-react-typescript > dev: source

--- fixtures/plurid-react-typescript/src/index.tsx
+++ fixtures/plurid-react-typescript/src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
@@ -9,9 +8,9 @@ import './index.css';
 // import App from '/apps/App-simple-router-link';
 // import App from '/apps/App-simple-router-parametric';
 // import App from '/apps/App-simple-router-config';
-// import App from './apps/App-simple';
+import App from './apps/App-simple';
 // import App from './apps/App-simple-multiview';
-import App from './apps/App-simple-parametric';
+// import App from './apps/App-simple-parametric';
 // import App from '/apps/App-simple-link';
 // import App from '/apps/App-parametric-link';
 // import App from '/apps/App-transform';
