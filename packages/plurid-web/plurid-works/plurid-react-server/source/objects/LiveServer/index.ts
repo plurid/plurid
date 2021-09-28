243cc25b6ee9c5550ee60500009c1d8c9c2d56ea
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-28 15:37:48 +0300

    plurid-web/plurid-works/plurid-react-server > dev: objects/LiveServer

--- packages/plurid-web/plurid-works/plurid-react-server/source/objects/LiveServer/index.ts
+++ packages/plurid-web/plurid-works/plurid-react-server/source/objects/LiveServer/index.ts
@@ -1,4 +1,16 @@
 // #region imports
+    // #region libraries
+    import {
+        Socket,
+    } from 'net';
+    import http from 'http';
+
+    import express, {
+        Application,
+    } from 'express';
+    // #endregion libraries
+
+
     // #region external
     import {
         PluridLiveServerOptions,
@@ -11,12 +23,21 @@
 // #region module
 class LiveServer {
     private options: PluridLiveServerOptions;
+    private expressServer: Application;
+    private httpServer: http.Server;
+    private sockets: Socket[] = [];
 
 
     constructor(
         options?: Partial<PluridLiveServerOptions>,
     ) {
         this.options = this.resolveOptions(options);
+
+        this.expressServer = express();
+        this.setupExpressServer();
+
+        this.httpServer = http.createServer(this.expressServer);
+        this.setupHttpServer();
     }
 
 
@@ -30,6 +51,24 @@ class LiveServer {
         };
 
         return resolvedOptions;
+    }
+
+    private setupExpressServer() {
+
+    }
+
+    private setupHttpServer() {
+        this.httpServer.on('connection', (socket) => {
+            this.sockets.push(socket);
+
+            socket.once('close', () => {
+                this.sockets.splice(this.sockets.indexOf(socket), 1);
+            });
+        });
+
+        this.httpServer.on('error', (error) => {
+            throw error;
+        });
     }
 
 
