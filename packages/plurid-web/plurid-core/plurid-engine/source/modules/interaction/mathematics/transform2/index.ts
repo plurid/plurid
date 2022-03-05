fff6a8297f20609eab70f2b8aa7204794fb94b55
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-03-05 20:00:21 +0200

    plurid-web/plurid-core/plurid-engine > dev: interaction/mathematics/transform

--- packages/plurid-web/plurid-core/plurid-engine/source/modules/interaction/mathematics/transform2/index.ts
+++ packages/plurid-web/plurid-core/plurid-engine/source/modules/interaction/mathematics/transform2/index.ts
@@ -6,15 +6,15 @@ export const multiplyMatrices = (
     m1: Matrix,
     m2: Matrix,
 ): Matrix => {
-    let result: Matrix = [];
+    const result: Matrix = [];
 
-    for (var i = 0; i < m1.length; i++) {
+    for (let i = 0; i < m1.length; i++) {
         result[i] = [];
 
-        for (var j = 0; j < m2[0].length; j++) {
+        for (let j = 0; j < m2[0].length; j++) {
             let sum = 0;
 
-            for (var k = 0; k < m1[0].length; k++) {
+            for (let k = 0; k < m1[0].length; k++) {
                 sum += m1[i][k] * m2[k][j];
             }
 
