b140a2755bae266aa04adcc16a5a989111e02715
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-03-06 08:53:31 +0200

    fixtures/plurid-react-typescript > dev: apps

--- fixtures/plurid-react-typescript/src/apps/App-simple-link.tsx
+++ fixtures/plurid-react-typescript/src/apps/App-simple-link.tsx
@@ -15,12 +15,32 @@ const App = () => {
                     <h1>Plane 1</h1>
                     <br />
                     <PluridLink route="/plane-2">link to plane 2</PluridLink>
+
+                    <div
+                        style={{
+                            margin: '4rem',
+                        }}
+                    >
+                        <PluridLink route="/plane-2">link to plane 2</PluridLink>
+                    </div>
                 </div>
             ),
         },
         {
             route: '/plane-2',
-            component: () => (<h1>Plane 2</h1>),
+            component: () => (
+                <div>
+                    <h1>Plane 2</h1>
+
+                    <div
+                        style={{
+                            margin: '2rem',
+                        }}
+                    >
+                        <PluridLink route="/plane-1">link to plane 1</PluridLink>
+                    </div>
+                </div>
+            ),
         },
     ];
 
