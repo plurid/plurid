a9c133402c8761bac5ed5d0287a25dd7d0f8603c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-07-25 12:02:17 +0300

    plurid-web/plurid-works/plurid-react > setup: scripts

---
+++ packages/plurid-web/plurid-works/plurid-react/scripts/development.js
@@ -0,0 +1,54 @@
+const esbuild = require('esbuild');
+
+
+
+esbuild.build({
+    entryPoints: [
+        'source/index.tsx',
+    ],
+    outdir: 'distribution',
+    define: {
+        // 'process.env.ENV_MODE': JSON.stringify(process.env.ENV_MODE),
+        'process.env.ENV_MODE': JSON.stringify('local'),
+        global: 'window',
+    },
+    bundle: true,
+    format: 'cjs',
+    assetNames: 'assets/[name]',
+    loader: {
+        '.ttf': 'file',
+        '.png': 'file',
+        '.jpg': 'file',
+        '.jpeg': 'file',
+        '.svg': 'file',
+        '.gif': 'file',
+        '.mov': 'file',
+    },
+    external: [
+        "@plurid/elementql",
+        "@plurid/elementql-client-react",
+        "@plurid/plurid-data",
+        "@plurid/plurid-engine",
+        "@plurid/plurid-functions",
+        "@plurid/plurid-functions-react",
+        "@plurid/plurid-icons-react",
+        "@plurid/plurid-pubsub",
+        "@plurid/plurid-themes",
+        "@plurid/plurid-ui-components-react",
+        "@plurid/plurid-ui-state-react",
+        "cross-fetch",
+        "hammerjs",
+        "react",
+        "react-dom",
+        "react-redux",
+        "redux",
+        "redux-thunk",
+        "styled-components",
+    ],
+    watch: {
+        onRebuild(error, result) {
+            if (error) console.error('watch build failed:', error)
+            else console.log('watch build succeeded')
+        },
+    },
+});
