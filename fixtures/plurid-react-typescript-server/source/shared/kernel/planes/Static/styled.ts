4da3cdb8fa3da2ed4d78a0b52211e6cad1ed493b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-08-01 14:20:36 +0300

    fixtures/plurid-react-typescript-server > dev: source

--- fixtures/plurid-react-typescript-server/source/shared/kernel/planes/Static/styled.ts
+++ fixtures/plurid-react-typescript-server/source/shared/kernel/planes/Static/styled.ts
@@ -11,7 +11,7 @@ export const StyledStatic = styled.div`
     display: grid;
     place-content: center;
     text-align: center;
-    height: 100vh;
+    height: 100%;
 
     a {
         color: #ccc;
