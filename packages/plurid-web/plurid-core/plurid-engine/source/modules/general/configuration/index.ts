da00c8cc6107868dffa6ae08d1d27c3da30db879
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-08-09 14:11:04 +0300

    plurid-web/plurid-core/plurid-engine > dev: general/configuration

--- packages/plurid-web/plurid-core/plurid-engine/source/modules/general/configuration/index.ts
+++ packages/plurid-web/plurid-core/plurid-engine/source/modules/general/configuration/index.ts
@@ -66,275 +66,32 @@ export const merge = (
         return targetConfiguration;
     }
 
+    const mergedConfiguration = objects.merge(
+        targetConfiguration,
+        configuration,
+        {
+            'global.theme.general': () => resolveTheme(configuration.global?.theme, 'general') as any,
+            'global.theme.interaction': () => resolveTheme(configuration.global?.theme, 'interaction') as any,
 
-    const specifiedOrDefault = <T>(
-        path: string,
-        type: string,
-        configuration: PluridPartialConfiguration,
-    ): T => {
-        const item = objects.getNested(configuration, path);
-        if (typeof item === type) {
-            return item;
-        }
+            'elements.toolbar.drawers': () => (configuration.elements
+                && configuration.elements.toolbar
+                && configuration.elements.toolbar.drawers
+                    ? configuration.elements.toolbar.drawers
+                    : targetConfiguration.elements.toolbar.drawers) as (keyof typeof TOOLBAR_DRAWERS)[],
+            'elements.toolbar.toggledDrawers': () => (configuration.elements
+                && configuration.elements.toolbar
+                && configuration.elements.toolbar.toggledDrawers
+                    ? configuration.elements.toolbar.toggledDrawers
+                    : targetConfiguration.elements.toolbar.toggledDrawers) as (keyof typeof TOOLBAR_DRAWERS)[],
+            'elements.switch.show': () => configuration?.elements?.switch?.show ?? false,
 
-        return objects.getNested(targetConfiguration, path);
-    }
-
-
-    /**
-     * HACK:
-     * Bypass RecursivePartial on the `configuration.space.layout` object.
-     */
-    const layout: any = configuration
-        && configuration.space
-        && typeof configuration.space.layout === 'object'
-            ? configuration.space.layout
-            : targetConfiguration.space.layout;
-
-    const mergedConfiguration: PluridConfiguration = {
-        ...targetConfiguration,
-        global: {
-            micro: specifiedOrDefault(
-                'global.micro',
-                'boolean',
-                configuration,
-            ),
-            transparentUI: specifiedOrDefault(
-                'global.transparentUI',
-                'boolean',
-                configuration,
-            ),
-            language: specifiedOrDefault(
-                'global.language',
-                'string',
-                configuration,
-            ),
-            render: specifiedOrDefault(
-                'global.render',
-                'string',
-                configuration,
-            ),
-            theme: {
-                // TODO
-                // fix the Theme type
-                general: resolveTheme(configuration.global?.theme, 'general') as any,
-                interaction: resolveTheme(configuration.global?.theme, 'interaction') as any,
-            },
+            'space.layout': () => configuration
+                && configuration.space
+                && typeof configuration.space.layout === 'object'
+                    ? configuration.space.layout
+                    : targetConfiguration.space.layout,
         },
-        elements: {
-            ...targetConfiguration.elements,
-            toolbar: {
-                show: specifiedOrDefault(
-                    'elements.toolbar.show',
-                    'boolean',
-                    configuration,
-                ),
-                opaque: specifiedOrDefault(
-                    'elements.toolbar.opaque',
-                    'boolean',
-                    configuration,
-                ),
-                conceal: specifiedOrDefault(
-                    'elements.toolbar.conceal',
-                    'boolean',
-                    configuration,
-                ),
-                transformIcons: specifiedOrDefault(
-                    'elements.toolbar.transformIcons',
-                    'boolean',
-                    configuration,
-                ),
-                transformButtons: specifiedOrDefault(
-                    'elements.toolbar.transformButtons',
-                    'boolean',
-                    configuration,
-                ),
-                drawers: (configuration.elements
-                    && configuration.elements.toolbar
-                    && configuration.elements.toolbar.drawers
-                        ? configuration.elements.toolbar.drawers
-                        : targetConfiguration.elements.toolbar.drawers) as (keyof typeof TOOLBAR_DRAWERS)[],
-                toggledDrawers: (configuration.elements
-                    && configuration.elements.toolbar
-                    && configuration.elements.toolbar.toggledDrawers
-                        ? configuration.elements.toolbar.toggledDrawers
-                        : targetConfiguration.elements.toolbar.toggledDrawers) as (keyof typeof TOOLBAR_DRAWERS)[],
-            },
-            viewcube: {
-                show: specifiedOrDefault(
-                    'elements.viewcube.show',
-                    'boolean',
-                    configuration,
-                ),
-                opaque: specifiedOrDefault(
-                    'elements.viewcube.opaque',
-                    'boolean',
-                    configuration,
-                ),
-                conceal: specifiedOrDefault(
-                    'elements.viewcube.conceal',
-                    'boolean',
-                    configuration,
-                ),
-                buttons: specifiedOrDefault(
-                    'elements.viewcube.buttons',
-                    'boolean',
-                    configuration,
-                ),
-            },
-            plane: {
-                width: specifiedOrDefault(
-                    'elements.plane.width',
-                    'number',
-                    configuration,
-                ),
-                opacity: specifiedOrDefault(
-                    'elements.plane.opacity',
-                    'number',
-                    configuration,
-                ),
-                controls: {
-                    show: specifiedOrDefault(
-                        'elements.plane.controls.show',
-                        'boolean',
-                        configuration,
-                    ),
-                    title: specifiedOrDefault(
-                        'elements.plane.controls.title',
-                        'boolean',
-                        configuration,
-                    ),
-                    pathbar: {
-                        domainURL: specifiedOrDefault(
-                            'elements.plane.controls.pathbar.domainURL',
-                            'boolean',
-                            configuration,
-                        ),
-                    },
-                },
-            },
-            switch: {
-                show: configuration?.elements?.switch?.show ?? false,
-            },
-        },
-        space: {
-            ...targetConfiguration.space,
-            layout,
-            camera: specifiedOrDefault(
-                'space.camera',
-                'string',
-                configuration,
-            ),
-            perspective: specifiedOrDefault(
-                'space.perspective',
-                'number',
-                configuration,
-            ),
-            opaque: specifiedOrDefault(
-                'space.opaque',
-                'boolean',
-                configuration,
-            ),
-            fadeInTime: specifiedOrDefault(
-                'space.fadeInTime',
-                'number',
-                configuration,
-            ),
-            center: specifiedOrDefault(
-                'space.center',
-                'boolean',
-                configuration,
-            ),
-            transformOrigin: {
-                show: specifiedOrDefault(
-                    'space.transformOrigin.show',
-                    'boolean',
-                    configuration,
-                ),
-                size: specifiedOrDefault(
-                    'space.transformOrigin.size',
-                    'string',
-                    configuration,
-                ),
-            },
-            transformLocks: {
-                rotationX: specifiedOrDefault(
-                    'space.transformLocks.rotationX',
-                    'boolean',
-                    configuration,
-                ),
-                rotationY: specifiedOrDefault(
-                    'space.transformLocks.rotationY',
-                    'boolean',
-                    configuration,
-                ),
-                translationX: specifiedOrDefault(
-                    'space.transformLocks.translationX',
-                    'boolean',
-                    configuration,
-                ),
-                translationY: specifiedOrDefault(
-                    'space.transformLocks.translationY',
-                    'boolean',
-                    configuration,
-                ),
-                translationZ: specifiedOrDefault(
-                    'space.transformLocks.translationZ',
-                    'boolean',
-                    configuration,
-                ),
-                scale: specifiedOrDefault(
-                    'space.transformLocks.scale',
-                    'boolean',
-                    configuration,
-                ),
-            },
-            transformMode: specifiedOrDefault(
-                'space.transformMode',
-                'string',
-                configuration,
-            ),
-            transformMultimode: specifiedOrDefault(
-                'space.transformMultimode',
-                'boolean',
-                configuration,
-            ),
-            transformTouch: specifiedOrDefault(
-                'space.transformTouch',
-                'string',
-                configuration,
-            ),
-            firstPerson: specifiedOrDefault(
-                'space.firstPerson',
-                'boolean',
-                configuration,
-            ),
-        },
-        network: {
-            host: specifiedOrDefault(
-                'network.host',
-                'string',
-                configuration,
-            ),
-            protocol: specifiedOrDefault(
-                'network.protocol',
-                'string',
-                configuration,
-            ),
-        },
-        development: {
-            planeDebugger: specifiedOrDefault(
-                'development.planeDebugger',
-                'boolean',
-                configuration,
-            ),
-            spaceDebugger: specifiedOrDefault(
-                'development.spaceDebugger',
-                'boolean',
-                configuration,
-            ),
-        },
-    };
+    );
 
     return mergedConfiguration;
 }
