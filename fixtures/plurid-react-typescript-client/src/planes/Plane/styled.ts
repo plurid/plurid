291d5cff9ad10f67148ea7fa97a553d0b34bac70
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-08-17 11:43:28 +0300

    fixtures/plurid-react-typescript-client > dev: source

--- fixtures/plurid-react-typescript-client/src/planes/Plane/styled.ts
+++ fixtures/plurid-react-typescript-client/src/planes/Plane/styled.ts
@@ -1,7 +1,12 @@
-import styled from 'styled-components';
+// #region imports
+    // #region libraries
+    import styled from 'styled-components';
+    // #endregion libraries
+// #endregion imports
 
 
 
+// #region module
 export const StyledPlane = styled.div`
     font-family: 'Ubuntu', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     height: 400px;
@@ -14,3 +19,4 @@ export const StyledPlane = styled.div`
         margin: 1.5rem;
     }
 `;
+// #endregion module
