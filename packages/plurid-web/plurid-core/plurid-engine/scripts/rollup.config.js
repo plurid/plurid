d5f40dad8f377ce6e823e2ae7314250573a238c3
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-08-24 14:04:37 +0300

    plurid-web/plurid-core/plurid-engine > setup: scripts

--- packages/plurid-web/plurid-core/plurid-engine/scripts/rollup.config.js
+++ packages/plurid-web/plurid-core/plurid-engine/scripts/rollup.config.js
@@ -33,7 +33,6 @@ export default {
     plugins: [
         typescript({
             typescript: ttypescript,
-            rollupCommonJSResolveHack: true,
             clean: true,
         }),
         commonjs(),
