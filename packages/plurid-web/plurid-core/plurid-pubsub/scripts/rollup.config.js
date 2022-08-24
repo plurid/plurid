04a6651e80c196c2b29428bd78730ce31ce396f4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-08-24 14:02:43 +0300

    plurid-web/plurid-core/plurid-pubsub > setup: scripts

--- packages/plurid-web/plurid-core/plurid-pubsub/scripts/rollup.config.js
+++ packages/plurid-web/plurid-core/plurid-pubsub/scripts/rollup.config.js
@@ -25,7 +25,6 @@ export default {
     plugins: [
         typescript({
             typescript: ttypescript,
-            rollupCommonJSResolveHack: true,
             clean: true,
         }),
         terser({
