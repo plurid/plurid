44c4042bdd118224b455e9ec43d015fdf21790f8
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-10-20 16:29:53 +0300

    fixtures/plurid-react-typescript-server > setup: scripts

--- fixtures/plurid-react-typescript-server/scripts/custom.js
+++ fixtures/plurid-react-typescript-server/scripts/custom.js
@@ -2,13 +2,13 @@
 
 
 const esModules = [
-    // add depende to be bundled as ES modules
+    // add dependencies to be bundled as ES modules
     // string or (dependency: string): string
 ];
 
 
 const externals = [
-    // add packages to be left as external to the server
+    // add dependencies to be left as external to the server
     // string or (dependency: string): string
 ];
 
